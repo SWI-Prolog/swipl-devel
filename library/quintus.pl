163f903cce1911b0abed2ab5c95acf8f66809899
Jan Wielemaker <wielemak@science.uva.nl>
1994-03-02 14:32:30 +0000

    * Added various new predicates

--- library/quintus.pl
+++ library/quintus.pl
@@ -18,6 +18,7 @@
 	, sin/2
 	, cos/2
 	, tan/2
+	, random/3
 
 	, (mode)/1
 	, (public)/1
@@ -25,6 +26,9 @@
 	, no_style_check/1
 	, retractall/1
 	, otherwise/0
+	, (initialization)/1
+	, absolute_file_name/3
+	, numbervars/3
 	]).
 
 		/********************************
@@ -46,6 +50,34 @@ unix(access(File, 0)) :-
 
 file_exists(File) :-
 	exists_file(File).
+
+
+%	absolute_file_name(+File, +Conditions, -Path).
+
+absolute_file_name(File, Conditions, Path) :-
+	(   memberchk(extensions(Exts), Conditions)
+	->  true
+	;   Exts = []
+	),
+	member(Ext, [''|Exts]),
+	ensure_extension(File, Ext, F2),
+	absolute_file_name(F2, Path),
+	(   memberchk(access(Access), Conditions)
+	->  access_file(Path, Access)
+	;   true
+	), !.
+
+
+ensure_extension(X, '', X) :- !.
+ensure_extension(Base, Ext, Name) :-
+	concat('.', Ext, Ext2),
+	ensure_extension_(Base, Ext2, Name).
+
+ensure_extension_(Base, Ext, Base) :-
+	concat(_, Ext, Base), !.
+ensure_extension_(Base, Ext, Name) :-
+	concat(Base, Ext, Name).
+
 
 		/********************************
 		*        META PREDICATES        *
@@ -105,6 +137,11 @@ sin(A, V) :- V is sin(A).
 cos(A, V) :- V is cos(A).
 tan(A, V) :- V is tan(A).
 
+%	random(+Min, +Max, -Value)
+
+random(Min, Max, Value) :-
+	Value is Min + random(Max).
+
 
 		/********************************
 		*          STYLE CHECK          *
@@ -131,6 +168,22 @@ no_style_check(QOption) :-
 
 mode(_).
 public(_).
+
+:- op(1199, fx, initialization).
+
+:- module_transparent
+	(initialization)/1.
+
+initialization(Goal) :-
+	Goal.
+
+		 /*******************************
+		 *	TERM MANIPULATION	*
+		 *******************************/
+
+numbervars(Term, From, To) :-
+	numbervars(Term, '$VAR', From, To).
+
 
 		/********************************
 		*            MODULES            *
