a3586b43895b7e2f329e6efc0aa31525f763b93a
unknown <unknown>
1995-04-18 12:29:04 +0000

    This commit was manufactured by cvs2svn to create branch 'V57X'.

---
+++ src/INDEX.pl
@@ -0,0 +1,12 @@
+/*  $Id$
+
+    Creator: make/0
+
+    Purpose: Provide index for autoload
+*/
+
+index((a), 0, a, a).
+index((b), 0, b, b).
+index((a), 0, a, c).
+index((hello), 1, t, t).
+index((test), 1, t2, t2).
