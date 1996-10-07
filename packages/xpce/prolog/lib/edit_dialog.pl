8e658dec08a5bffad40e4ff92be4b48342ea584d
Jan Wielemaker <wielemak@science.uva.nl>
1996-10-07 15:04:57 +0000

    * Added edit_dialog.pl

---
+++ packages/xpce/prolog/lib/edit_dialog.pl
@@ -0,0 +1,19 @@
+/*  $Id$
+
+    Part of XPCE
+    Designed and implemented by Anjo Anjewierden and Jan Wielemaker
+    E-mail: jan@swi.psy.uva.nl
+
+    Copyright (C) 1994 University of Amsterdam. All rights reserved.
+*/
+
+:- module(dia_main,
+	  [ dialog/0			% start dialog editor
+	  ]).
+:- use_module(library(pce)).
+
+:- pce_autoload(dia_editor, library('dialog/dialog')).
+
+dialog :-
+	send(new(dia_editor), open).
+	
