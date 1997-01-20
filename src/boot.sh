2b633148b485b713cd8c1a43bee6bd4a1a99431d
Jan Wielemaker <wielemak@science.uva.nl>
1997-01-20 16:36:57 +0000

    * Remove old files

--- src/boot.sh
+++
@@ -1,3 +0,0 @@
-BOOT=c:/pl/boot
-export BOOT
-./pl -o c:/pl/pl.wic -b $BOOT/init.pl -c $BOOT/load.pl
--- src/dump.cmd
+++
@@ -1,2 +0,0 @@
-echo save_program('%2.cmd', [ goal = '''$welcome''', toplevel = prolog]). halt. | %1 -f none -B
-echo forall(library_directory(X), ((exists_directory(X) , write('Index for '), write(X), nl, make_library_index(X)) ; true)). |  %1
