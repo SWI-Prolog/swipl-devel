ede8868a6bc7d3708be79d07e915c8ab30dc7752
Jan Wielemaker <wielemak@science.uva.nl>
1999-11-11 11:36:02 +0000

    * Forgotten files

---
+++ library/statistics.pl
@@ -0,0 +1,141 @@
+/*  $Id$
+
+    Part of SWI-Prolog
+    Designed and implemented by Jan Wielemaker
+
+    Copyright (C) 1999 SWI, University of Amsterdam. All rights reserved.
+*/
+
+
+:- module(prolog_statistics,
+	  [ time/1,
+	    profiler/2,
+	    show_profile/1,
+	    profile/3
+	  ]).
+
+:- module_transparent
+	time/1.
+
+%	time(:Goal)
+%
+%	Time the execution of Goal.  Possible choice-points of Goal are
+%	removed.
+
+time(Goal) :-
+	statistics(cputime, OldTime), 
+	statistics(inferences, OldInferences), 
+	(   catch(Goal, E, fail)
+	->  Result = yes
+	;   Result = no
+	),
+	statistics(inferences, NewInferences), 
+	statistics(cputime, NewTime), 
+	UsedTime is NewTime - OldTime, 
+	UsedInf  is NewInferences - OldInferences - 3, 
+	(   UsedTime =:= 0
+	->  Lips = 'Infinite'
+	;   Lips is integer(UsedInf / UsedTime)
+	), 
+	print_message(informational, time(UsedInf, UsedTime, Lips)),
+	(   nonvar(E)
+	->  throw(E)
+	;   Result == yes
+	).
+
+%    profile(-Old, +New)
+%    change or query profiling status.
+
+profiler(Old, New) :-
+	'$profile'(OldInt, OldInt), 
+	map_profile(Old, OldInt), 
+	atom(New), 
+	map_profile(New, NewInt), !, 
+	'$profile'(_, NewInt).
+profiler(_, New) :-
+	throw(error(domain_error(profile_type, New), _)).
+
+
+map_profile(off, 	0).
+map_profile(cumulative, 1).
+map_profile(plain, 	2).
+
+
+%   show_profile(N)
+%   Show the top N functions' profile. Negative numbers or 0 show ALL
+%   functions that have been called during profiling.
+
+show_profile(N) :-
+	findall( triple(Perc, Calls, Module:Head), 
+		 enum_profile_count(Module:Head, Calls, Perc), 
+		 List), 
+	sort(List, Sorted), 
+	reverse(Sorted, HighFirst), 
+	format('~w~t~w =~41|~t~w~57| = ~w ~t~w~79|~n',
+	       [ 'Predicate', 'Box Entries', 'Calls+Redos'
+	       , 'Exits+Fails', 'Time'
+	       ]),
+	format('~61t~79|~n'),
+	show_profile(N, HighFirst).
+
+enum_profile_count(Head, Calls, Perc) :-
+	current_predicate(_, Head), 
+	\+ predicate_property(Head, imported_from(_)), 
+	profile_count(Head, Calls, Perc), 
+	Calls \== 0.
+
+show_profile(0, _) :- !.
+show_profile(_, []) :- !.
+show_profile(N, [triple(Prom, Total, Pred)|Rest]) :-
+	predicate_name(Pred, Name),
+	profile_box(Pred, Calls, Redos, Exits, Fails),
+	format('~w~t~D =~41|~t~D+~D~57| = ~D+~D ~t~1d%~79|~n',
+	       [Name, Total, Calls, Redos, Exits, Fails, Prom]), 
+	succ(M, N), 
+	show_profile(M, Rest).
+
+:- module_transparent
+	profile/3.
+
+profile(Goal, Style, N) :-
+	profiler(_, off), 
+	reset_profiler, 
+	profiler(_, Style), 
+	(   catch(time(Goal), E, fail)
+	->  Rval = true
+	;   Rval = fail
+	),
+	profiler(_, off), 
+	show_profile(N), !, 
+	(   nonvar(E)
+	->  throw(E)
+	;   Rval == true
+	).
+
+
+		 /*******************************
+		 *	      MESSAGES		*
+		 *******************************/
+
+:- multifile
+	prolog:message/3.
+
+prolog:message(time(UsedInf, UsedTime, Lips)) -->
+	[ '~D inferences in ~2f seconds (~w Lips)'-[UsedInf, UsedTime, Lips] ].
+
+
+:- module_transparent
+	predicate_name/2.
+
+%	predicate_name(+Head, -String)
+%	Convert `Head' into a predicate name.
+
+predicate_name(Goal, String) :-
+	'$strip_module'(Goal, Module, Head), 
+	functor(Head, Name, Arity), 
+	(   memberchk(Module, [user, system])
+	->  sformat(String, '~w/~w',	[Name, Arity])
+	;   sformat(String, '~w:~w/~w',	[Module, Name, Arity])
+	).
+
+
---
+++ library/threadutil.pl
@@ -0,0 +1,91 @@
+/*  $Id$
+
+    Part of SWI-Prolog
+    Designed and implemented by Jan Wielemaker
+
+    Copyright (C) 1999 SWI, University of Amsterdam. All rights reserved.
+*/
+
+:- module(thread_util,
+	  [ threads/0,			% List available threads
+	    interactor/0,		% Create a new interactor
+	    attach_console/0		% Create an xterm-console for thread.
+	  ]).
+
+%	threads
+%
+%	List currently active threads.  If a thread has exited, get
+%	rid of them.
+
+threads :-
+	format('~*t~40|~n', "-"),
+	format('~t~w~8|  ~w~32|~n', ['Thread', 'Status']),
+	format('~*t~40|~n', "-"),
+	current_thread(Id, Status),
+	format('~t~w~8|  ~p~32|~n', [Id, Status]),
+	rip_thread(Status, Id),
+	fail.
+threads :-
+	format('~*t~40|~n', "-").
+
+rip_thread(running, _) :- !.
+rip_thread(_Status, Id) :-
+	thread_join(Id, _).
+	
+%	interactor
+%
+%	Run a Prolog toplevel in another thread with a new console window.
+
+interactor :-
+	thread_create(run_interactor, _Id, []).
+
+run_interactor :-
+	attach_console,
+	'$welcome',
+	run_prolog.
+
+run_prolog :-
+	catch(prolog, E,
+	      ( print_message(error, E),
+%		E = error(_, _),
+		run_prolog)).
+
+%	attach_console
+%
+%	Create an xterm-console and make the standard Prolog streams point to
+%	it.
+
+:- dynamic
+	has_console/1.			% which thread has a console?
+
+has_console(main).			% we assume main has one.
+
+attach_console :-
+	thread_self(Id),
+	has_console(Id), !.
+attach_console :-
+	thread_self(Id),
+	assert(has_console(Id)),
+	sformat(Title, 'SWI-Prolog Thread ~w interactor', [Id]),
+	open_xterm(Title, In, Out),
+	set_stream(In,  alias(user_input)),
+	set_stream(Out, alias(user_output)),
+	set_stream(Out, alias(user_error)),
+	set_stream(In,  alias(current_input)),
+	set_stream(Out, alias(current_output)),
+	thread_at_exit(close(In)),
+	thread_at_exit(close(Out)),
+	thread_at_exit(retractall(has_console(Id))).
+
+
+		 /*******************************
+		 *	       HOOKS		*
+		 *******************************/
+
+:- multifile
+	user:message_hook/3.
+
+user:message_hook(trace_mode(on), _, Lines) :-
+	attach_console,
+	print_message_lines(user_error, '% ', Lines).
+	
