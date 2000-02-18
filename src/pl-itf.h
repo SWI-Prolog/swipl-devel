33d48949fdfabc36172a7b83d38a61d9c015d7c3
Jan Wielemaker <wielemak@science.uva.nl>
2000-02-18 11:58:59 +0000

    * Checkin 3.3.1

--- src/pl-itf.h
+++ src/pl-itf.h
@@ -31,7 +31,7 @@ before loading this file.  See end of this file.
 /* PLVERSION: 10000 * <Major> + 100 * <Minor> + <Patch> */
 
 #ifndef PLVERSION
-#define PLVERSION 30300
+#define PLVERSION 30301
 #endif
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
