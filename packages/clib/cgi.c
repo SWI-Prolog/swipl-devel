e4f6e53d8fae54226a130b0e18a994a7fc949cad
Jan Wielemaker <wielemak@science.uva.nl>
2000-05-01 12:20:36 +0000

    * Added support for multipart * Added version for generic package management

--- packages/clib/cgi.c
+++ packages/clib/cgi.c
@@ -13,11 +13,16 @@
 #include "clib.h"
 
 static int
-isinteger(const char *s, long *val)
+isinteger(const char *s, long *val, int len)
 { char *e;
+  
+  if ( len < 0 )
+    len = strlen(s);
+  if ( len == 0 )
+    return FALSE;
 
   *val = strtol(s, &e, 10);
-  if ( *s && !*e )
+  if ( e == s+len )
     return TRUE;
 
   return FALSE;
@@ -25,11 +30,16 @@ isinteger(const char *s, long *val)
 
 
 static int
-isfloat(const char *s, double *val)
+isfloat(const char *s, double *val, int len)
 { char *e;
 
+  if ( len < 0 )
+    len = strlen(s);
+  if ( len == 0 )
+    return FALSE;
+
   *val = strtod(s, &e);
-  if ( *s && !*e )
+  if ( e == s+len )
     return TRUE;
 
   return FALSE;
@@ -44,9 +54,9 @@ add_to_form(const char *name, const char *value, void *closure)
   long vl;
   double vf;
 
-  if ( isinteger(value, &vl) )
+  if ( isinteger(value, &vl, strlen(value)) )
     PL_put_integer(val, vl);
-  else if ( isfloat(value, &vf) )
+  else if ( isfloat(value, &vf, strlen(value)) )
     PL_put_float(val, vf);
   else
     PL_put_atom_chars(val, value);
@@ -61,10 +71,38 @@ add_to_form(const char *name, const char *value, void *closure)
 }
 
 
+static int
+mp_add_to_form(const char *name, const char *value, int len,
+	       const char *file, void *closure)
+{ term_t head = PL_new_term_ref();
+  term_t tail = (term_t) closure;
+  term_t val  = PL_new_term_ref();
+  long vl;
+  double vf;
+
+  if ( isinteger(value, &vl, len) )
+    PL_put_integer(val, vl);
+  else if ( isfloat(value, &vf, len) )
+    PL_put_float(val, vf);
+  else
+    PL_put_atom_nchars(val, len, value);
+
+  if ( !PL_unify_list(tail, head, tail) ||
+       !PL_unify_term(head,
+		      PL_FUNCTOR, PL_new_functor(PL_new_atom(name), 1),
+		      PL_TERM, val) )
+    return FALSE;
+
+  return TRUE;
+}
+
+
 static foreign_t
 pl_cgi_get_form(term_t form)
-{ char *data = get_raw_form_data(NULL);
+{ int len;
+  char *data = get_raw_form_data(&len);
   term_t list = PL_copy_term_ref(form);
+  char *ct, *boundary;
 
   if ( !data )
   { term_t ctx = PL_new_term_ref();
@@ -74,9 +112,16 @@ pl_cgi_get_form(term_t form)
 		    ERR_EXISTENCE, "cgi_form", ctx);
   }
 
-  break_form_argument(data, add_to_form, (void *)list);
+  if ( (ct = getenv("CONTENT_TYPE")) &&
+       (boundary = strstr(ct, "boundary=")) )
+  { boundary = strchr(boundary, '=')+1;
 
-  return TRUE;
+    break_multipart(data, len, boundary, mp_add_to_form, (void *)list);
+  } else
+  { break_form_argument(data, add_to_form, (void *)list);
+  }
+
+  return PL_unify_nil(list);
 }
 
 
