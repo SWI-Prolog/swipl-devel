b7a5d496eedb5748ff37efea7de27bb5d0f2056c
Jan Wielemaker <wielemak@science.uva.nl>
2001-02-19 10:22:35 +0000

    * Modernised a bit

--- library/readln.pl
+++ library/readln.pl
@@ -35,14 +35,14 @@
 	This predicate gives you the flexibility.
 	It succeeds with arg1 = list of word&atoms
 			 arg2 = Ascii code of last character
-				(but 'end_of_file' in case of ^D).
+				(but '-1' in case of ^D).
 	To change one or more of the defaults you have to
 	instantiate argument3 and/or argument4 and/or argument5.
 	 !! Uninstantiated arguments are defaulted !!
 	- stop character(s):
 		instantiate argument 3 with the list of ASCII code's
 		of the desired stop characters (Note: you can also
-		say: ".!?", what is equivalent to [46,33,63] ).
+		say: ".!?", what is equivalent to [46,33,63]).
 	- word character(s):
 		instantiate argument 4 with the list of ASCII code's
 		of the desired word-part characters (Note: wou can also
@@ -60,7 +60,7 @@ Main predicates provided:
 				 readln(P, [10],"_01213456789",uppercase).)
     readln(P, LastCh)   - idem as above but the second argument is unified
 			  with the last character read (the ascii-code for
-			  the stop-character or 'end_of_file')
+			  the stop-character or -1)
     readln(P, LastCh, Arg1, Arg2, Arg3)
 			- idem as above but the default setting is changed
 			  for the instantiated args:
@@ -69,131 +69,138 @@ Main predicates provided:
 			  Arg3: uppercase/lowercase conversion
 
 Examples:
-	read_sentence( P,Case ) :-
-		readln( P,_,".!?","_0123456789",Case ).
+	read_sentence(P,Case) :-
+		readln(P,_,".!?","_0123456789",Case).
 
-	read_in( P ) :-				% with numbers as separate
-		readln( P,Eof,_,"", _ ).	% entities.
+	read_in(P) :-				% with numbers as separate
+		readln(P,Eof,_,"", _).	% entities.
 
-	read_atom( A ) :-			% stop on newline,
-		readln( A,_,_," ",_).		% space is part of word
+	read_atom(A) :-			% stop on newline,
+		readln(A,_,_," ",_).		% space is part of word
 
 
    Author: Wouter Jansweijer
    Date: 26 april 1985
 
+   Modified: Jan Wielemaker
+   Date: 19 feb 2001
+
+   Modernised a bit and fixed some end_of_file/-1 issues.  As we have modules
+   since a while I removed the ugly $ stuff :-)
 ******************************************************************************/
 
-:- module( readln,[readln/1,readln/2,readln/5] ).
+:- module(readln,
+	  [ readln/1,			% -Line
+	    readln/2,			% -Line, +EOL
+	    readln/5			% See above
+	  ]).
 
-:- style_check( +dollar ).
-
-readln( Read ) :-			% the default is read up to EOL
-	$readln( Line,LastCh,[10],"_0123456789",uppercase ),
-	(   LastCh \== end_of_file,
-		Read = Line
-	|   append( Line,[end_of_file],Read )
-	), !.
+readln(Read) :-			% the default is read up to EOL
+	rl_readln(Line, LastCh, [10], "_0123456789", uppercase),
+	(   LastCh == -1
+	->  append(Line,[end_of_file], Read)
+	;   Read = Line
+	).
 
 readln(Read, LastCh):-
-	$readln(Read, LastCh, [10], "_0123456789", uppercase).
+	rl_readln(Read, LastCh, [10], "_0123456789", uppercase).
 
-readln( P,EOF,StopChars,WordChars,Case ) :-
-	(   var( StopChars ),
-		Arg1 = [10]
-	|   Arg1 = StopChars
+readln(P, EOF, StopChars, WordChars, Case) :-
+	(   var(StopChars)
+	->  Arg1 = [10]
+	;   Arg1 = StopChars
 	),
-	(   var( WordChars ),
-		Arg2 = "01234567890_"
-	|   Arg2 = WordChars
+	(   var(WordChars)
+	->  Arg2 = "01234567890_"
+	;   Arg2 = WordChars
 	),
-	(   var( Case ),
-		Arg3 = lowercase
-	|   Arg3 = Case
-	),  !,
-	$readln( P,EOF,Arg1,Arg2,Arg3 ).
+	(   var(Case)
+	->  Arg3 = lowercase
+	;   Arg3 = Case
+	),
+	rl_readln(P, EOF, Arg1, Arg2, Arg3).
 
-$readln( P,EOF,StopChars,WordChars,Case ) :-
-	$initread( L,EOF,StopChars ),
-	$blanks( L,LL ), !,
-	$words( P,LL,[],options(WordChars,Case) ), !.
+rl_readln(P, EOF, StopChars, WordChars, Case) :-
+	rl_initread(L, EOF, StopChars),
+	rl_blanks(L, LL), !,
+	rl_words(P, LL,[], options(WordChars, Case)), !.
 
-$initread( S,EOF,StopChars ) :- 
+rl_initread(S, EOF, StopChars) :- 
 	get0(K),
-	$readrest( K,S,EOF,StopChars ).
+	rl_readrest(K, S, EOF, StopChars).
 
-$readrest( -1,[],end_of_file,_ ) :- !.
-$readrest( 0'\,[K1|R],EOF,StopChars ) :-
-	get0( K1 ),					% skip it, take next char
-	get0( K2 ),
-	$readrest( K2,R,EOF,StopChars ).
-$readrest( K,[K],K,StopChars ) :-			% the stop char(s)
-	member( K,StopChars ), !.
-$readrest( K,[K|R],EOF,StopChars ) :-			% the normal case
-	get0( K1 ),
-	$readrest( K1,R,EOF,StopChars ).
+rl_readrest(-1, [], end_of_file, _) :- !.
+rl_readrest(0'\, [K1|R], EOF, StopChars) :-
+	get0(K1),			% skip it, take next char
+	get0(K2),
+	rl_readrest(K2, R, EOF, StopChars).
+rl_readrest(K, [K], K, StopChars) :-	% the stop char(s)
+	member(K, StopChars), !.
+rl_readrest(K, [K|R], EOF, StopChars) :-	% the normal case
+	get0(K1),
+	rl_readrest(K1, R, EOF, StopChars).
 
-$words( [W|Ws],S1,S4,Options ) :-
-	$word( W,S1,S2,Options ), !,
-	$blanks( S2,S3 ),
-	$words( Ws,S3,S4,Options ).
-$words( [],S1,S2,_ ) :-
-	$blanks( S1,S2), !.
-$words( [],S,S,_ ).
+rl_words([W|Ws], S1, S4, Options) :-
+	rl_word(W, S1, S2, Options), !,
+	rl_blanks(S2, S3),
+	rl_words(Ws, S3, S4, Options).
+rl_words([], S1, S2, _) :-
+	rl_blanks(S1, S2), !.
+rl_words([], S, S, _).
 
-$word( N,[46|S1],S3,_ ) :-		% the dot can be in the beginning of
-	$basic_num( N1,S1,S2 ),	!,	% a real number.
-	$basic_nums( Rest,S2,S3,dot ),	% N.B. only ONE dot IN a number !!
-	name( N,[48,46,N1|Rest] ).	% i.e '0.<number>'
-$word( N,S0,S2,_ ) :-
-	$basic_num( N1,S0,S1 ), !,
-	$basic_nums( Rest,S1,S2,_ ),
-	name( N,[N1|Rest] ).
-$word( W,S0,S2,Options ) :-
-	$basic_char( C1,S0,S1,Options ), !,
-	$basic_chars( Rest,S1,S2,Options ),
-	name( W,[C1|Rest] ).
-$word( P,[C|R],R,_ ) :-
-	name( P,[C] ), !.
+rl_word(N, [46|S1], S3, _) :-		% the dot can be in the beginning of
+	rl_basic_num(N1, S1, S2),	!,	% a real number.
+	rl_basic_nums(Rest, S2, S3, dot),	% only ONE dot IN a number !!
+	name(N,[48, 46, N1|Rest]).	% i.e '0.<number>'
+rl_word(N, S0, S2, _) :-
+	rl_basic_num(N1, S0, S1), !,
+	rl_basic_nums(Rest, S1, S2, _),
+	name(N,[N1|Rest]).
+rl_word(W, S0, S2, Options) :-
+	rl_basic_char(C1, S0, S1, Options), !,
+	rl_basic_chars(Rest, S1, S2, Options),
+	name(W, [C1|Rest]).
+rl_word(P,[C|R], R, _) :-
+	name(P, [C]), !.
 
-$basic_chars( [A|As],S0,S2,Options ) :-
-   $basic_char( A,S0,S1,Options ), !,
-   $basic_chars( As,S1,S2,Options ).
-$basic_chars( [],S,S,_ ).
+rl_basic_chars([A|As], S0, S2, Options) :-
+	rl_basic_char(A, S0, S1, Options), !,
+	rl_basic_chars(As, S1, S2, Options).
+rl_basic_chars([], S, S, _).
 
-$basic_nums( [46,N|As],[46|S1],S3,Dot ) :- % a dot followed by at least one digit
-   var( Dot ),				   % but not found a dot already
-   $basic_num( N,S1,S2 ), !,
-   $basic_nums( As,S2,S3,dot ).
-$basic_nums( [A|As],S0,S2,Dot ) :-
-   $basic_num( A,S0,S1 ), !,
-   $basic_nums( As,S1,S2,Dot ).
-$basic_nums( [],S,S,_ ).
+rl_basic_nums([46,N|As], [46|S1], S3, Dot) :- % a dot followed by >= one digit
+	var(Dot),			% but not found a dot already
+	rl_basic_num(N, S1, S2), !,
+	rl_basic_nums(As, S2, S3, dot).
+rl_basic_nums([A|As], S0, S2, Dot) :-
+	rl_basic_num(A, S0, S1), !,
+	rl_basic_nums(As, S1, S2, Dot).
+rl_basic_nums([], S, S, _).
 
-$blanks( [C|S0],S1 ) :-
-   $blank( C ), !,
-   $blanks( S0,S1 ).
-$blanks( S,S ).
+rl_blanks([C|S0], S1) :-
+	rl_blank(C), !,
+	rl_blanks(S0, S1).
+rl_blanks(S, S).
 
-/* Basic Character types that form $words together */
+/* Basic Character types that form rl_words together */
 
-$basic_char( A,[C|S],S,options(WordChars,Case) ) :-
-	$lc( C,A,WordChars,Case ).
+rl_basic_char(A, [C|S], S, options(WordChars, Case)) :-
+	rl_lc(C, A, WordChars, Case).
 
-$basic_num( N,[N|R],R ) :-
-	between( 0'0,0'9,N ).
+rl_basic_num(N, [N|R], R) :-
+	code_type(N, digit).
 
-$blank(X) :-
-	X =< 32.
+rl_blank(X) :-
+	code_type(X, space).
 
-$lc( X,X1,_,Case ) :-	
-	between( 0'A,0'Z,X ), !,
-	$fix_case( Case,X,X1 ).
-$lc( X,X,_,_ ) :-
-	between( 0'a,0'z,X ), !.
-$lc( X,X,WordChars,_ ) :-
-	memberchk( X,WordChars ).
+rl_lc(X, X1, _, Case) :-	
+	code_type(X, upper), !,
+	rl_fix_case(Case, X, X1).
+rl_lc(X, X, _, _) :-
+	code_type(X, lower).
+rl_lc(X, X, WordChars, _) :-
+	memberchk(X, WordChars).
 
-$fix_case( lowercase,U,L ) :- !,
-	plus( U,32,L ).
-$fix_case( _,C,C ).
+rl_fix_case(lowercase, U, L) :- !,
+	code_type(L, lower(U)).
+rl_fix_case(_, C, C).
