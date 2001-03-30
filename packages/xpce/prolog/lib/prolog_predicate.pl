96831a301c23a8f836ce4396193e5cb4a9d66eeb
Jan Wielemaker <wielemak@science.uva.nl>
2001-03-30 19:12:18 +0000

    * Represent a Prolog predicate

---
+++ packages/xpce/prolog/lib/prolog_predicate.pl
@@ -0,0 +1,91 @@
+/*  $Id$
+
+    Part of XPCE
+    Designed and implemented by Anjo Anjewierden and Jan Wielemaker
+    E-mail: jan@swi.psy.uva.nl
+
+    Copyright (C) 2001 University of Amsterdam. All rights reserved.
+*/
+
+:- module(prolog_predicate, []).
+:- use_module(library(pce)).
+:- require([ concat_atom/2
+	   , term_to_atom/2
+	   ]).
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Class prolog_predicate is  a  rather  simple   class  to  represent  the
+identity of a Prolog  predicate.  It   is  used  with predicate_item for
+locating predicates and an important reason   for  the existence of this
+class is to have the type prolog_predicate available.
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+:- pce_begin_class(prolog_predicate, object,
+		   "Represent a Prolog predicate").
+
+variable(module,	name*,	get, "Module of the predicate").
+variable(name,		name,	get, "Name of predicate").
+variable(arity,		'0..',	get, "Arity of the predicate").
+
+initialise(P, Term:prolog) :->
+	"Create from [Module]:Name/Arity"::
+	(   Term = Module:Name/Arity
+	->  true
+	;   Term = Name/Arity,
+	    Module = @nil
+	),
+	send(P, slot, module, Module),
+	send(P, slot, name, Name),
+	send(P, slot, arity, Arity).
+
+convert(_, From:name, P:prolog_predicate) :<-
+	"Convert textual and Prolog term"::
+	term_to_atom(From, Term),
+	(   (   Term = Module:Name/Arity
+	    ;	Term = Name/Arity
+	    )
+	->  new(P, prolog_predicate(Term))
+	;   Term = Module:Head,
+	    callable(Head)
+	->  functor(Head, Name, Arity),
+	    new(P, prolog_predicate(Module:Name/Arity))
+	;   callable(Head)
+	->  new(P, prolog_predicate(Name/Arity))
+	).
+
+print_name(P, PN:name) :<-
+	"Return as [Module:]Name/Arity"::
+	get(P, name, Name),
+	get(P, arity, Arity),
+	(   get(P, module, Module), Module \== @nil
+	->  concat_atom([Name, /, Arity], PN)
+	;   concat_atom([Module : Name, /, Arity], PN)
+	).
+	
+head(P, Qualify:[bool], Head:prolog) :<-
+	"Get a head-term"::
+	get(P, module, Module),
+	get(P, name, Name),
+	get(P, arity, Arity),
+	functor(Head0, Name, Arity),
+	(   (   Qualify == @off
+	    ;	Qualify == @default,
+		Module == @nil
+	    )
+	->  Head = Head0
+	;   Module \== @nil
+	->  Head = Module:Head0
+	;   Head = user:Head0
+	).
+
+source(P, Loc:source_location) :<-
+	"Return source-location from Prolog DB"::
+	get(P, head, @on, Head),
+	predicate_property(Head, file(File)),
+	(   predicate_property(Head, line_count(Line))
+	->  new(Loc, source_location(File, Line))
+	;   new(Loc, source_location(File))
+	).
+	
+:- pce_end_class(prolog_predicate).
+	  
