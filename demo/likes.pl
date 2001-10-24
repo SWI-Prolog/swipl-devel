47b7f0fb7faaa7a61ba1edb11ef999728272a036
Jan Wielemaker <wielemak@science.uva.nl>
2001-10-24 09:29:26 +0000

    * Starting a demo

---
+++ demo/likes.pl
@@ -0,0 +1,39 @@
+%% Demo coming from http://clwww.essex.ac.uk/course/LG519/2-facts/index_18.html
+%% Distributed with SWI-Prolog with permission from the author.
+%%
+%% Please load this file into SWI-Prolog
+%%
+%% Sam's likes and dislikes in food
+%% 
+%% Considering the following will give some practice
+%% in thinking about backtracking.
+%% ?- likes(sam,dahl).
+%% ?- likes(sam,chop_suey).
+%% ?- likes(sam,pizza).
+%% ?- likes(sam,chips).
+%% ?- likes(sam,curry).
+
+likes(sam,Food) :-
+        indian(Food),
+        mild(Food).
+likes(sam,Food) :-
+        chinese(Food).
+likes(sam,Food) :-
+        italian(Food).
+likes(sam,chips).
+
+indian(curry).
+indian(dahl).
+indian(tandoori).
+indian(kurma).
+
+mild(dahl).
+mild(tandoori).
+mild(kurma).
+
+chinese(chow_mein).
+chinese(chop_suey).
+chinese(sweet_and_sour).
+
+italian(pizza).
+italian(spaghetti).
