88b90d63627d625a8a2eabb7c53a4625c181170e
Jan Wielemaker <wielemak@science.uva.nl>
2001-12-07 15:53:03 +0000

    * Proper handling of doctype(_) option

--- packages/sgml/sgml.pl
+++ packages/sgml/sgml.pl
@@ -163,6 +163,7 @@ parser_option(line(_)).
 parser_option(space(_)).
 parser_option(number(_)).
 parser_option(defaults(_)).
+parser_option(doctype(_)).
 
 set_parser_options(Parser, Options, RestOptions) :-
 	parser_option(Option),
