40d85b5ae1d0017aac4e47ee541a8eafacbe173e
Jan Wielemaker <wielemak@science.uva.nl>
2002-01-02 15:07:16 +0000

    * Documentation patches

--- packages/xpce/pl/src/interface.c
+++ packages/xpce/pl/src/interface.c
@@ -2547,7 +2547,7 @@ the possibility of reentrance at moments this is not allowed in PCE ...
 #endif
 
 #ifndef TIMEOUT
-#define TIMEOUT 0			/* just wait */
+#define TIMEOUT 250			/* just wait */
 #endif
 
 static int
