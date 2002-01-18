0871ce7d0c6b075628973fbcbabb7028c940444f
Jan Wielemaker <wielemak@science.uva.nl>
2002-01-18 11:52:58 +0000

    * Added class persistent_frame

---
+++ packages/xpce/prolog/lib/persistent_frame.pl
@@ -0,0 +1,235 @@
+/*  $Id$
+
+    Developed in the MIA project
+    Designed and implemented by Jan Wielemaker
+    E-mail: jan@swi.psy.uva.nl
+
+    Copyright (C) 2002 University of Amsterdam. All rights reserved.
+*/
+
+:- module(persistent_frame, []).
+:- use_module(library(pce)).
+:- use_module(library(pce_config)).
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+This library defines the class  persistent_frame,   a  subclass of class
+frame remembering its geometry  and  optionally   (by  default  on)  the
+subwindow layout.
+
+This class cooperates with the   library(pce_config),  a generic package
+for managing application preferences.  It collects the locations of user
+frames in the file <profile-dir>/Geometry.cnf
+
+Geometry information is stored in  the   internal  configuration DB (see
+library(pce_config))  if  a  frame  is  closed   or  on  exit  from  the
+application. The internal database is written   to  tehe above mentioned
+file on exit from the application.
+
+Somehow the system must identify the frame   to decide which geometry to
+use. This is done using the  <->geometry_key.   If  not set, this is the
+classname or, if the class is not subclassed   it  is the <-label of the
+frame.
+
+Exploiting this library is very simple,  just make your toplevel windows
+for  which  you  want  the  geometry  remembered  a  subclass  of  class
+persistent_frame rather than class frame.  Note   that  this implies you
+have to create your frame explitely:
+
+	...
+	new(F, persistent_frame('Pretty Application')),
+	send(F, geometry_key, pretty_app),
+	send(F, append, new(D, dialog)),
+	send(new(V, view), right, D),
+	...
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+:- pce_begin_class(persistent_frame, frame, "Frame remembering location").
+
+variable(persistent_subwindow_layout, bool := @on, get,
+	 "Remember the layout of the subwindows?").
+variable(geometry_key,		      [name], send,
+	 "Key used to identify this frame").
+
+unlink(F) :->
+	send(F, save_layout),
+	send_super(F, unlink).
+
+open(F, Pos:point=[point], Grab:grab=[bool], Normalise:normalise=[bool]) :->
+	send(F, create),
+	send(F, load_layout),
+	send_super(F, open, Pos, Grab, Normalise).
+
+:- pce_group(config).
+
+geometry_key(F, Key:name) :<-
+	"Name to store geometry"::
+	(   get(F, slot, geometry_key, Key),
+	    Key \== @default
+	->  true
+	;   get(F, class_name, Key),
+	    Key \== persistent_frame
+	->  true
+	;   get(F, label, Key)
+	).
+
+save_layout(F) :->
+	"Save current layout in config DB"::
+	get(F, geometry, Geometry),
+	get(F, geometry_key, Key),
+	set_config(history/geometry/Key, Geometry),
+	(   get(F, persistent_subwindow_layout, @on),
+	    get(F, tile, RootTile),
+	    get(RootTile, members, Members),
+	    Members \== @nil,
+	    get_tile_layout(RootTile, Layout)
+	->  set_config(history/subwindow_layout/Key, Layout)
+	;   true
+	).
+
+load_layout(F) :->
+	load_geometry_config,
+	get(F, geometry_key, Key),
+	(   get_config(history/geometry/Key, Geometry)
+	->  send(F, geometry, Geometry)
+	;   true
+	),
+	(   get(F, persistent_subwindow_layout, @on),
+	    get_config(history/subwindow_layout/Key, Layout)
+	->  get(F, tile, RootTile),
+	    apply_tile_layout(RootTile, Layout)
+	;   true
+	).
+
+get_tile_layout(T, layout(Me, SubLayout)) :-
+	get_this_tile_layout(T, Me),
+	(   get(T, members, Members),
+	    Members \== @nil
+	->  chain_list(Members, List),
+	    maplist(get_this_tile_layout, List, SubLayout)
+	;   SubLayout = []
+	).
+
+get_this_tile_layout(T, Size) :-
+	get(T, can_resize, @on), !,
+	get(T, area, A),
+	(   get(T?super, orientation, horizontal)
+	->  get(A, width, Size)
+	;   get(A, height, Size)
+	).
+get_this_tile_layout(_, *).
+
+apply_tile_layout(T, layout(Me, SubLayout)) :-
+	apply_this_tile_layout(T, Me),
+	(   get(T, members, Members),
+	    Members \== @nil
+	->  chain_list(Members, List),
+	    maplist(apply_this_tile_layout, List, SubLayout)
+	;   true
+	).
+
+apply_this_tile_layout(_, *) :- !.
+apply_this_tile_layout(T, Size) :-
+	(   get(T?super, orientation, horizontal)
+	->  send(T, width, Size)
+	;   send(T, height, Size)
+	).
+
+:- pce_end_class(persistent_frame).
+
+
+		 /*******************************
+		 *	    EXIT HOOKS		*
+		 *******************************/
+
+:- initialization
+   send(@pce, exit_message,
+	message(@display?frames,
+		for_some,
+		if(message(@arg1, instance_of, persistent_frame),
+		   message(@arg1, save_layout)))).
+
+
+		 /*******************************
+		 *	   CONFIG HOOKS		*
+		 *******************************/
+
+config(config/file,
+       [ default('Geometry')
+       ]).
+config(history/geometry/_Key,
+       [ type(geometry),
+	 editable(false),
+	 comment('(X-)geometry for persistent frames')
+       ]).
+config(history/subwindow_layout/_Key,
+       [ type(subwindow_layout),
+	 editable(false),
+	 comment('Sub-window layout for persistent frames')
+       ]).
+
+:- register_config(config).
+
+load_geometry_config :-
+	context_module(M),
+	ensure_loaded_config(M:_).
+
+
+		 /*******************************
+		 *	       TEST		*
+		 *******************************/
+
+end_of_file.				% make the file end here.
+
+test :-
+	send(new(mydialog), open).
+
+:- pce_begin_class(myframe1, persistent_frame).
+
+initialise(F) :->
+	send_super(F, initialise, 'Frame holding a browser'),
+	send(F, append, new(browser)).
+
+:- pce_end_class(myframe1).
+
+
+:- pce_begin_class(myframe2, persistent_frame).
+
+initialise(F) :->
+	send_super(F, initialise, 'Frame holding a view'),
+	send(F, append, new(V, view)),
+	send(new(picture), left, V).
+
+:- pce_end_class(myframe2).
+
+:- pce_begin_class(myframe3, persistent_frame).
+
+initialise(F) :->
+	send_super(F, initialise, 'Frame holding a picture'),
+	send(F, append, new(picture)).
+
+:- pce_end_class(myframe3).
+
+:- pce_begin_class(mydialog, persistent_frame).
+
+initialise(F) :->
+	send_super(F, initialise('Test persistent frames')),
+	send(F, append, new(D, dialog)),
+	send(D, append, button(frame_1)),
+	send(D, append, button(frame_2)),
+	send(D, append, button(frame_3)),
+	send(D, open).
+
+frame_1(_F) :->
+	send(new(myframe1), open).
+
+frame_2(_F) :->
+	send(new(myframe2), open).
+
+frame_3(_F) :->
+	send(new(myframe3), open).
+
+:- pce_end_class(mydialog).
+	     
+
+
