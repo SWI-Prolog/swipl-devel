d05dea85b76c4ed90faafbacaf3cd43890314add
Jan Wielemaker <wielemak@science.uva.nl>
2002-02-20 19:52:52 +0000

    * Include config.h

--- packages/clib/mime.c
+++ packages/clib/mime.c
@@ -22,6 +22,10 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <SWI-Stream.h>
 #include <SWI-Prolog.h>
 #include <rfc2045.h>
