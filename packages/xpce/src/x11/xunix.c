10badd83bdf613f4fd082f319bd7ae4856f94e05
Jan Wielemaker <wielemak@science.uva.nl>
2002-03-26 13:00:29 +0000

    * Basic unix functions

---
+++ packages/xpce/src/x11/xunix.c
@@ -0,0 +1,54 @@
+/*  $Id$
+
+    Part of XPCE --- The SWI-Prolog GUI toolkit
+
+    Author:        Jan Wielemaker and Anjo Anjewierden
+    E-mail:        jan@swi.psy.uva.nl
+    WWW:           http://www.swi.psy.uva.nl/projects/xpce/
+    Copyright (C): 1985-2002, University of Amsterdam
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include <h/kernel.h>
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Actually this isn't part of the window   system, but OS binding. For the
+moment we will ignore this fact. The corresponding windows functions are
+in ../msw/mswin.c
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+char *ws_user(void)
+    Get the identifier name of the currently logged in user.
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+char *
+ws_user()
+{ char *name;
+#ifdef HAVE_GETLOGIN
+
+  if ( (name = getlogin()) )
+    return name;
+
+#else
+
+  if ( (name = getenv("USER")) )
+    return name;
+
+#endif
+
+  return NULL;
+}
