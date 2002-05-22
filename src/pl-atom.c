0d2095a97fea62e7d21b3343b86bfaf246ac767a
Jan Wielemaker <wielemak@science.uva.nl>
2002-05-22 12:03:55 +0000

    * Atom mutex problem

--- src/pl-atom.c
+++ src/pl-atom.c
@@ -232,7 +232,9 @@ lookupAtom(const char *s, unsigned int length)
 	 memcmp(s, a->name, length) == 0 )
     { 
 #ifdef O_ATOMGC
+      LOCK(L_MISC);			/* used by PL_(un)register_atom() */
       a->references++;
+      UNLOCK(L_MISC);
 #endif
       UNLOCK();
       return a->atom;
