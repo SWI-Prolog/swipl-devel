989fb8c65f604c4591c9f1b4e2e96f3df8dda7c0
Jan Wielemaker <wielemak@science.uva.nl>
2002-09-22 08:31:18 +0000

    * Cygwin compatibility

---
+++ packages/xpce/src/msw/mscygwin.c
@@ -0,0 +1,25 @@
+/*  $Id$
+
+    Part of SWI-Prolog
+
+    Author:  Jan Wielemaker and Anjo Anjewierden
+    E-mail:  jan@swi.psy.uva.nl
+    WWW:     http://www.swi.psy.uva.nl/projects/xpce/
+    Copying: GPL-2.  See the file COPYING or http://www.gnu.org
+
+    Copyright (C) 1990-2001 SWI, University of Amsterdam. All rights reserved.
+*/
+
+#include "include.h"
+
+void
+ws_done_process(Process p)
+{
+}
+
+UINT
+GetEnhMetaFileBits(HENHMETAFILE hemf, UINT cbBuffer, LPBYTE lpbBuffer)
+{ Cprintf("GetEnhMetaFileBits() stub called\n");
+  return FALSE;
+}
+ 
