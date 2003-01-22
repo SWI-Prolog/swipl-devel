143d3a7c8333eebeafb7a2e02f7255965ea77bf7
Jan Wielemaker <wielemak@science.uva.nl>
2003-01-22 19:48:24 +0000

    * Added assume to meta-predicates

--- packages/xpce/prolog/lib/emacs/prolog_xref.pl
+++ packages/xpce/prolog/lib/emacs/prolog_xref.pl
@@ -355,6 +355,7 @@ xref_meta(call_cleanup(A, B),	[A, B]).
 xref_meta(call_cleanup(A, _, B),[A, B]).
 xref_meta(on_signal(_,_,A),	[A+1]).
 xref_meta(with_mutex(_,A),	[A]).
+xref_meta(assume(G),		[G]).	% library(debug)
 
 					% XPCE meta-predicates
 xref_meta(pce_global(_, new(_)), _) :- !, fail.
