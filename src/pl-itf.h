c4bf80f823c5b97cf1f5b15005d5f7fa03bc197a
Jan Wielemaker <wielemak@science.uva.nl>
2003-02-02 10:51:23 +0000

    * Preparing 5.1.7

--- src/pl-itf.h
+++ src/pl-itf.h
@@ -44,7 +44,7 @@ before loading this file.  See end of this file.
 /* PLVERSION: 10000 * <Major> + 100 * <Minor> + <Patch> */
 
 #ifndef PLVERSION
-#define PLVERSION 50106
+#define PLVERSION 50107
 #endif
 
 		 /*******************************
