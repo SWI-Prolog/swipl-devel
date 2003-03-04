d2aae1bebcd36779e487ba8566e8af7421dcd421
Jan Wielemaker <wielemak@science.uva.nl>
2003-03-04 13:25:43 +0000

    * More sublte timing

--- src/Tests/thread/queue_create.pl
+++ src/Tests/thread/queue_create.pl
@@ -4,15 +4,28 @@
 	  ]).
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-This simple test verifies creation and destruction of message queues
-associated with threads.
+This simple test verifies creation  and   destruction  of message queues
+associated with threads. There appears to be  a HUGE difference in speed
+between platforms in the elapsed time it takes to get this test done, so
+first we try to figure out how fast it is about.
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 queue_create :-
-	queue_create(1000).
+	elapsed_time(queue_create(10), T),
+	(   T > 0
+	->  Times is min(1000, round(10*(5/T)))
+	;   Times = 1000
+	),
+	queue_create(Times).
 
 queue_create(Times) :-
 	forall(between(1, Times, _), test).
+
+elapsed_time(G, T) :-
+	get_time(T0),
+	G,
+	get_time(T1),
+	T is T1 - T0.
 
 test :-
 	thread_self(Me),
