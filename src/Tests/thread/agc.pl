acaecfd01b26235ed6c0eb7ea642a9be4a9f4238
Jan Wielemaker <wielemak@science.uva.nl>
2003-03-06 17:27:44 +0000

    * Adjust to speed of system to a max of 5 seconds

--- src/Tests/thread/agc.pl
+++ src/Tests/thread/agc.pl
@@ -1,5 +1,6 @@
 :- module(agc,
-	  [ agc/0
+	  [ agc/0,
+	    agc/1
 	  ]).
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -14,12 +15,29 @@ agc_loop :-
 	garbage_collect_atoms,
 	fail.
 
+
 agc :-
+	elapsed_time(agc(100), T),
+	(   T > 0
+	->  Times is min(50000, round(100*(5/T)))
+	;   Times = 50000
+	),
+	agc(Times).
+
+
+agc(Times) :-
 	thread_create(agc_loop, Id, []),
-	forall(between(0, 50000, X),
+	forall(between(1, Times, X),
 	       (   atom_concat(xxxx, X, A),
 		   assert(a(A))
 	       )),
 	thread_signal(Id, abort),
 	thread_join(Id, _),
 	retractall(a(_)).
+
+
+elapsed_time(G, T) :-
+	get_time(T0),
+	G,
+	get_time(T1),
+	T is T1 - T0.
