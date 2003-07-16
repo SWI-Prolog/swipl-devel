7656181bd2c44e18213b2b86fd03c551f5daeaf3
Jan Wielemaker <wielemak@science.uva.nl>
2003-07-16 19:12:06 +0000

    * Cleanup

--- packages/xpce/prolog/lib/pce_toc.pl
+++ packages/xpce/prolog/lib/pce_toc.pl
@@ -31,7 +31,6 @@
 
 :- module(pce_toc, []).
 :- use_module(library(pce)).
-:- use_module(library(pce_unclip)).
 :- require([ default/3
 	   , ignore/1
 	   ]).
@@ -526,7 +525,7 @@ image(TF, Image:image) :<-
 :- pce_group(event).
 
 event(TF, Ev:event) :->
-	(   send(TF, send_super, event, Ev)
+	(   send_super(TF, event, Ev)
 	;   send(@toc_node_recogniser, event, Ev)
 	).
 
