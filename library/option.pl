6174b1bc77f6cbf513643e562594cef1b2ddfa0a
Jan Wielemaker <wielemak@science.uva.nl>
2003-10-08 14:47:16 +0000

    * Simple option processing

---
+++ library/option.pl
@@ -0,0 +1,51 @@
+/*  $Id$
+
+    Part of SWI-Prolog
+
+    Author:        Jan Wielemaker
+    E-mail:        jan@swi.psy.uva.nl
+    WWW:           http://www.swi-prolog.org
+    Copyright (C): 1985-2002, University of Amsterdam
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    As a special exception, if you link this library with other files,
+    compiled with a Free Software compiler, to produce an executable, this
+    library does not by itself cause the resulting executable to be covered
+    by the GNU General Public License. This exception does not however
+    invalidate any other reasons why the executable file might be covered by
+    the GNU General Public License.
+*/
+
+:- module(swi_option,
+	  [ option/3			% +Term, +List, +Default
+	  ]).
+
+%	option(Option(?Value), +OptionList, +Default)
+%
+%	Get an option from a OptionList. OptionList can use the
+%	Name=Value as well as the Name(Value) convention.
+
+option(Opt, Options) :-
+	memberchk(Opt, Options), !.
+option(Opt, Options) :-
+	functor(Opt, OptName, 1),
+	arg(1, Opt, OptVal),
+	memberchk(OptName=OptVal, Options), !.
+
+option(Opt, Options, _) :-
+	option(Opt, Options), !.
+option(Opt, _, Default) :-
+	arg(1, Opt, Default).
