70b082661d0c27e32c560e247af094cd17a26669
Jan Wielemaker <wielemak@science.uva.nl>
2003-10-27 10:51:01 +0000

    * Preparing 5.2.10

--- pl.spec
+++ pl.spec
@@ -1,6 +1,6 @@
 Summary:	SWI-Prolog - Edinburgh compatible Prolog compiler
 Name:		pl
-Version: 	5.2.9
+Version: 	5.2.10
 Release:	1
 License:	LGPL
 Source:		ftp://swi.psy.uva.nl/pub/SWI-Prolog/pl-%{version}.tar.gz
--- src/pl-itf.h
+++ src/pl-itf.h
@@ -44,7 +44,7 @@ before loading this file.  See end of this file.
 /* PLVERSION: 10000 * <Major> + 100 * <Minor> + <Patch> */
 
 #ifndef PLVERSION
-#define PLVERSION 50209
+#define PLVERSION 50210
 #endif
 
 		 /*******************************
