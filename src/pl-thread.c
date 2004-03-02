66f2b413792c9bf755aea2cc04022117234b6ee8
Jan Wielemaker <wielemak@science.uva.nl>
2004-03-02 20:36:13 +0000

    * Lock other threads on reconsult

--- src/pl-thread.c
+++ src/pl-thread.c
@@ -3106,6 +3106,7 @@ doThreadLocalData(int sig)
   { DEBUG(1, Sdprintf("\n\tDone work on %d; suspending ...",
 		      info->pl_tid));
     
+    info->status = PL_THREAD_SUSPENDED;
     sem_post(sem_mark_ptr);
     wait_resume(info);
   } else
