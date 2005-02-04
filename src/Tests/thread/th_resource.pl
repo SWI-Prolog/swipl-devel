2f0ea1c2d3efc4ea4ecde54786ae81cbaa93cdfe
Jan Wielemaker <wielemak@science.uva.nl>
2005-02-04 10:38:11 +0000

    * Resource exhaustion test

---
+++ src/Tests/thread/th_resource.pl
@@ -0,0 +1,36 @@
+:- module(th_resource,
+	  [ th_resource/0
+	  ]).
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Test to see what happens if we create threads exhausting virtual memory.
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+th_resource :-
+	length(L, 20),
+	create_threads(L),
+	kiss_threads(L),
+	join_threads(L).
+
+create_threads([]).
+create_threads([H|T]) :-
+	thread_create(thread_get_message(_), H,
+		      [ local(infinite),
+			global(infinite),
+			trail(infinite)
+		      ]),
+	create_threads(T).
+
+kiss_threads([]).
+kiss_threads([H|T]) :-
+	thread_send_message(H, done),
+	kiss_threads(T).
+
+join_threads([]).
+join_threads([H|T]) :-
+	thread_join(H, Status),
+	check_status(Status),
+	join_threads(T).
+
+check_status(true).
+check_status(exception(error(resource_error(virtual_memory), _))).
