c557a113e09af6088dfe828d8fed4629c2e05ae4
Jan Wielemaker <wielemak@science.uva.nl>
2005-02-21 20:53:36 +0000

    * Disable debugging

--- src/pl-wic.c
+++ src/pl-wic.c
@@ -22,7 +22,7 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-#define O_DEBUG 1
+/*#define O_DEBUG 1*/
 #include "pl-incl.h"
 #ifdef HAVE_SYS_PARAM_H
 #include <sys/param.h>
