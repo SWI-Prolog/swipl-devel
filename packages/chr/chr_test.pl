e9d28fae38ed622e19df1b63bf6bcb430ed3ff28
Tom Schrijvers <Tom.Schrijvers@cs.kuleuven.be>
2005-11-04 15:30:11 +0000

    bug fixes

--- packages/chr/chr_test.pl
+++ packages/chr/chr_test.pl
@@ -50,6 +50,7 @@ follow_links(File, File).
 run_test_script(Script) :-
 	file_base_name(Script, Base),
 	file_name_extension(Pred, _, Base),
+	format(' ~w~n',[Script]),
 	load_files(Script, []), %[silent(true)]),
 	Pred.
 
