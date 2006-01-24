922faabf89de5204187dd9519e184af2ca43ba2f
Jan Wielemaker <wielemak@science.uva.nl>
2006-01-24 15:30:49 +0000

    * Install examples

--- packages/cppproxy/examples/parms.pl
+++ packages/cppproxy/examples/parms.pl
@@ -10,10 +10,18 @@ compile(CPP) :-
 	file_name_extension(Exe, _, CPP),
 	compile(CPP, Exe).
 
+%	compile(+C++-Source, -Executable)
+%	
+%	Call the C++ compiler.  The   path  -I../../../../include  finds
+%	SWI-proxy.cpp and SWI-proxy.h in the   distribution.  -I.. finds
+%	them in the package source.
+
 compile(CPP, Exe) :-
 	port(Port),
 	current_prolog_flag(c_cc, gcc), !,
-	sformat(Cmd, 'g++ -DPORT=~w -I.. -I. -o ~w ~w', [Port, Exe, CPP]),
+	sformat(Cmd,
+		'g++ -DPORT=~w -I. -I.. -I../../../../include -o ~w ~w',
+		[Port, Exe, CPP]),
 	shell(Cmd).
 	
 port(5000).
