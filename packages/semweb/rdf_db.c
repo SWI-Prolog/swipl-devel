7711cd03542859d9d6bb1b35e00f610164935f33
Jan Wielemaker <wielemak@science.uva.nl>
2006-02-04 14:54:47 +0000

    * Copyright update

--- packages/semweb/rdf_db.c
+++ packages/semweb/rdf_db.c
@@ -3,9 +3,9 @@
     Part of SWI-Prolog
 
     Author:        Jan Wielemaker
-    E-mail:        jan@swi.psy.uva.nl
+    E-mail:        wielemal@science.uva.nl
     WWW:           http://www.swi-prolog.org
-    Copyright (C): 1985-2002, University of Amsterdam
+    Copyright (C): 1985-2006, University of Amsterdam
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
