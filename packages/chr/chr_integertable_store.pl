f6f297ff6ddb0a7afaf26dba27c531c4b3826c4d
Tom Schrijvers <Tom.Schrijvers@cs.kuleuven.be>
2006-03-10 14:13:15 +0000

    missing headers

--- packages/chr/chr_integertable_store.pl
+++ packages/chr/chr_integertable_store.pl
@@ -1,7 +1,34 @@
-% author: Jon Sneyers
-% email:  Jon Sneyers@cs.kuleuven.be
-% based on chr_hashtable_store (by Tom Schrijvers)
-% copyright: K.U.Leuven, 2005
+/*  $Id$
+
+    Part of CHR (Constraint Handling Rules)
+
+    based on chr_hashtable_store (by Tom Schrijvers)
+    Author:        Jon Sneyers
+    E-mail:        Jon.Sneyers@cs.kuleuven.be
+    WWW:           http://www.swi-prolog.org
+    Copyright (C): 2005, K.U. Leuven
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    As a special exception, if you link this library with other files,
+    compiled with a Free Software compiler, to produce an executable, this
+    library does not by itself cause the resulting executable to be covered
+    by the GNU General Public License. This exception does not however
+    invalidate any other reasons why the executable file might be covered by
+    the GNU General Public License.
+*/
 
 % is it safe to use nb_setarg here?
 
