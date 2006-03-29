327fda0c3e6faf47d646c7570c88af8a36a5a6b7
Jan Wielemaker <wielemak@science.uva.nl>
2006-03-29 08:31:12 +0000

    * Cleanup

--- src/Unicode/derived_core_properties.pl
+++ src/Unicode/derived_core_properties.pl
@@ -1,6 +1,40 @@
+/*  $Id$
+
+    Part of SWI-Prolog
+
+    Author:        Jan Wielemaker and Richard O'Keefe
+    E-mail:        jan@swi.psy.uva.nl
+    WWW:           http://www.swi-prolog.org
+    Copyright (C): 1985-2002, University of Amsterdam
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    As a special exception, if you link this library with other files,
+    compiled with a Free Software compiler, to produce an executable, this
+    library does not by itself cause the resulting executable to be covered
+    by the GNU General Public License. This exception does not however
+    invalidate any other reasons why the executable file might be covered by
+    the GNU General Public License.
+*/
+
 :- module(unicode_derived_core_properties,
 	  [ unicode_derived_core_property/2
 	  ]).
+:- use_module(library(debug), [debug/3]).
+:- use_module(library(lists), [member/2, numlist/3]).
+:- use_module(library(readutil), [read_line_to_codes/2]).
 
 :- dynamic
 	derived_property/2,
@@ -104,5 +138,3 @@ ws -->
 	[].
 
 skip_rest(_, []).
-eol -->
-	[10].
