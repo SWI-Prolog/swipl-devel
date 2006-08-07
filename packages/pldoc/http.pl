41099dcd95a76f29c91ecc9871251b3667a771f1
Jan Wielemaker <wielemak@science.uva.nl>
2006-08-07 13:58:37 +0000

    * First version with HTTP server

---
+++ packages/pldoc/http.pl
@@ -0,0 +1,167 @@
+/*  $Id$
+
+    Part of SWI-Prolog
+
+    Author:        Jan Wielemaker
+    E-mail:        wielemak@science.uva.nl
+    WWW:           http://www.swi-prolog.org
+    Copyright (C): 1985-2006, University of Amsterdam
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    As a special exception, if you link this library with other files,
+    compiled with a Free Software compiler, to produce an executable, this
+    library does not by itself cause the resulting executable to be covered
+    by the GNU General Public License. This exception does not however
+    invalidate any other reasons why the executable file might be covered by
+    the GNU General Public License.
+*/
+
+:- module(pldoc_http,
+	  [ doc_server/1		% ?Port
+	  ]).
+:- use_module(library('http/thread_httpd')).
+:- use_module(library('http/http_parameters')).
+:- use_module(library('http/mimetype')).
+
+doc_server(Port) :-
+	doc_server(Port, []).
+
+doc_server(Port, Options) :-
+	http_server(doc_reply,
+                    [ port(Port),
+                      timeout(60),
+		      keep_alive_timeout(1)
+                    | Options
+                    ]).
+
+doc_reply(Request) :-
+	memberchk(path(Path), Request),
+	reply(Path, Request).
+
+:- discontiguous
+	reply/2.
+
+%	/
+%	
+%	Reply with frameset
+
+reply(/, _) :-
+	phrase(html([ head(title('SWI-Prolog documentation server')),
+		      frameset([cols('200,*')],
+			       [ frame([ src('sidebar.html'),
+					 name(sidebar)
+				       ]),
+				 frame([ src('welcome.html'),
+					 name(main)
+				       ])
+			       ])
+		    ]), HTML),
+	format('Content-type: text/html~n~n'),
+	print_html(HTML).
+
+%	/sidebar.html
+%	
+%	Reply with main menu.
+
+reply('/sidebar.html', _Request) :- !,
+	findall(File, documented_file(File), Files0),
+	sort(Files0, Files),
+	reply_page('Sidebar',
+		   [ p(file),
+		     p(\files(Files))
+		   ]).
+
+documented_file(File) :-
+	pldoc_comment(_, File:_, _, _).
+
+files([]) -->
+	[].
+files([H|T]) -->
+	file(H),
+	files(T).
+
+file(File) -->
+	{ www_form_encode(File, Enc),
+	  format(string(FileRef), '/file?file=~w', [Enc]),
+	  file_base_name(File, Base),
+	  file_directory_name(File, Path),
+	  file_base_name(Path, Parent),
+	  format(string(Title), '.../~w/~w', [Parent, Base])
+	},
+	html([ a([target=main, href=FileRef], Title),
+	       br([])
+	     ]).
+
+
+%	file?file=REF
+%	
+%	Reply using documentation of file
+
+reply('/file', Request) :-
+	http_parameters(Request,
+			[ file(File, [])
+			]),
+	format('Content-type: text/html~n~n'),
+	doc_for_file(File, current_output, []).
+
+
+%	/welcome.html
+%	
+%	Initial empty page
+
+
+reply('/welcome.html', _Request) :- !,
+	reply_page('Welcome', []).
+
+
+%	/pldoc.css
+%	
+%	Reply the documentation style-sheet
+
+reply('/pldoc.css', _Request) :-
+	reply_file('pldoc.css').
+
+
+		 /*******************************
+		 *	       UTIL		*
+		 *******************************/
+
+reply_page(Title, Content) :-
+	phrase(page(title(Title), Content), HTML),
+	format('Content-type: text/html~n~n'),
+	print_html(HTML).
+
+reply_file(File) :-
+	absolute_file_name(File, Path, [access(read)]),
+	file_mime_type(Path, MimeType),
+	throw(http_reply(file(MimeType, Path))).
+
+
+                 /*******************************
+                 *        PCEEMACS SUPPORT      *
+                 *******************************/
+
+:- multifile
+        emacs_prolog_colours:goal_colours/2,
+        prolog:called_by/2.
+
+
+emacs_prolog_colours:goal_colours(reply_page(_, HTML),
+                                  built_in-[classify, Colours]) :-
+        catch(html_write:html_colours(HTML, Colours), _, fail).
+
+prolog:called_by(reply_page(_, HTML), Called) :-
+        catch(phrase(html_write:called_by(HTML), Called), _, fail).
