47407c7375b784bcef9d1b703a7c747ccf9a358c
Jan Wielemaker <wielemak@science.uva.nl>
2006-12-01 20:43:24 +0000

    * Test gzip interaction

--- packages/zlib/ztest.pl
+++ packages/zlib/ztest.pl
@@ -9,16 +9,29 @@
 :- use_module(user:library(plunit)).
 :- use_module(user:library(readutil)).
 
+read_file_to_codes(File, Codes) :-
+	open(File, read, In),
+	call_cleanup(read_stream_to_codes(In, Codes), close(In)).
+
 :- begin_tests(zlib).
 
 test(gunzip,
-     [ setup(shell('gzip < ztest.pl > plunit-tmp.gz'))%,
-       %cleanup(delete_file('plunit-tmp.gz'))
+     [ setup(shell('gzip < ztest.pl > plunit-tmp.gz')),
+       cleanup(delete_file('plunit-tmp.gz'))
      ]) :-
 	gzopen('plunit-tmp.gz', read, ZIn),
 	call_cleanup(read_stream_to_codes(ZIn, Codes0), close(ZIn)),
-	open('ztest.pl', read, In),
-	call_cleanup(read_stream_to_codes(In, Codes1), close(In)),
+	read_file_to_codes('ztest.pl', Codes1),
 	Codes0 == Codes1.
 	
+test(gzip,
+     [ cleanup(delete_file('plunit-tmp.gz'))
+     ]) :-
+	read_file_to_codes('ztest.pl', Codes),
+	gzopen('plunit-tmp.gz', write, ZOut),
+	format(ZOut, '~s', [Codes]),
+	close(ZOut),
+	read_file_to_codes(pipe('gunzip < plunit-tmp.gz'), Codes1),
+	Codes == Codes1.
+
 :- end_tests(zlib).
