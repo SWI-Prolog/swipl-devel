b1a82d1cc07680ff069e050f1c510053656419a5
Jan Wielemaker <wielemak@science.uva.nl>
2006-12-08 15:58:43 +0000

    * Added parse_time/2

--- library/date.pl
+++ library/date.pl
@@ -30,7 +30,8 @@
 */
 
 :- module(date,
-	  [ date_time_value/3		% Extract values from a date/time struct
+	  [ date_time_value/3,		% ?Field, ?DaTime, ?Value
+	    parse_time/2		% +Date, -Stamp
 	  ]).
 
 %%	date_time_value(?Field:atom, +Struct:datime, -Value) is nondet.
@@ -60,3 +61,73 @@ date_time_value(daylight_saving, date(_,_,_,_,_,_,_,_,D), D) :- D \== (-).
 
 date_time_value(date,		 date(Y,M,D,_,_,_,_,_,_), date(Y,M,D)).
 date_time_value(time,		 date(_,_,_,H,M,S,_,_,_), time(H,M,S)).
+
+%%	parse_time(+Text, -Stamp) is det.
+%
+%	Stamp is a timestamp created from   parsing Text. Currently only
+%	deals with RFC1123 from the HTTP protocol.
+
+parse_time(Text, Stamp) :-
+	atom_codes(Text, Codes),
+	phrase(date(Y,Mon,D,H,Min,S,UTCOffset), Codes),
+	date_time_stamp(date(Y,Mon,D,H,Min,S,UTCOffset,-,-), Stamp).
+
+% RFC 1123: "Fri, 08 Dec 2006 15:29:44 GMT"
+date(Y,Mon,D,H,Min,S,0) -->
+	day_name(_), ",", ws,
+	day_of_the_month(D), ws,
+	month_name(Mon), ws,
+	year(Y), ws,
+	hour(H), ":", minute(Min), ":", second(S), ws,
+	(   "GMT"
+	->  []
+	;   []
+	).
+
+day_name(1) --> "Sun".
+day_name(2) --> "Mon".
+day_name(3) --> "Tue".
+day_name(4) --> "Wed".
+day_name(5) --> "Thu".
+day_name(6) --> "Fri".
+day_name(7) --> "Sat".
+
+month_name(1) --> "Jan".
+month_name(2) --> "Feb".
+month_name(3) --> "Mar".
+month_name(4) --> "Apr".
+month_name(5) --> "May".
+month_name(6) --> "Jun".
+month_name(7) --> "Jul".
+month_name(8) --> "Aug".
+month_name(9) --> "Sep".
+month_name(10) --> "Oct".
+month_name(11) --> "Nov".
+month_name(12) --> "Dec".
+
+day_of_the_month(N) --> int2digit(N), { between(1, 31, N) }.
+hour(N)             --> int2digit(N), { between(0, 23, N) }.
+minute(N)	    --> int2digit(N), { between(0, 59, N) }.
+second(N)           --> int2digit(N), { between(0, 60, N) }. % leap second
+
+int2digit(N) -->
+	digit(D0),
+	digit(D1),
+	{ N is D0*10+D1 }.
+	  
+year(Y) -->
+	digit(D0),
+	digit(D1),
+	digit(D2),
+	digit(D3),
+	{ Y is D0*1000+D1*100+D2*10+D3 }.
+
+digit(D) -->
+	[C],
+	{ code_type(C, digit(D)) }.
+
+ws -->
+	" ", !,
+	ws.
+ws -->
+	[].
