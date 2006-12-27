74fc5539ac431f149f25e6cb3f63f63fbc331b5f
Jan Wielemaker <wielemak@science.uva.nl>
2006-12-27 15:34:32 +0000

    * float_fractional_part for integers

--- src/pl-arith.c
+++ src/pl-arith.c
@@ -2209,8 +2209,9 @@ ar_float_fractional_part(Number n1, Number r)
 #ifdef O_GMP
     case V_MPZ:
 #endif
-      cpNumber(r, n1);
-      break;
+      r->value.i = 0;
+      r->type = V_INTEGER;
+      succeed;
 #ifdef O_GMP
     case V_MPQ:
       r->type = V_MPQ;
