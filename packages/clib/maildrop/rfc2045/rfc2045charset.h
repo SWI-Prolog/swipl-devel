a8e3bfb0e6cdffd7ef51f6549dad98652c480685
Jan Wielemaker <wielemak@science.uva.nl>
2007-01-13 19:33:30 +0000

    * Generated file

---
+++ packages/clib/maildrop/rfc2045/rfc2045charset.h
@@ -0,0 +1,10 @@
+/*
+** Copyright 1998 - 1999 Double Precision, Inc.  See COPYING for
+** distribution information.
+*/
+
+/* $Id$ */
+
+#define	RFC2045CHARSET	"us-ascii"
+
+#define	RFC2045MIMEMSG	"This is a MIME-formatted message.  If you see this text it means that your\nE-mail software does not support MIME-formatted messages.\n"
