6fc4e1fc00f636e65354d61c76501b146b2b7ab6
Jan Wielemaker <wielemak@science.uva.nl>
2007-05-11 09:20:43 +0000

    * No longer big argument stack after -G

--- src/pl-main.c
+++ src/pl-main.c
@@ -474,9 +474,7 @@ parseCommandLineOptions(int argc0, char **argv, int *compile)
 
 	  switch(*s)
 	  { case 'L':	GD->options.localSize    = size; goto next;
-	    case 'G':	GD->options.globalSize   = size;
-	    		GD->options.argumentSize = size/4;
-							 goto next;
+	    case 'G':	GD->options.globalSize   = size; goto next;
 	    case 'T':	GD->options.trailSize    = size; goto next;
 	    case 'A':	GD->options.argumentSize = size; goto next;
 	    case 'H':	GD->options.heapSize     = size; goto next;
