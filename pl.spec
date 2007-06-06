2a39a50292093fc17e61cc4d7aa79413aed7634c
Jan Wielemaker <wielemak@science.uva.nl>
2007-06-06 10:05:56 +0000

    * Preparing 5.6.35

--- pl.spec
+++ pl.spec
@@ -1,5 +1,5 @@
 Name:		pl
-Version:	5.6.34
+Version:	5.6.35
 Release:	200
 Epoch:		0
 Summary:	A Free Software Prolog compiler.
--- src/pl-itf.h
+++ src/pl-itf.h
@@ -55,7 +55,7 @@ before loading this file.  See end of this file.
 /* PLVERSION: 10000 * <Major> + 100 * <Minor> + <Patch> */
 
 #ifndef PLVERSION
-#define PLVERSION 50634
+#define PLVERSION 50635
 #endif
 
 		 /*******************************
