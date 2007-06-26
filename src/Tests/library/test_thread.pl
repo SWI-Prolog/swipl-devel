f69a8aa3cfca0031fe173206f4fa7645d8a7cd17
Jan Wielemaker <wielemak@science.uva.nl>
2007-06-26 08:17:07 +0000

    * Tests for library(thread)

---
+++ src/Tests/library/test_thread.pl
@@ -0,0 +1,31 @@
+:- module(test_thread, [test_thread/0]).
+
+test_thread :-
+	run_tests(thread).
+
+:- use_module(library(plunit)).
+:- use_module(library(thread)).
+
+:- begin_tests(thread).
+
+test(true, true) :-
+	concurrent(2, [true], []).
+test(unify, true(A==3)) :-
+	concurrent(2, [A=3], []).
+test(unify, true([A,B]==[3,4])) :-
+	concurrent(2, [A=3, B = 4], []).
+test(fail, fail) :-
+	concurrent(2, [_A=3, fail, _B = 4], []).
+test(error, throws(x)) :-
+	concurrent(2, [_A=3, throw(x), _B = 4], []).
+
+test(first, true(X==1)) :-
+	first_solution(X, [X=1,X=1], []).
+test(first, fail) :-
+	first_solution(X, [fail,X=1], []).
+test(first, true(X==1)) :-
+	first_solution(X, [fail,X=1], [on_fail(continue)]).
+test(first, true(X==1)) :-
+	first_solution(X, [(repeat,fail), X=1], []).
+
+:- end_tests(thread).
