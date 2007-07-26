75d2d8c3d9949e0549dff55f648b211e03821e83
Jan Wielemaker <wielemak@science.uva.nl>
2007-07-26 19:06:51 +0000

    * Test is too slow on some hardware

--- src/Tests/library/test_thread.pl
+++ src/Tests/library/test_thread.pl
@@ -19,7 +19,7 @@ test(fail, fail) :-
 test(error, throws(x)) :-
 	concurrent(2, [_A=3, throw(x), _B = 4], []).
 test(concur, true) :-
-	forall(between(0, 200, _),
+	forall(between(0, 20, _),
 	       (   concurrent(2, [X=1,Y=2], []),
 		   ground(X-Y))).
 
