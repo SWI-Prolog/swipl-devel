6b4f7cec64326b24a02aa2f044e6321a249ca56c
Jan Wielemaker <wielemak@science.uva.nl>
2007-08-22 08:45:34 +0000

    * Fix license

--- library/clp/clpfd.pl
+++ library/clp/clpfd.pl
@@ -1,8 +1,35 @@
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-   CLP(FD) - Finite domain constraint solver.
-   Copyright (C) 2007 Markus Triska (triska@gmx.at)
-   Licence: GPL
+/*  $Id$
 
+    Part of SWI-Prolog
+
+    Author:        Markus Triska
+    E-mail:        triska@gmx.at
+    WWW:           http://www.swi-prolog.org
+    Copyright (C): 2007, Markus Triska
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    As a special exception, if you link this library with other files,
+    compiled with a Free Software compiler, to produce an executable, this
+    library does not by itself cause the resulting executable to be covered
+    by the GNU General Public License. This exception does not however
+    invalidate any other reasons why the executable file might be covered by
+    the GNU General Public License.
+*/
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    Thanks to Tom Schrijvers for his "bounds.pl", the first finite
    domain constraint solver included with SWI-Prolog. I've learned a
    lot from it and could even use some of the code for this solver.
