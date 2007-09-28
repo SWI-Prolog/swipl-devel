bae14d307f1c187ac74d985d5e855b3e5b5b6d04
Jan Wielemaker <wielemak@science.uva.nl>
2007-09-28 13:42:13 +0000

    * Updated about box

--- packages/xpce/prolog/lib/man/v_manual.pl
+++ packages/xpce/prolog/lib/man/v_manual.pl
@@ -546,13 +546,13 @@ help_on_prolog(_M) :->
 		********************************/
 
 about([ 'XPCE version %s'+[@pce?version]-boldhuge,
-	'Copyright 1992-2003, University of Amsterdam',
+	'Copyright 1992-2007, University of Amsterdam',
 	'XPCE comes with ABSOLUTELY NO WARRANTY.',
 	'This is free software (LGPL), and you are welcome to',
 	'redistribute it under certain conditions.',
 	url('http://www.swi-prolog.org/packages/xpce/'),
 	'Jan Wielemaker\nAnjo Anjewierden'-italic,
-	'SWI\nUniversity of Amsterdam\nRoetersstraat 15\n1018 WB  Amsterdam\nThe Netherlands'
+	'HCS\nUniversity of Amsterdam\nKruislaan 419\n1098 VA  Amsterdam\nThe Netherlands'
       ]).
 
 
