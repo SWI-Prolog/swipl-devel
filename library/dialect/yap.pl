83d90d84875fe0e300c121bae26d47b5ffd5a0f9
Jan Wielemaker <wielemak@science.uva.nl>
2007-09-29 19:35:12 +0000

    * Partially filled

--- library/dialect/yap.pl
+++ library/dialect/yap.pl
@@ -30,12 +30,24 @@
 */
 
 :- module(yap,
-	  [ 
+	  [ gc/0,
+	    depth_bound_call/2,		% :Goal, +Limit
+	    system/1,			% +Command
+	    exists/1,			% +File
+	    assert_static/1,		% :Term
+	    false/0
 	  ]).
 
 /** <module> YAP Compatibility module
 
-This module provides compatibility to YAP.  It must:
+This  module  provides  compatibility  to   YAP  through  the  directive
+expects_dialect/1:
+
+	==
+	:- expects_dialect(yap)
+	==
+
+The task of this module is:
 
 	* Implement system predicates available in YAP we do not yet or
 	do not wish to support in SWI-Prolog.  Export these predicates.
@@ -48,6 +60,14 @@ This module provides compatibility to YAP.  It must:
 	* Alter the library search path, placing dialect/yap *before*
 	the system libraries.
 
+	* Allow for =|.yap|= extension as extension for Prolog files.
+	If both a =|.pl|= and =|.yap|= is present, the =|.yap|= file
+	is loaded if the current environment expects YAP.
+
+Current            set            is              taken             from
+http://www.david-reitter.com/compling/prolog/compat_swi.pl,  written  by
+David Reitter and Steve Moyle
+
 @tbd	Fill it in!
 @author Jan Wielemaker
 */
@@ -59,10 +79,12 @@ This module provides compatibility to YAP.  It must:
 :- multifile
 	user:goal_expansion/2,
 	user:file_search_path/2,
+	user:prolog_file_type/2,
 	yap_expansion/2.
 :- dynamic
 	user:goal_expansion/2,
-	user:file_search_path/2.
+	user:file_search_path/2,
+	user:prolog_file_type/2.
 
 user:goal_expansion(In, Out) :-
 	prolog_load_context(dialect, yap),
@@ -70,7 +92,18 @@ user:goal_expansion(In, Out) :-
 
 %%	yap_expansion(+In, +Out)
 %
-%	goal_expansion rules to emulate YAP behaviour in SWI-Prolog.
+%	goal_expansion rules to emulate YAP behaviour in SWI-Prolog. The
+%	expansions  below  maintain  optimization    from   compilation.
+%	Defining them as predicates would loose compilation.
+
+yap_expansion(eval_arith(Expr, Result),
+	      Result is Expr).
+yap_expansion(if(Goal, Then),
+	      (Goal *-> Then; true)).
+yap_expansion(if(Goal, Then, Else),
+	      (Goal *-> Then; Else)).
+yap_expansion(false, fail).
+
 
 
 		 /*******************************
@@ -95,4 +128,86 @@ push_yap_library :-
 	;   true
 	).
 
-:- push_yap_library.
+
+%%	push_yap_file_extension
+%
+%	Looks for .yap files before looking for .pl files if the current
+%	dialect is =yap=.
+
+push_yap_file_extension :-
+	asserta((user:prolog_file_type(yap, prolog) :-
+		    prolog_load_context(dialect, yap))).
+
+:- push_yap_library,
+   push_yap_file_extension.
+
+
+		 /*******************************
+		 *	 SYSTEM PREDICATES	*
+		 *******************************/
+
+%%	gc
+%
+%	Garbage collect.
+%	
+%	@compat yap
+
+gc :-
+	garbage_collect.
+
+%%	depth_bound_call(:Goal, :Limit)
+%
+%	Equivalent to call_with_depth_limit(Goal, Limit, _Reached)
+%	
+%	@compat yap
+
+:- module_transparent
+	depth_bound_call/2.
+
+depth_bound_call(G, L) :- 
+	call_with_depth_limit(G, L, _).
+
+%%	system(+Command)
+%
+%	Equivalent to shell(Command).
+%	
+%	@compat yap
+
+system(Command) :-
+	shell(Command).
+
+%%	exists(+File)
+%
+%	Equivalent to exists_file(File).
+%	
+%	@compat yap
+
+exists(File) :-
+	exists_file(File).
+
+%%	assert_static(:Term)
+%
+%	Assert    as    static    predicate.      SWI-Prolog    provides
+%	compile_predicates/1 to achieve this. The   emulation  is a mere
+%	alias for assert/1, as  immediate   compilation  would  prohibit
+%	further calls to this predicate.
+%	
+%	@compat yap
+%	@deprecated Use assert/1 and compile_predicates/1 after
+%	completing the predicate definition.
+
+:- module_transparent
+	assert_static/1.
+
+assert_static(Term) :-
+	assert(Term).
+
+
+%%	false
+%
+%	Equivalent to fail/0
+%	
+%	@compat yap
+
+false :-
+	fail.
