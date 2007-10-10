a671a865ab90582d0a9eafac3390e58e93193cd4
Jan Wielemaker <wielemak@science.uva.nl>
2007-10-10 21:24:18 +0200

    listen/[2,3] to properly use goal_expansion/2

--- library/broadcast.pl
+++ library/broadcast.pl
@@ -165,10 +165,13 @@ canonical_listener(Templ, Templ).
 		 *	  GOAL EXPANSION	*
 		 *******************************/
 
-goal_expansion(listen(L,T,G0), listen(L,T,G)) :-
+:- multifile
+	user:goal_expansion/2.
+
+user:goal_expansion(listen(L,T,G0), listen(L,T,G)) :-
 	expand_goal(G0, G).
-goal_expansion(listen(T,G0), listen(T,G)) :-
+user:goal_expansion(listen(T,G0), listen(T,G)) :-
 	expand_goal(G0, G).
-goal_expansion(unlisten(L,T,G0), unlisten(L,T,G)) :-
+user:goal_expansion(unlisten(L,T,G0), unlisten(L,T,G)) :-
 	expand_goal(G0, G).
 
