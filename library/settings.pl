73ccd9d25d39092ad883f1c7203af44bac4ace15
Jan Wielemaker <wielemak@science.uva.nl>
2007-10-11 20:56:46 +0200

    * library(settings): Properly remove old values when (re-)loading a file

--- library/settings.pl
+++ library/settings.pl
@@ -1,3 +1,34 @@
+/*  $Id$
+
+    Part of SWI-Prolog
+
+    Author:        Jan Wielemaker
+    E-mail:        wielemak@science.uva.nl
+    WWW:           http://www.swi-prolog.org
+    Copyright (C): 1985-2007, University of Amsterdam
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    As a special exception, if you link this library with other files,
+    compiled with a Free Software compiler, to produce an executable, this
+    library does not by itself cause the resulting executable to be covered
+    by the GNU General Public License. This exception does not however
+    invalidate any other reasons why the executable file might be covered by
+    the GNU General Public License.
+*/
+
 :- module(settings,
 	  [ setting/4,			% :Name, +Type, +Default, +Comment, +Src
 	    setting/2,			% :Name, ?Value
@@ -399,13 +430,15 @@ store_setting(setting(Module:Name, Value), _) :-
 	eval_default(Default0, Module, Type, Default),
 	(   Value == Default
 	->  true
-	;   check_type(Type, Value),
+	;   check_type(Type, Value)
+	->  retractall(value(Name, Module, _)),
 	    assert(value(Name, Module, Value)),
 	    broadcast(settings(changed(Module:Name, Default, Value)))
 	).
 store_setting(setting(Module:Name, Value), Options) :- !,
 	(   option(undefined(load), Options, load)
-	->  assert(value(Name, Module, Value))
+	->  retractall(value(Name, Module, _)),
+	    assert(value(Name, Module, Value))
 	;   existence_error(setting, Module:Name)
 	).
 store_setting(Term, _) :-
