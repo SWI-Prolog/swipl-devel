66406541b002ba228ac8c252cc057df8190272f4
Tom Schrijvers <tom.schrijvers@cs.kuleuven.be>
2008-02-24 17:13:49 +0100

    minor CHR updates

--- packages/chr/chr_compiler_options.pl
+++ packages/chr/chr_compiler_options.pl
@@ -338,7 +338,7 @@ chr_pp_flag_definition(toplevel_show_store,[on,off]).
 chr_pp_flag_definition(term_indexing,[off,on]).
 chr_pp_flag_definition(verbosity,[on,off]).
 chr_pp_flag_definition(ht_removal,[off,on]).
-chr_pp_flag_definition(mixed_stores,[off,on]).
+chr_pp_flag_definition(mixed_stores,[on,off]).
 chr_pp_flag_definition(line_numbers,[off,on]).
 chr_pp_flag_definition(dynattr,[off,on]).
 chr_pp_flag_definition(experiment,[off,on]).
