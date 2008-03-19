bb7c3fc20369142f4724c8a8719aa81bb70b1ae1
Jan Wielemaker <wielemak@science.uva.nl>
2008-03-19 16:27:52 +0100

    Got basic parsing working

--- packages/uri/parse.c
+++ packages/uri/parse.c
@@ -24,9 +24,10 @@
 
 
 #include <SWI-Prolog.h>
+#include <stdlib.h>
 #include <uriparser/Uri.h>
 
-static functor_t FUNCTOR_uri8;
+static functor_t FUNCTOR_uri7;
 static atom_t    ATOM_;
 
 static void
@@ -35,6 +36,54 @@ put_text_range(term_t t, UriTextRangeW *text)
     PL_unify_wchars(t, PL_ATOM, text->afterLast - text->first, text->first);
   else
     PL_put_atom(t, ATOM_);
+}
+
+
+static void
+put_num_text_range(term_t t, UriTextRangeW *text)
+{ if ( text->afterLast > text->first )
+  { unsigned long v;
+    wchar_t *e;
+
+    v = wcstoul(text->first, &e, 10);
+    if ( e == text->afterLast )
+      PL_put_integer(t, v);
+    else
+      PL_unify_wchars(t, PL_ATOM, text->afterLast - text->first, text->first);
+  }
+}
+
+
+
+static void
+put_path_segments(term_t t, UriPathSegmentW *segment)
+{ size_t len = 0;
+  UriPathSegmentW *s;
+  wchar_t tmp[256];
+  wchar_t *buf, *o;
+
+  for(s=segment; s; s=s->next)
+  { len += (s->text.afterLast -  s->text.first) + 1;
+  }
+  if ( len == 0 )
+    len = 1;				/* single / */
+
+  if ( len < 256 )
+    buf = tmp;
+  else
+    buf = PL_malloc((len+1)*sizeof(wchar_t));
+
+  for(o=buf,s=segment; s; s=s->next)
+  { size_t l = s->text.afterLast -  s->text.first;
+
+    *o++ = '/';
+    wcsncpy(o, s->text.first, l);
+    o+= l;
+  }
+  if ( o == buf )
+    *o++ = '/';
+
+  PL_unify_wchars(t, PL_ATOM, o-buf, buf);
 }
 
 
@@ -54,11 +103,17 @@ parse_uri(term_t text, term_t parts)
     return FALSE;			/* TBD: Exception */
   }
   
-  term_t av = PL_new_term_refs(8);
+  term_t av = PL_new_term_refs(7);
   put_text_range(av+0, &uri.scheme);
+  put_text_range(av+1, &uri.userInfo);
+  put_text_range(av+2, &uri.hostText);
+  put_num_text_range(av+3, &uri.portText);
+  put_path_segments(av+4, uri.pathHead);
+  put_text_range(av+5, &uri.query);
+  put_text_range(av+6, &uri.fragment);
 
   term_t f = PL_new_term_ref();
-  PL_cons_functor_v(f, FUNCTOR_uri8, av);
+  PL_cons_functor_v(f, FUNCTOR_uri7, av);
 
   uriFreeUriMembersW(&uri);
 
@@ -68,7 +123,7 @@ parse_uri(term_t text, term_t parts)
 
 static void
 install_parse()
-{ FUNCTOR_uri8 = PL_new_functor(PL_new_atom("uri"), 8);
+{ FUNCTOR_uri7 = PL_new_functor(PL_new_atom("uri"), 7);
   ATOM_ = PL_new_atom("");
 
   PL_register_foreign("parse_uri", 2, parse_uri, 0);
