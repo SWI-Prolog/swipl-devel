43379291fa76bcd0955b977bfea207ceead0c095
Jan Wielemaker <wielemak@science.uva.nl>
2008-04-15 22:27:23 +0200

    Use byte-order independent version on big-endian systems

--- src/pl-hash.c
+++ src/pl-hash.c
@@ -9,6 +9,50 @@
 
 #include <pl-incl.h>
 
+#if WORDS_BIGENDIAN
+
+unsigned int
+MurmurHashAligned2(const void * key, int len, unsigned int seed)
+{ const unsigned int m = 0x5bd1e995;
+  const int r = 24;
+  unsigned int h = seed ^ len;
+  const unsigned char * data = (const unsigned char *)key;
+
+  while( len >= 4 )
+  { unsigned int k;
+
+    k  = data[0];
+    k |= data[1] << 8;
+    k |= data[2] << 16;
+    k |= data[3] << 24;
+
+    k *= m;
+    k ^= k >> r;
+    k *= m;
+    
+    h *= m;
+    h ^= k;
+    
+    data += 4;
+    len -= 4;
+  }
+
+  switch( len )
+  { case 3: h ^= data[2] << 16;
+    case 2: h ^= data[1] << 8;
+    case 1: h ^= data[0];
+      h *= m;
+  };
+
+  h ^= h >> 13;
+  h *= m;
+  h ^= h >> 15;
+
+  return h;
+}
+
+#else /*WORDS_BIGENDIAN*/
+
 #define MIX(h,k,m) { k *= m; k ^= k >> r; k *= m; h *= m; h ^= k; }
 
 unsigned int
@@ -115,3 +159,5 @@ MurmurHashAligned2(const void *key, int len, unsigned int seed)
     return h;
   }
 }
+
+#endif /*WORDS_BIGENDIAN*/
