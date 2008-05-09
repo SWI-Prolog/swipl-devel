b65a9a4db0469dc46bd0fe182b1522600c54480c
Jan Wielemaker <wielemak@science.uva.nl>
2008-05-09 22:23:01 +0200

    Delete traces of limit_stack/2

--- src/pl-funcs.h
+++ src/pl-funcs.h
@@ -723,7 +723,6 @@ COMMON(void) 		emptyStacks(void);
 COMMON(void) 		freeStacks(ARG1_LD);
 COMMON(void) 		freeLocalData(PL_local_data_t *ld);
 COMMON(word) 		pl_trim_stacks(void);
-COMMON(word) 		pl_limit_stack(term_t s, term_t l);
 COMMON(word) 		pl_stack_parameter(term_t s, term_t k, term_t o, term_t n);
 COMMON(void) 		ensureRoomStack(Stack s, size_t n);
 COMMON(int) 		_PL_get_signum(term_t sig, int *n);
