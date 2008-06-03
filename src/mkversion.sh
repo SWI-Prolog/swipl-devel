ec71bf60960eface81c18ae84f2ee631872cc87f
Jan Wielemaker <wielemak@science.uva.nl>
2008-06-03 16:52:15 +0200

    Small enhancement

--- src/mkversion.sh
+++ src/mkversion.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cat <<_EOM_
+cat >> version.h.new <<_EOM_
 /*  Generate file that contains the version identifier from
     the "git describe" command.
 
@@ -13,6 +13,16 @@ gitversion=''
 if v=`git describe 2>/dev/null`; then
     if [ ! -z $v ]; then
 	gitversion=`echo $v | sed 's/^V//'`
-	echo "#define GIT_VERSION "'"'$gitversion'"'
+	echo "#define GIT_VERSION "'"'$gitversion'"' >> version.h.new
     fi
+fi
+
+if [ ! -r version.h ]; then
+    mv version.h.new version.h
+elif cmp version.h version.h.new >/dev/null; then
+    echo "No GIT version change"
+    rm version.h.new
+else
+    echo "Updated GIT version"
+    mv version.h.new version.h
 fi
