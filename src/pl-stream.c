756e9e88dabdb6a2b1ce98a825336784a3d46dce
Jan Wielemaker <wielemak@science.uva.nl>
2008-06-04 11:05:07 +0200

    FIXED: Setting buffer size on input streams

--- src/pl-stream.c
+++ src/pl-stream.c
@@ -160,26 +160,69 @@ not needed.
 
 static int
 S__setbuf(IOSTREAM *s, char *buffer, int size)
-{ if ( size == 0 )
+{ char *newbuf, *newunbuf;
+  int newflags = s->flags;
+
+  if ( size == 0 )
     size = SIO_BUFSIZE;
 
-  S__removebuf(s);
-  s->bufsize = size;
+  if ( (s->flags & SIO_OUTPUT) )
+  { if ( S__removebuf(s) < 0 )
+      return -1;
+  }
 
   if ( buffer )
-  { s->unbuffer = s->buffer = buffer;
-    s->flags |= SIO_USERBUF;
+  { newunbuf = newbuf = buffer;
+    newflags |= SIO_USERBUF;
   } else
-  { if ( !(s->unbuffer = malloc(s->bufsize+UNDO_SIZE)) )
+  { if ( !(newunbuf = malloc(size+UNDO_SIZE)) )
     { errno = ENOMEM;
       return -1;
     }
-    s->flags &= ~SIO_USERBUF;
-    s->buffer = s->unbuffer + UNDO_SIZE;
+    newflags &= ~SIO_USERBUF;
+    newbuf = newunbuf + UNDO_SIZE;
   }
 
-  s->limitp = &s->buffer[s->bufsize];
-  s->bufp   = s->buffer;
+  if ( (s->flags & SIO_INPUT) )
+  { int buffered = s->limitp - s->bufp;
+    int copy = (buffered < size ? buffered : size);
+
+    if ( size < buffered )
+    { int offset = size - buffered;
+      int64_t newpos;
+
+      if ( s->functions->seek64 )
+      { newpos = (*s->functions->seek64)(s->handle, offset, SIO_SEEK_CUR);
+      } else if ( s->functions->seek )
+      { newpos = (*s->functions->seek)(s->handle, offset, SIO_SEEK_CUR);
+      } else
+      { newpos = -1;
+	errno = ESPIPE;
+      }
+
+      if ( newpos == -1 )
+      { if ( !(newflags & SIO_USERBUF) )
+	{ int oldeno = errno;
+
+	  free(newunbuf);
+	  errno = oldeno;
+	  return -1;
+	}
+      }
+    }
+
+    memcpy(newbuf, s->bufp, copy);
+    S__removebuf(s);
+    s->unbuffer = newunbuf;
+    s->bufp = s->buffer = newbuf;
+    s->limitp = s->buffer+copy;
+  } else
+  { s->unbuffer = newunbuf;
+    s->bufp = s->buffer = newbuf;
+    s->limitp = &s->buffer[size];
+  }
+  s->bufsize = size;
+  s->flags = newflags;
 
   return size;
 }
