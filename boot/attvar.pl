9b3d1cfadb305074b2ed2c6047a8c14f53ef446a
Markus Triska <markus.triska@gmx.at>
2008-06-06 10:31:00 +0200

    Revert "in residual goals, match order of term variables more closely"
    This reverts commit f2029b4caf3599b34862580cae3f2deb3056e1c9.
    
    Walking attributes in this way may lose some of them.

--- boot/attvar.pl
+++ boot/attvar.pl
@@ -208,28 +208,9 @@ call_det(Goal, Det) :-
 %    defined in the modules the attributes stem from, is used to
 %    convert attributes to lists of goals.
 
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-   First collect attributes of variables directly involved in Term, then
-   walk the variables in their attributes.
-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-
 copy_term(Term, Copy, Gs) :-
 	term_variables(Term, Vs),
-	collect_primaries(Vs, Gs0, Gs1),
-	findall(Term-Gs0, phrase(collect_attributes(Vs,[]),Gs1), [Copy-Gs]).
-
-collect_primaries([])	  --> [].
-collect_primaries([V|Vs]) -->
-	(   { get_attrs(V, As) }
-	->  collect_primaries_(As, V)
-	;   []
-	),
-	collect_primaries(Vs).
-
-collect_primaries_([], _)		    --> [].
-collect_primaries_(att(Module,Value,As), V) -->
-	collect_single(V, Module, Value),
-	collect_primaries_(As, V).
+	findall(Term-GsC, phrase(collect_attributes(Vs,[]),GsC), [Copy-Gs]).
 
 collect_attributes([], _)	  --> [].
 collect_attributes([V|Vs], Tabu0) -->
@@ -247,10 +228,6 @@ collect_([], _, _)			--> [].
 collect_(att(Module,Value,As), V, Tabu) -->
 	{ term_variables(Value, Vs) },
 	collect_attributes(Vs, Tabu),
-	collect_single(V, Module, Value),
-	collect_(As, V, Tabu).
-
-collect_single(V, Module, Value) -->
 	(   { Module == freeze }
 	->  [freeze(V, Value)]
 	;   { current_predicate(Module:attribute_goals/3) }
@@ -261,7 +238,8 @@ collect_single(V, Module, Value) -->
 	    dot_list(Goal)
 	;   [put_attr(V, Module, Value)]
 	),
-	{ del_attr(V, Module) }.
+	{ del_attr(V, Module) },
+	collect_(As, V, Tabu).
 
 dlist([])     --> [].
 dlist([L|Ls]) --> [L], dlist(Ls).
