f20668195e2c8dba1137c49ca029dbd1151182fa
Markus Triska <markus.triska@gmx.at>
2008-06-18 19:25:57 +0200

    #\=/2 instead of #>/2 in example

--- library/clp/clpfd.pl
+++ library/clp/clpfd.pl
@@ -193,7 +193,7 @@ puzzle([S,E,N,D] + [M,O,R,E] = [M,O,N,E,Y]) :-
                   S*1000 + E*100 + N*10 + D +
                   M*1000 + O*100 + R*10 + E #=
         M*10000 + O*1000 + N*100 + E*10 + Y,
-        M #> 0, S #> 0.
+        M #\= 0, S #\= 0.
 ==
 
 Sample query and its result:
