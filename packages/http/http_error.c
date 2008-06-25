27217b9c03269cd1ab42fe12f1e6d00117579bfe
Jan Wielemaker <wielemak@science.uva.nl>
2008-06-25 11:52:07 +0200

    Added existence_error

--- packages/http/http_error.c
+++ packages/http/http_error.c
@@ -31,6 +31,7 @@ static functor_t FUNCTOR_error2;	/* error(Formal, Context) */
 static functor_t FUNCTOR_type_error2;	/* type_error(Term, Expected) */
 static functor_t FUNCTOR_domain_error2;	/* domain_error(Term, Expected) */
 static functor_t FUNCTOR_permission_error3; /* permission_error(Op, Type, Term) */
+static functor_t FUNCTOR_existence_error2; /* existence_error(Type, Term) */
 static int debuglevel = 0;
 
 #define MKFUNCTOR(name, arity) PL_new_functor(PL_new_atom(name), arity)
@@ -75,6 +76,20 @@ domain_error(term_t actual, const char *domain)
   PL_unify_term(ex, PL_FUNCTOR, FUNCTOR_error2,
 		      PL_FUNCTOR, FUNCTOR_domain_error2,
 		        PL_CHARS, domain,
+		        PL_TERM, actual,
+		      PL_VARIABLE);
+
+  return PL_raise_exception(ex);
+}
+
+
+static int
+existence_error(term_t actual, const char *type)
+{ term_t ex = PL_new_term_ref();
+
+  PL_unify_term(ex, PL_FUNCTOR, FUNCTOR_error2,
+		      PL_FUNCTOR, FUNCTOR_existence_error2,
+		        PL_CHARS, type,
 		        PL_TERM, actual,
 		      PL_VARIABLE);
 
@@ -132,6 +147,7 @@ init_errors()
 { FUNCTOR_error2            = MKFUNCTOR("error", 2);
   FUNCTOR_type_error2	    = MKFUNCTOR("type_error", 2);
   FUNCTOR_domain_error2     = MKFUNCTOR("domain_error", 2);
+  FUNCTOR_existence_error2  = MKFUNCTOR("existence_error", 2);
   FUNCTOR_permission_error3 = MKFUNCTOR("permission_error", 3);
 
 #ifdef O_DEBUG
