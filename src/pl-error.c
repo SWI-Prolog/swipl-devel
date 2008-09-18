ebe38d546e69d260dc57302e78d232a245cb74b7
Jan Wielemaker <wielemak@science.uva.nl>
2008-09-18 12:23:46 +0200

    Fixed for Windows

--- src/pl-error.c
+++ src/pl-error.c
@@ -83,8 +83,8 @@ PL_error(const char *pred, int arity, const char *msg, int id, ...)
       break;
     case ERR_MUST_BE_VAR:
     { int argn = va_arg(args, int);
-      /*term_t bound =*/ va_arg(args, term_t);
       char buf[50];
+      /*term_t bound =*/ va_arg(args, term_t);
 
       if ( !msg )
       { Ssprintf(buf, "%d-%s argument",
