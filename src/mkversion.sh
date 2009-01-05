bbb302bdcff307c6dabcd8509a714416d997fceb
Jan Wielemaker <J.Wielemaker@uva.nl>
2009-01-05 10:40:33 +0100

    Hopefully more reliable DIRTY test for the GIT version

--- src/mkversion.sh
+++ src/mkversion.sh
@@ -13,7 +13,7 @@ gitversion=''
 if v=`git describe --match 'V*' 2>/dev/null`; then
     if [ ! -z $v ]; then
 	gitversion=`echo $v | sed 's/^V//'`
-	if ! git diff --quiet; then gitversion="$gitversion-DIRTY"; fi
+	if [ `git diff | wc -c` != 0 ]; then gitversion="$gitversion-DIRTY"; fi
 	echo "#define GIT_VERSION "'"'$gitversion'"' >> version.h.new
     fi
 fi
