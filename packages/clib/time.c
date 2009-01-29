38330f855a9a2e233d8651ce5e574dee806f684b
Jan Wielemaker <J.Wielemaker@uva.nl>
2009-01-29 10:39:10 +0100

    Updated header

--- packages/clib/time.c
+++ packages/clib/time.c
@@ -3,9 +3,9 @@
     Part of SWI-Prolog
 
     Author:        Jan Wielemaker
-    E-mail:        wielemak@science.uva.nl
+    E-mail:        J.Wielemaker@uva.nl
     WWW:           http://www.swi-prolog.org
-    Copyright (C): 1985-2007, University of Amsterdam
+    Copyright (C): 1985-2009, University of Amsterdam
 
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License
