4974c32627bbbc46ae919b69d9418345219c155b
Markus Triska <markus.triska@gmx.at>
2009-05-20 23:17:30 +0200

    fix singleton

--- library/clp/clpfd.pl
+++ library/clp/clpfd.pl
@@ -1892,7 +1892,7 @@ matches([
          m(var(X) #>= var(Y)+integer(C))       -> [g(C1 is -C), p(x_leq_y_plus_c(Y, X, C1))],
          m(any(X) #>= integer(Y)+integer(Z))   -> [g(I is Y+Z), g(clpfd_geq_(X, I))],
          m(var(X) + integer(C) #>= var(Y))     -> [p(x_leq_y_plus_c(Y, X, C))],
-         m(var(X) - integer(C) #>= var(Y))     -> [g(C1 is -C), p(x_leq_y_plus_c(Y, X, C))],
+         m(var(X) - integer(C) #>= var(Y))     -> [g(C1 is -C), p(x_leq_y_plus_c(Y, X, C1))],
          m(integer(X)+integer(Y) #>= any(Z))   -> [g(I is X+Y), g(clpfd_geq_(I, Z))],
          m(integer(X) #>= any(Z) + integer(A)) -> [g(C is X - A), g(clpfd_geq_(C, Z))],
          m(abs(any(X)) #>= integer(I))         -> [g(parse_clpfd(X, RX)), g((I>0 -> I1 is -I, RX in inf..I1 \/ I..sup; true))],
