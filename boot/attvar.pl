404b7906661e1907ff11b832e8342b386a1efe52
Markus Triska <markus.triska@gmx.at>
2009-08-06 20:40:32 +0200

    use new term_attvars/2

--- boot/attvar.pl
+++ boot/attvar.pl
@@ -215,36 +215,18 @@ copy_term(Term, Copy, Gs) :-
 	Copy0 = Copy,
 	Gs0 = Gs.
 
-% All of Term's variables are visited in their natural order.
-% Attributes are collected, and visited attributed variables are
-% marked with a copy_term_visited attribute.
-
 term_residuals(Term) -->
-	(   { '$attributed'(Term) }
-	->  { term_variables(Term, Vs0), variables_attvars(Vs0, AVs) },
-	    att_terms_residuals(AVs)
-	;   []
-	).
+	{ term_attvars(Term, Vs) },
+	attvars_residuals(Vs).
 
-variables_attvars([], []).
-variables_attvars([V|Vs0], AVs) :-
-	(   attvar(V), \+ get_attr(V, copy_term_visited, _)
-	->  get_attrs(V, A),
-	    AVs = [A-V|Rest],
-	    put_attr(V, copy_term_visited, true),
-	    variables_attvars(Vs0, Rest)
-	;   variables_attvars(Vs0, AVs)
-	).
+attvars_residuals([]) --> [].
+attvars_residuals([V|Vs]) -->
+	{ get_attrs(V, As) },
+	attvar_residuals(As, V),
+	attvars_residuals(Vs).
 
-copy_term_visited:attribute_goals(_) --> [].
-
-att_terms_residuals([])	       --> [].
-att_terms_residuals([A-V|AVs]) -->
-	att_term_residuals(A, V),
-	att_terms_residuals(AVs).
-
-att_term_residuals([], _)		    --> [].
-att_term_residuals(att(Module,Value,As), V) -->
+attvar_residuals([], _) --> [].
+attvar_residuals(att(Module,Value,As), V) -->
 	(   { nonvar(V) }
 	->  % a previous projection predicate could have instantiated
 	    % this variable, for example, to avoid redundant goals
@@ -260,8 +242,7 @@ att_term_residuals(att(Module,Value,As), V) -->
 	    ;	[put_attr(V, Module, Value)]
 	    )
 	),
-	term_residuals(Value),
-	att_term_residuals(As, V).
+	attvar_residuals(As, V).
 
 list([])     --> [].
 list([L|Ls]) --> [L], list(Ls).
