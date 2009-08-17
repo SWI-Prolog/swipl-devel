1da88136e0238b09334c4c43c96c2bc4756cdf79
Jan Wielemaker <J.Wielemaker@uva.nl>
2009-08-17 16:21:27 +0200

    Preparing 5.7.14

--- pl.spec
+++ pl.spec
@@ -1,5 +1,5 @@
 Name:		pl
-Version:	5.7.13
+Version:	5.7.14
 Release:	200
 Epoch:		0
 Summary:	A Free Software Prolog compiler.
--- src/SWI-Prolog.h
+++ src/SWI-Prolog.h
@@ -54,7 +54,7 @@ extern "C" {
 /* PLVERSION: 10000 * <Major> + 100 * <Minor> + <Patch> */
 
 #ifndef PLVERSION
-#define PLVERSION 50713
+#define PLVERSION 50714
 #endif
 
 		 /*******************************
