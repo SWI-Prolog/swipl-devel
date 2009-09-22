2f51d3d5cb6b044dc526f13e2af62dd3ca217f6a
Jan Wielemaker <J.Wielemaker@uva.nl>
2009-09-22 21:14:05 +0200

    Declare more functions safe

--- src/tools/shift.pl
+++ src/tools/shift.pl
@@ -3,3 +3,12 @@
 	  ]).
 
 shift_safe('$attvars_after_choicepoint/2', predicate).
+shift_safe('succ/2', predicate).
+shift_safe('unify_with_occurs_check/2', predicate).
+
+shift_safe(unify_vmi, function).
+shift_safe(vm_compile_instruction, function).
+shift_safe(unify_ptrs, function).
+shift_safe(unifyAtomic, function).
+shift_safe(unify_with_occurs_check, function).
+shift_safe(var_or_integer, function).
