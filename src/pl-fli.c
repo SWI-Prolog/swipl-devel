96aaf3a4222d868b8c6a335fcb9fbd7b95cacaea
Jan Wielemaker <J.Wielemaker@uva.nl>
2009-10-16 14:43:17 +0200

    Make PL_put_term() return TRUE, so it is easier in chains of conditions.

--- src/pl-fli.c
+++ src/pl-fli.c
@@ -2255,12 +2255,13 @@ PL_put_term__LD(term_t t1, term_t t2 ARG_LD)
 
 
 #undef PL_put_term
-void
+int
 PL_put_term(term_t t1, term_t t2)
 { GET_LD
   Word p2 = valHandleP(t2);
 
   setHandle(t1, linkVal(p2));
+  return TRUE;
 }
 #define PL_put_term(t1, t2) PL_put_term__LD(t1, t2 PASS_LD)
 
