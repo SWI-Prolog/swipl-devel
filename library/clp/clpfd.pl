7272fc58cf44f60d909354d46fe108e1e209217d
Markus Triska <markus.triska@gmx.at>
2009-10-26 13:57:52 +0100

    add PlDoc examples for all_distinct/1, global_cardinality/2 and circuit/1

--- library/clp/clpfd.pl
+++ library/clp/clpfd.pl
@@ -4525,7 +4525,14 @@ v_in_stack(V) --> { get_attr(V, in_stack, true) }.
 
 %% all_distinct(+Ls).
 %
-% Like all_different/1, with stronger propagation.
+%  Like all_different/1, with stronger propagation. For example,
+%  all_distinct/1 can detect that not all variables can assume distinct
+%  values given the following domains:
+%
+%  ==
+%  ?- maplist(in, Vs, [1..3, 1..3, 1..2\/4, 1..2\/4, 1\/3..4]), all_distinct(Vs).
+%  false.
+%  ==
 
 all_distinct(Ls) :- regin_attach(Ls).
 
@@ -4749,6 +4756,15 @@ element_([I|Is], N0, N, V) :-
 %     domain variable. The constraint holds iff each V in Vs is equal
 %     to some key, and for each Key-Num pair in Pairs, the number of
 %     occurrences of Key in Vs is Num.
+%
+%     Example:
+%
+%     ==
+%     ?- Vs = [_,_,_], global_cardinality(Vs, [1-2,3-_]), label(Vs).
+%     Vs = [1, 1, 3] ;
+%     Vs = [1, 3, 1] ;
+%     Vs = [3, 1, 1].
+%     ==
 
 global_cardinality(Xs, Pairs) :-
         must_be(list, Xs),
@@ -5091,7 +5107,17 @@ gcc_pair(Pair) :-
 %
 %     True if the list Vs of finite domain variables induces a
 %     Hamiltonian circuit, where the k-th element of Vs denotes the
-%     successor of node k. Node indexing starts with 1.
+%     successor of node k. Node indexing starts with 1. Examples:
+%
+%     ==
+%     ?- length(Vs, _), circuit(Vs), label(Vs).
+%     Vs = [] ;
+%     Vs = [1] ;
+%     Vs = [2, 1] ;
+%     Vs = [2, 3, 1] ;
+%     Vs = [3, 1, 2] ;
+%     Vs = [2, 3, 4, 1] .
+%     ==
 
 circuit(Vs) :-
         must_be(list, Vs),
