8671f26ab620e17873dbfa015da4fbcd51e79674
Markus Triska <markus.triska@gmx.at>
2010-01-24 01:40:12 +0100

    further multiplication strengthening -- down to 1652 cases in -4..4
    The following program is useful to test boundaries for infinities:
    
       :- use_module(library(clpfd)).
    
       run(N) :-
               Lower #= -N,
               Vs = [X,Y,Z],
               between(Lower, N, LZ),
               (   CsZ = [Z #>= LZ] ; CsZ = [Z #=< LZ] ; CsZ = [] ),
               between(Lower, N, LX),
               (   CsX = [X #>= LX] ; CsX = [X #=< LX] ; CsX = [] ),
               between(Lower, N, LY),
               (   CsY = [Y #>= LY] ; CsY = [Y #=< LY] ; CsY = [] ),
               append([CsZ,CsX,CsY], Cs),
               G1 = (X*Y #= Z),
               G2 = maplist(call, Cs),
               G3 = (maplist(between(Lower,N), [X,Y]),
                        L2 #= -N^2, U2 #= N^2, between(L2,U2,Z)),
               findall(Vs, (G1,G2,G3), Sols1),
               findall(Vs, (G2,G1,G3), Sols2),
               findall(Vs, (G2,G3,G1), Sols3),
               (   Sols1 == Sols2, Sols2 == Sols3 -> false
               ;   portray_clause(failed-Vs-Cs-Sols1-Sols2-Sols3)
               ).

--- library/clp/clpfd.pl
+++ library/clp/clpfd.pl
@@ -4160,7 +4160,7 @@ min_divide(L1,U1,L2,U2,Min) :-
         ;   L1 = n(NL1), NL1 > 0 -> Min cis -U1
         ;   U1 = n(NU1), NU1 < 0, U2 cis_leq n(0) ->
             (   finite(L2) -> Min cis div(U1+L2+n(1),L2)
-            ;   Min = n(0)
+            ;   Min = n(1)
             )
         ;   U1 = n(NU1), NU1 < 0, cis_geq_zero(L2) -> Min cis div(L1,L2)
         ;   U1 = n(NU1), NU1 < 0 -> Min = L1
@@ -4171,7 +4171,10 @@ max_divide(L1,U1,L2,U2,Max) :-
         (   L2 = n(_), cis_geq_zero(L1), cis_geq_zero(L2) ->
             Max cis div(U1,L2)
                                 % TODO: cover more cases
-        ;   L1 = n(NL1), NL1 > 0, U2 cis_leq n(0) -> Max cis div(L1,L2)
+        ;   L1 = n(NL1), NL1 > 0, U2 cis_leq n(0) ->
+            (   finite(L2) -> Max cis div(L1-L2-n(1),L2)
+            ;   Max = n(-1)
+            )
         ;   L1 = n(NL1), NL1 > 0 -> Max = U1
         ;   U1 = n(NU1), NU1 < 0, U2 cis_leq n(-1) -> Max cis div(L1,U2)
         ;   U1 = n(NU1), NU1 < 0, cis_geq_zero(L2) ->
