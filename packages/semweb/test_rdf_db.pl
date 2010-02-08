a648a195b61a1d3e878e2de04ffe170b5e65389b
Jan Wielemaker <J.Wielemaker@uva.nl>
2010-02-08 12:03:59 +0100

    Fix paths for running tests in semweb package

--- packages/semweb/test_rdf_db.pl
+++ packages/semweb/test_rdf_db.pl
@@ -37,7 +37,7 @@
 :- asserta(user:file_search_path(library, '../sgml')).
 :- asserta(user:file_search_path(foreign, '../clib')).
 :- asserta(user:file_search_path(library, '../clib')).
-:- asserta(user:file_search_path(library, '../sgml/RDF')).
+:- asserta(user:file_search_path(library, '../RDF')).
 :- asserta(user:file_search_path(foreign, '.')).
 :- use_module(rdf_db).
 :- use_module(rdfs).
