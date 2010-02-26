20a767c40d7071e55be4965d7993b087ad952a16
Jan Wielemaker <J.Wielemaker@uva.nl>
2010-02-26 20:26:54 +0100

    Added Ciao libraries read.pl, write.pl, strings.pl and format.pl

--- src/common.mk
+++ src/common.mk
@@ -6,5 +6,6 @@
 DIALECT=yap.pl hprolog.pl commons.pl ciao.pl sicstus.pl
 YAP=	README.TXT
 SICSTUS=block.pl timeout.pl system.pl arrays.pl lists.pl
-CIAO=	assertions.pl isomodes.pl regtypes.pl sockets.pl
+CIAO=	assertions.pl isomodes.pl regtypes.pl sockets.pl \
+	read.pl write.pl strings.pl format.pl
 ISO=	iso_predicates.pl
