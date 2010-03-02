064bc95e9c4f9166e899ef472a3e921bac443e80
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2010-03-02 16:22:19 +0100

    Embed manifests in all files

--- src/win32/embed_manifests.cmd
+++ src/win32/embed_manifests.cmd
@@ -1,13 +1,12 @@
+@echo off
 SETLOCAL
 
 call call_vcvars.cmd
 
 set "_=%CD%"
 cd ..
-for /F %%1 in ('dir *.exe /B /S /A:-D') do (mt.exe -manifest "%%1.manifest" -outputresource:"%%1";1)
-for /F %%1 in ('dir *.dll /B /S /A:-D') do (mt.exe -manifest "%%1.manifest" -outputresource:"%%1";2)
+for /F "delims=" %%1 in ('dir *.exe /B /S /A:-D') do (echo "%%1" && mt.exe -nologo -manifest "%%1.manifest" -outputresource:"%%1";1)
+for /F "delims=" %%1 in ('dir *.dll /B /S /A:-D') do (echo "%%1" && mt.exe -nologo -manifest "%%1.manifest" -outputresource:"%%1";2)
 cd %_%
 
 ENDLOCAL
-
-pause
