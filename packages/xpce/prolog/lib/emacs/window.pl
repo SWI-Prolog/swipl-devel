fd20a788cfef02c5eaefcfd57beb852d15545579
Jan Wielemaker <J.Wielemaker@uva.nl>
2010-04-01 11:47:18 +0200

    Remove unused `reuse' variable

--- packages/xpce/prolog/lib/emacs/window.pl
+++ packages/xpce/prolog/lib/emacs/window.pl
@@ -597,8 +597,6 @@ prompter(D, Prompter:dialog_item*) :->
 
 class_variable(size,         size, size(80,32), "Size of text-field").
 
-variable(reuse,		bool := @on,	both, "Re-use for opening buffers").
-
 initialise(V, B:buffer=[emacs_buffer], W:width=[int], H:height=[int]) :->
 	"Create for buffer"::
 	get(V, class_variable_value, size, size(DW, DH)),
