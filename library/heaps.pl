bc342dbf3006ad96808cae37d9ee7bd8120def3e
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2010-09-27 20:32:58 +0200

    ENHANCED: New implementation of library(heaps) by Lars Buitinck.  Faster and more compatible to the SICStus library(heaps).

--- library/heaps.pl
+++ library/heaps.pl
@@ -1,233 +1,192 @@
-%   File   : HEAPS.PL
-%   Author : R.A.O'Keefe
-%   Updated: 29 November 1983
-%   Purpose: Implement heaps in Prolog.
+/*  Part of SWI-Prolog
 
-:- module(heaps, [
-		  add_to_heap/4,
-		  empty_heap/1,
-		  get_from_heap/4,
-		  heap_size/2,
-		  heap_to_list/2,
-		  list_to_heap/2,
-		  min_of_heap/3,
-		  min_of_heap/5
-		 ]).
+    Author:        Lars Buitinck
+    E-mail:        larsmans@gmail.com
+    WWW:           http://www.swi-prolog.org
+    Copyright (C): 2010, Lars Buitinck
 
-/** <module> Priority Queues
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
 
-    A heap is a labelled binary tree where the key of each node is less
-    than or equal to the keys of its sons.  The point of a heap is that
-    we can keep on adding new elements to the heap and we can keep on
-    taking out the minimum element.  If there are N elements total, the
-    total time is O(NlgN).  If you know all the elements in advance, you
-    are better off doing a merge-sort, but this file is for when you
-    want to do say a best-first search, and have no idea when you start
-    how many elements there will be, let alone what they are.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-    A heap is represented as a triple t(N, Free, Tree) where N is the
-    number of elements in the tree, Free is a list of integers which
-    specifies unused positions in the tree, and Tree is a tree made of
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-	| t			 | terms for empty subtrees and |
-	| t(Key,Datum,Lson,Rson) | terms for the rest		|
-
-    The nodes of the tree are notionally numbered like this:
-
-    ==
-				    1
-		     2				    3
-             4               6               5               7
-         8      12      10     14       9       13      11     15
-      ..  ..  ..  ..  ..  ..  ..  ..  ..  ..  ..  ..  ..  ..  ..  ..
-    ==
-
-    The idea is that if the maximum number of elements that have been in
-    the heap so far is M, and the tree currently has K elements, the tree
-    is some subtreee of the tree of this form having exactly M elements,
-    and the Free list is a list of K-M integers saying which of the
-    positions in the M-element tree are currently unoccupied.  This free
-    list is needed to ensure that the cost of passing N elements through
-    the heap is O(NlgM) instead of O(NlgN).  For M say 100 and N say 10^4
-    this means a factor of two.  The cost of the free list is slight.
-    The storage cost of a heap in a copying Prolog (which Dec-10 Prolog is
-    not) is 2K+3M words.
-
-    @tbd   Missing for SICStus compatibility:
-
-   * delete_from_heap(+OldHeap, +Key, ?Datum, ?NewHeap)
-	  deletes a single Key-Datum pair in OldHeap producing
-	  NewHeap.
-
-   * is_heap(+Heap)
-          is true if Heap is a valid heap
-
-   @author R.A.O'Keefe
+    As a special exception, if you link this library with other files,
+    compiled with a Free Software compiler, to produce an executable, this
+    library does not by itself cause the resulting executable to be covered
+    by the GNU General Public License. This exception does not however
+    invalidate any other reasons why the executable file might be covered by
+    the GNU General Public License.
 */
 
-%%  add_to_heap(+OldHeap, +Key, +Datum, -NewHeap)
-%   inserts the new Key-Datum pair into the heap.  The insertion is
-%   not stable, that is, if you insert several pairs with the same
-%   Key it is not defined which of them will come out first, and it
-%   is possible for any of them to come out first depending on the
-%   history of the heap.  If you need a stable heap, you could add
-%   a counter to the heap and include the counter at the time of
-%   insertion in the key.  If the free list is empty, the tree will
-%   be grown, otherwise one of the empty slots will be re-used.  (I
-%   use imperative programming language, but the heap code is as
-%   pure as the trees code, you can create any number of variants
-%   starting from the same heap, and they will share what common
-%   structure they can without interfering with each other.)
+:- module(heaps,
+          [ add_to_heap/4,		% +Heap0, +Priority, ?Key, -Heap
+	    delete_from_heap/4,		% +Heap0, -Priority, +Key, -Heap
+	    empty_heap/1,		% +Heap
+	    get_from_heap/4,		% ?Heap0, ?Priority, ?Key, -Heap
+	    heap_size/2,		% +Heap, -Size:int
+	    heap_to_list/2,		% +Heap, -List:list
+	    is_heap/1,			% +Term
+	    list_to_heap/2,		% +List:list, -Heap
+	    merge_heaps/3,		% +Heap0, +Heap1, -Heap
+	    min_of_heap/3,		% +Heap, ?Priority, ?Key
+	    min_of_heap/5		% +Heap, ?Priority1, ?Key1,
+	    				%        ?Priority2, ?Key2
+          ]).
 
-add_to_heap(t(M,[],OldTree), Key, Datum, t(N,[],NewTree)) :- !,
-	N is M+1,
-	add_to_heap(N, Key, Datum, OldTree, NewTree).
-add_to_heap(t(M,[H|T],OldTree), Key, Datum, t(N,T,NewTree)) :-
-	N is M+1,
-	add_to_heap(H, Key, Datum, OldTree, NewTree).
+/** <module> heaps/priority queues
+ *
+ * Heaps are data structures that return the entries inserted into them in an
+ * ordered fashion, based on a priority. This makes them the data structure of
+ * choice for implementing priority queues, a central element of algorithms
+ * such as best-first/A* search and Kruskal's minimum-spanning-tree algorithm.
+ *
+ * This module implements min-heaps, meaning that items are retrieved in
+ * ascending order of key/priority. It was designed to be compatible with
+ * the SICStus Prolog library module of the same name. The merge_heaps/3
+ * predicate is an SWI-specific extension. The portray_heap/1 predicate is
+ * not implemented.
+ *
+ * The current version implements top-down skew heaps, as described by Sleator
+ * and Tarjan (1986). All operations can be performed in at most O(lg n)
+ * amortized time, except for delete_from_heap/4, heap_to_list/2 and
+ * list_to_heap/2.
+ *
+ * @author Lars Buitinck
+ */
 
+/*
+ * Heaps are represented as heap(H,Size) terms, where H is the "actual heap"
+ * and Size is an integer. The "actual heaps" are either nil (empty) or terms
+ * of the form t(L,TopElem,TopPrio,R), where L and R are again "actual heaps".
+ */
 
-add_to_heap(1, Key, Datum, _, t(Key,Datum,t,t)) :- !.
-add_to_heap(N, Key, Datum, t(K1,D1,L1,R1), t(K2,D2,L2,R2)) :-
-	E is N mod 2,
-	M is N // 2,
-	sort2(Key, Datum, K1, D1, K2, D2, K3, D3),
-	add_to_heap(E, M, K3, D3, L1, R1, L2, R2).
-
-
-add_to_heap(0, N, Key, Datum, L1, R, L2, R) :- !,
-	add_to_heap(N, Key, Datum, L1, L2).
-add_to_heap(1, N, Key, Datum, L, R1, L, R2) :- !,
-	add_to_heap(N, Key, Datum, R1, R2).
-
-
-sort2(Key1, Datum1, Key2, Datum2, Key1, Datum1, Key2, Datum2) :-
-	Key1 @< Key2,
-	!.
-sort2(Key1, Datum1, Key2, Datum2, Key2, Datum2, Key1, Datum1).
-
-
-%%  empty_heap(?Heap)
-%   is true if Heap is the empty heap
-
-empty_heap(t(0,[],t)).
-
-
-
-%%	get_from_heap(+OldHeap, ?Key, ?Datum, -NewHeap)
+%%	add_to_heap(+Heap0, +Priority, ?Key, -Heap) is semidet.
 %
-%	returns the Key-Datum pair in OldHeap with the smallest Key, and
-%	also a New Heap which is the   Old  Heap with that pair deleted.
-%	The easy part is picking off the smallest element. The hard part
-%	is repairing the heap structure using repair_heap/4.
+%	Adds Key with priority Priority  to   Heap0,  constructing a new
+%	heap in Heap.
 
-get_from_heap(t(N,Free,t(Key,Datum,L,R)), Key, Datum, t(M,[Hole|Free],Tree)) :-
-	M is N-1,
-	repair_heap(L, R, Tree, Hole).
+add_to_heap(heap(Q0,M),P,X,heap(Q1,N)) :-
+	meld(Q0,t(X,P,nil,nil),Q1),
+	N is M+1.
 
-
-%%	repair_heap(+Heap1, +Heap2, -Heap, -GapPos)
+%%	delete_from_heap(+Heap0, -Priority, +Key, -Heap) is semidet.
 %
-%	repair_heap/4 takes a pair of heaps and returns a new heap built
-%	from their elements, and the position number   of the gap in the
-%	new tree. Note that repair_heap is *not* tail-recursive.
+%	Deletes Key from Heap0, leaving its priority in Priority and the
+%	resulting data structure  in  Heap.   Useful  for  changing  the
+%	priority of Key. Fails if Key is not found in Heap0.
+%
+%	@bug This predicate is extremely inefficient.
 
-repair_heap(t(K1,D1,L1,R1), t(K2,D2,L2,R2), t(K2,D2,t(K1,D1,L1,R1),R3), N) :-
-	K2 @< K1,
-	!,
-	repair_heap(L2, R2, R3, M),
-	N is 2*M+1.
-repair_heap(t(K1,D1,L1,R1), t(K2,D2,L2,R2), t(K1,D1,L3,t(K2,D2,L2,R2)), N) :- !,
-	repair_heap(L1, R1, L3, M),
-	N is 2*M.
-repair_heap(t(K1,D1,L1,R1), t, t(K1,D1,L3,t), N) :- !,
-	repair_heap(L1, R1, L3, M),
-	N is 2*M.
-repair_heap(t, t(K2,D2,L2,R2), t(K2,D2,t,R3), N) :- !,
-	repair_heap(L2, R2, R3, M),
-	N is 2*M+1.
-repair_heap(t, t, t, 1) :- !.
+delete_from_heap(Q0,P,X,Q) :-
+	get_from_heap(Q0,P,X,Q), !.
+delete_from_heap(Q0,Px,X,Q) :-
+	get_from_heap(Q0,Py,Y,Q1),
+	delete_from_heap(Q1,Px,X,Q2),
+	add_to_heap(Q2,Py,Y,Q).
 
+%%	empty_heap(?Heap) is semidet.
+%
+%       True if Heap is an empty heap.
 
+empty_heap(heap(nil,0)).
 
-%%  heap_size(+Heap, ?Size)
-%   reports the number of elements currently in the heap.
+%%	get_from_heap(?Heap0, ?Priority, ?Key, -Heap) is semidet.
+%
+%	Retrieves the minimum-priority  pair   Priority-Key  from Heap0.
+%	Heap is Heap0 with that pair removed.
 
-heap_size(t(Size,_,_), Size).
+get_from_heap(heap(t(X,P,L,R),M),P,X,heap(Q,N)) :-
+	meld(L,R,Q),
+	N is M-1.
 
+%%	heap_size(+Heap, -Size:int) is det.
+%
+%	Determines the number of elements in Heap.
 
+heap_size(heap(_,N),N).
 
-%%  heap_to_list(+Heap, -List)
-%   returns the current set of Key-Datum pairs in the Heap as a
-%   List, sorted into ascending order of Keys.  This is included
-%   simply because I think every data structure foo ought to have
-%   a foo_to_list and list_to_foo relation (where, of course, it
-%   makes sense!) so that conversion between arbitrary data
-%   structures is as easy as possible.  This predicate is basically
-%   just a merge sort, where we can exploit the fact that the tops
-%   of the subtrees are smaller than their descendants.
+%%	heap_to_list(+Heap, -List:list) is det.
+%
+%	Constructs a list List  of   Priority-Element  terms, ordered by
+%	(ascending) priority.
 
-heap_to_list(t(_,_,Tree), List) :-
-	heap_tree_to_list(Tree, List).
+heap_to_list(Q,L) :-
+	to_list(Q,L).
+to_list(heap(nil,0),[]) :- !.
+to_list(Q0,[P-X|Xs]) :-
+	get_from_heap(Q0,P,X,Q),
+	heap_to_list(Q,Xs).
 
+%%	is_heap(+X) is semidet.
+%
+%	Returns true is X is a heap.
+%
+%	@bug does not test the integrity of the actual heap.
 
-heap_tree_to_list(t, []) :- !.
-heap_tree_to_list(t(Key,Datum,Lson,Rson), [Key-Datum|Merged]) :-
-	heap_tree_to_list(Lson, Llist),
-	heap_tree_to_list(Rson, Rlist),
-	heap_tree_to_list(Llist, Rlist, Merged).
+is_heap(V) :-
+	var(V), !, fail.
+is_heap(heap(_,N)) :-
+	integer(N).
 
+%%	list_to_heap(+List:list, -Heap) is det.
+%
+%	If List is a list of   Priority-Element terms, constructs a heap
+%	out of List.
 
-heap_tree_to_list([H1|T1], [H2|T2], [H2|T3]) :-
-	H2 @< H1,
-	!,
-	heap_tree_to_list([H1|T1], T2, T3).
-heap_tree_to_list([H1|T1], T2, [H1|T3]) :- !,
-	heap_tree_to_list(T1, T2, T3).
-heap_tree_to_list([], T, T) :- !.
-heap_tree_to_list(T, [], T).
+list_to_heap(Xs,Q) :-
+	empty_heap(Empty),
+	list_to_heap(Xs,Empty,Q).
 
+list_to_heap([],Q,Q).
+list_to_heap([P-X|Xs],Q0,Q) :-
+	add_to_heap(Q0,P,X,Q1),
+	list_to_heap(Xs,Q1,Q).
 
+%%	min_of_heap(+Heap, ?Priority, ?Key) is semidet.
+%
+%	Unifies Key with  the  minimum-priority   element  of  Heap  and
+%	Priority with its priority value.
 
-%%  list_to_heap(+List, -Heap)
-%   takes a list of Key-Datum pairs (such as keysort could be used to
-%   sort) and forms them into a heap.  We could do that a wee bit
-%   faster by keysorting the list and building the tree directly, but
-%   this algorithm makes it obvious that the result is a heap, and
-%   could be adapted for use when the ordering predicate is not @<
-%   and hence keysort is inapplicable.
+min_of_heap(heap(t(X,P,_,_),_),P,X).
 
-list_to_heap(List, Heap) :-
-	list_to_heap(List, 0, t, Heap).
+%%	min_of_heap(+Heap, ?Priority1, ?Key1, ?Priority2, ?Key2) is semidet.
+%
+%	Gets the two minimum-priority elements from Heap.
 
+min_of_heap(Q,Px,X,Py,Y) :-
+	get_from_heap(Q,Px,X,Q0),
+	min_of_heap(Q0,Py,Y).
 
-list_to_heap([], N, Tree, t(N,[],Tree)) :- !.
-list_to_heap([Key-Datum|Rest], M, OldTree, Heap) :-
-	N is M+1,
-	add_to_heap(N, Key, Datum, OldTree, MidTree),
-	list_to_heap(Rest, N, MidTree, Heap).
+%%	merge_heaps(+Heap0, +Heap1, -Heap) is det.
+%
+%	Merge the two heaps Heap0 and Heap1 in Heap.
 
+merge_heaps(heap(L,K),heap(R,M),heap(Q,N)) :-
+	meld(L,R,Q),
+	N is K+M.
 
+/*
+ * The meld/3 predicate is the backbone of the implementation.
+ * It merges two heaps according to the skew heap definition.
+ */
 
-%%  min_of_heap(+Heap, ?Key, ?Datum)
-%   returns the Key-Datum pair at the top of the heap (which is of
-%   course the pair with the smallest Key), but does not remove it
-%   from the heap.  It fails if the heap is empty.
-
-%%  min_of_heap(+Heap, ?Key1, ?Datum1, ?Key2, ?Datum2)
-%   returns the smallest (Key1) and second smallest (Key2) pairs in
-%   the heap, without deleting them.  It fails if the heap does not
-%   have at least two elements.
-
-min_of_heap(t(_,_,t(Key,Datum,_,_)), Key, Datum).
-
-
-min_of_heap(t(_,_,t(Key1,Datum1,Lson,Rson)), Key1, Datum1, Key2, Datum2) :-
-	min_of_heap(Lson, Rson, Key2, Datum2).
-
-
-min_of_heap(t(Ka,_,_,_), t(Kb,Db,_,_), Kb, Db) :-
-	Kb @< Ka, !.
-min_of_heap(t(Ka,Da,_,_), _, Ka, Da).
-min_of_heap(t, t(Kb,Db,_,_), Kb, Db).
-
+meld(nil,Q,Q) :- !.
+meld(Q,nil,Q).
+meld(L,R,Q) :-
+	L = t(X,Px,LL,LR),
+	R = t(Y,Py,RL,RR),
+	(   Px @< Py
+	->  Q = t(X,Px,L1,LL),
+	    meld(LR,R,L1)
+	;   Q = t(Y,Py,L1,RL),
+	    meld(L,RR,L1)
+	).
