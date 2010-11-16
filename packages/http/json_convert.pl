274d744336bde0b56c9835d6493e745509034f33
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2010-11-16 13:37:53 +0100

    Update file-header

--- packages/http/json_convert.pl
+++ packages/http/json_convert.pl
@@ -3,9 +3,10 @@
     Part of SWI-Prolog
 
     Author:        Jan Wielemaker
-    E-mail:        wielemak@science.uva.nl
+    E-mail:        J.Wielemaker@cs.vu.nl
     WWW:           http://www.swi-prolog.org
-    Copyright (C): 1985-2007, University of Amsterdam
+    Copyright (C): 1985-2010, University of Amsterdam
+			      VU University Amsterdam
 
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License
