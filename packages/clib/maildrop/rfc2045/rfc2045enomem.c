57814562ca3c7fa012b17aa6ad01a1d96dbda01e
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2010-12-15 13:40:03 +0100

    Removed clib; preparing to add as module

--- packages/clib/maildrop/rfc2045/rfc2045enomem.c
+++
@@ -1,9 +0,0 @@
-#if	HAVE_CONFIG_H
-#include       "config.h"
-#endif
-#include	"rfc2045.h"
-
-void rfc2045_enomem()
-{
-	rfc2045_error("Out of memory.");
-}
--- packages/clib/testrlimit.pl
+++
@@ -1,8 +0,0 @@
-test :-
-	rlimit(cpu, _, 2),
-	on_signal(xcpu, _, cpu_exceeded),
-	( repeat, fail ).
-
-cpu_exceeded(_Sig) :-
-	format(user_error, 'CPU time exceeded~n', []),
-	halt(1).
