SWI-Prolog/swipl-devel

commit 401926761e0e97844205291e1aa6c7dfcc018d76

MODIFIED: Give a syntax error on control-characters and Unicode unassigned characters if they are encountered outside quoted material or comment.

src/os/pl-ctype.h
@@ -68,7 +68,7 @@ extern const char _PL_char_types[];	/* array of character types */
 	((unsigned)(c) <= 0xff ? (_PL_char_types[(unsigned char)(c)] t) : w)
 
 #define isControlW(c)	PlCharType(c, == CT, iswcntrl((wint_t)c))
-#define isBlankW(c)	PlCharType(c, <= SP, iswspace((wint_t)c))
+#define isBlankW(c)	PlCharType(c, == SP, iswspace((wint_t)c))
 #define isDigitW(c)	PlCharType(c, == DI, FALSE)
 #define isLowerW(c)	PlCharType(c, == LC, iswlower((wint_t)c))
 #define isUpperW(c)	PlCharType(c, == UC, iswupper((wint_t)c))

man/overview.doc
@@ -1856,22 +1856,22 @@ character-case.  In such languages variables \emph{must} be written as
 \verb$_name$.
 
     \item [White space]
-All characters marked as separators (Z*) or control (C*) in the Unicode
-tables are handled as layout characters.
+All characters marked as separators (Z*) in the Unicode tables are
+handled as layout characters.
+
+    \item [Control and unassigned characters]
+Control and unassigned (C*) characters produce a syntax error if
+encountered outside quoted atoms/strings and outside comments.
 
     \item [Other characters]
 The first 128 characters follow the ISO Prolog standard.  Unicode symbol
 and punctuation characters (general category S* and P*) act as glueing
-symbol characters (i.e., just like \const{==}, an unquoted sequence of
-these characters are combined as an atom).
+symbol characters (i.e., just like \const{==}: an unquoted sequence of
+symbol characters are combined into an atom).
 
 Other characters (this is mainly \const{No}: \textit{a numeric character
 of other type}) are currently handled as `solo'.
 \end{itemlist}
-
-\paragraph{Issues}
-
-The current system handles control characters as layout.
 
 
 \subsubsection{Singleton variable checking}	\label{sec:singleton}

src/os/pl-ctype.c
@@ -784,7 +784,7 @@ EndPredDefs
 
 const char _PL_char_types[] = {
 /* ^@  ^A  ^B  ^C  ^D  ^E  ^F  ^G  ^H  ^I  ^J  ^K  ^L  ^M  ^N  ^O    0-15 */
-   CT, CT, CT, CT, CT, CT, CT, CT, CT, CT, CT, CT, CT, CT, CT, CT,
+   CT, CT, CT, CT, CT, CT, CT, CT, CT, SP, SP, SP, SP, SP, CT, CT,
 /* ^P  ^Q  ^R  ^S  ^T  ^U  ^V  ^W  ^X  ^Y  ^Z  ^[  ^\  ^]  ^^  ^_   16-31 */
    CT, CT, CT, CT, CT, CT, CT, CT, CT, CT, CT, CT, CT, CT, CT, CT,
 /* sp   !   "   #   $   %   &   '   (   )   *   +   ,   -   .   /   32-47 */

src/pl-read.c
@@ -50,7 +50,7 @@ static strnumstat scan_decimal(cucharp *sp, Number n);
 	((unsigned)(c) <= 0xff ? (_PL_char_types[(unsigned)(c)] t) \
 			       : (uflagsW(c) & (w)))
 
-#define PlBlankW(c)	CharTypeW(c, <= SP, U_SEPARATOR|U_CONTROL)
+#define PlBlankW(c)	CharTypeW(c, == SP, U_SEPARATOR)
 #define PlUpperW(c)	CharTypeW(c, == UC, U_UPPERCASE)
 #define PlIdStartW(c)	(c <= 0xff ? (isLower(c)||isUpper(c)||c=='_') \
 				   : uflagsW(c) & U_ID_START)
@@ -1067,7 +1067,6 @@ raw_read2(ReadData _PL_rd ARG_LD)
       default:	if ( c < 0xff )
 		{ switch(_PL_char_types[c])
 		  { case SP:
-		    case CT:
 		    blank:
 		      if ( dotseen )
 		      { if ( rb.here - rb.base == 1 )
@@ -2156,6 +2155,8 @@ get_token__LD(bool must_be_op, ReadData _PL_rd ARG_LD)
 		  break;
 		}
 #endif
+    case CT:
+		syntaxError("illegal_character", _PL_rd);
     default:
     		{ sysError("read/1: tokeniser internal error");
     		  break;		/* make lint happy */

src/test.pl
@@ -133,7 +133,7 @@ syntax(number-2) :-
 	catch(atom_to_term('2\'', _, _), E, true),
 	E = error(syntax_error(end_of_file), _).
 syntax(zero-1) :-
-	term_to_atom(T, 'hello(\000\"\000\x")'),
+	term_to_atom(T, 'hello("\000\x")'),
 	T == hello([0, 120]).
 syntax(latin-1) :-
 	atom_codes(A, [247]),

commit 69b025f732a9400fa90142b0b0d7bf8ad68164de

FIXED: Escape sequences when writing quoted atoms.  Bug introduced by 401926761e0e97844205291e1aa6c7dfcc018d76.  Ulrich Neumerkel and Mike Elston.

src/os/pl-ctype.h
@@ -37,7 +37,8 @@ extern const char _PL_char_types[];	/* array of character types */
 #define DI 10			/* Digit */
 
 #define isControl(c)	(_PL_char_types[(unsigned)(c) & 0xff] == CT)
-#define isBlank(c)	(_PL_char_types[(unsigned)(c) & 0xff] <= SP)
+#define isBlank(c)	(_PL_char_types[(unsigned)(c) & 0xff] == SP)
+#define isGraph(c)	(_PL_char_types[(unsigned)(c) & 0xff]  > SP)
 #define isDigit(c)	(_PL_char_types[(unsigned)(c) & 0xff] == DI)
 #define isLower(c)	(_PL_char_types[(unsigned)(c) & 0xff] == LC)
 #define isUpper(c)	(_PL_char_types[(unsigned)(c) & 0xff] == UC)

src/pl-write.c
@@ -299,7 +299,7 @@ PutCloseBrace(IOSTREAM *s)
 static bool
 putQuoted(int c, int quote, int flags, IOSTREAM *stream)
 { if ( (flags & PL_WRT_CHARESCAPES) )
-  { if ( !(c < 0xff && isControl(c)) && c != quote && c != '\\' )
+  { if ( !(c < 0xff && !isGraph(c)) && c != quote && c != '\\' )
     { TRY(Putc(c, stream));
     } else
     { char esc[8];