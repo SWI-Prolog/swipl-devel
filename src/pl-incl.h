8e91440f4b802b65b6066914c734dee86a867ed8
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2011-03-20 19:11:54 +0100

    Added MAY_ALIAS to make code C99 clean

--- src/pl-incl.h
+++ src/pl-incl.h
@@ -320,6 +320,12 @@ A common basis for C keywords.
 #define NORETURN
 #endif
 
+#if defined(__GNUC__) && !defined(MAY_ALIAS)
+#define MAY_ALIAS __attribute__ ((__may_alias__))
+#else
+#define MAY_ALIAS
+#endif
+
 #ifdef HAVE___BUILTIN_EXPECT
 #define likely(x)       __builtin_expect((x), 1)
 #define unlikely(x)     __builtin_expect((x), 0)
