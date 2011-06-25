SWI-Prolog/swipl-devel

commit bcae929a00d8a4093be1169877b2c1cd686473a2

FIXED: Abort at the exit and fail-port of catch/3.  At this moment the catch frame is still present, but the CHP_CATCH choicepoint is gone, causing an assertion violation in exception handling.  This catch frame should no longer be visible.  Keri Harris.

src/pl-vmi.c
@@ -3814,6 +3814,7 @@ VMI(I_EXITCATCH, VIF_BREAK, 0, ())
   { assert(BFR->type == CHP_CATCH);
     BFR = BFR->parent;
   }
+  set(FR, FR_CATCHED);
 
   VMI_GOTO(I_EXIT);
 }

src/pl-wam.c
@@ -2432,10 +2432,14 @@ next_choice:
 	{ SAVE_REGISTERS(qid);
 	  callCleanupHandler(ch->frame, FINISH_FAIL PASS_LD);
 	  LOAD_REGISTERS(qid);
+	} else
+	{ set(ch->frame, FR_CATCHED);
 	}
 	ch = BFR;			/* can be shifted */
 	if ( exception_term )
 	  THROW_EXCEPTION;
+      } else
+      { set(ch->frame, FR_CATCHED);
       }
       /*FALLTHROUGH*/
     case CHP_DEBUG:			/* Just for debugging purposes */

commit 804717dcf44ed3dc7b61e8145ba190e5dea6913a

FIXED: Catch/3 failed to catch exceptions when backtracking over the guarded goal.  Bug introduced in bcae929a00d8a4093be1169877b2c1cd686473a2. Günter Kniesel.

src/pl-vmi.c
@@ -3813,8 +3813,8 @@ VMI(I_EXITCATCH, VIF_BREAK, 0, ())
 { if ( BFR->frame == FR && BFR == (Choice)argFrameP(FR, 3) )
   { assert(BFR->type == CHP_CATCH);
     BFR = BFR->parent;
+    set(FR, FR_CATCHED);
   }
-  set(FR, FR_CATCHED);
 
   VMI_GOTO(I_EXIT);
 }

src/Tests/core/test_call.pl
@@ -40,7 +40,8 @@ test_call :-
 		    cross_module_call,
 		    snip,
 		    no_autoload,
-		    setup_call_cleanup
+		    setup_call_cleanup,
+		    catch
 		  ]).
 
 :- begin_tests(call1).
@@ -254,3 +255,11 @@ test_error_choice :-
 a(_).
 
 :- end_tests(setup_call_cleanup).
+
+:- begin_tests(catch).
+
+test(exit_nondet, fail) :-
+	catch((true;throw(homer_simpson(38))),_E,true),
+	fail.
+
+:- end_tests(catch).