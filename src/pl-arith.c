f876f1fb71675fe5b651e715ecd87b942fbcb5c5
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2011-08-22 21:44:24 +0200

    FIXED: Right shift >= 64 places of non-gmp numbers.  Bart Demoen.

--- src/pl-arith.c
+++ src/pl-arith.c
@@ -1326,7 +1326,7 @@ ar_shift(Number n1, Number n2, Number r, int dir)
 	}
       } else
       { if ( shift >= (long)sizeof(int64_t)*8 )
-	  r->value.i = 0;
+	  r->value.i = (r->value.i >= 0 ? 0 : -1);
 	else
 	  r->value.i = n1->value.i >> shift;
       }
