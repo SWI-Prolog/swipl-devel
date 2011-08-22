e4c535d041b13bc8030e92fc991e4479c9b85bc2
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2011-08-22 21:01:57 +0200

    Bug#8: Updated FSF address

--- boot/bags.pl
+++ boot/bags.pl
@@ -20,7 +20,7 @@
 
     You should have received a copy of the GNU General Public
     License along with this library; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
     As a special exception, if you link this library with other files,
     compiled with a Free Software compiler, to produce an executable, this
