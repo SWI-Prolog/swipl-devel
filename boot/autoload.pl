1976b307b0e1802a4916055df16dea7f49d2824f
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2011-09-27 13:01:09 +0200

    FIXED: Inconsistency in license text in the boot/*.pl files.  Paulo Moura.

--- boot/autoload.pl
+++ boot/autoload.pl
@@ -18,7 +18,7 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public
+    You should have received a copy of the GNU General Public
     License along with this library; if not, write to the Free Software
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
