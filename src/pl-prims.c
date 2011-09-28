SWI-Prolog/swipl-devel

commit 32bbcdac54c8bac721eeced8bd8c5d2d2a933d6d

FIXED: Possibly failing arg(-,+,?) due to left bindings of failed unification.

src/pl-prims.c
@@ -1978,27 +1978,36 @@ PRED_IMPL("arg", 3, arg, PL_FA_NONDETERMINISTIC)
     }
     case FRG_REDO:
     { term_t a;
+      fid_t fid;
+      int rc = FALSE;
 
       if ( !PL_get_name_arity(term, &name, &arity) )
 	sysError("arg/3: PL_get_name_arity() failed");
       argn = (int)CTX_INT + 1;
 
     genarg:
-      a = PL_new_term_ref();
+      if ( !(fid=PL_open_foreign_frame()) ||
+	   !(a = PL_new_term_ref()) )
+	return FALSE;
       for(; argn <= arity; argn++)
       { _PL_get_arg(argn, term, a);
 	if ( PL_unify(arg, a) )
 	{ if ( !PL_unify_integer(n, argn) )
-	    return FALSE;
+	    break;
 	  if ( argn == arity )
-	    succeed;
+	  { rc = TRUE;
+	    break;
+	  }
+	  PL_close_foreign_frame(fid);
 	  ForeignRedoInt(argn);
 	}
 	if ( exception_term )
-	  return FALSE;
+	  break;
+	PL_rewind_foreign_frame(fid);
       }
 
-      fail;
+      PL_close_foreign_frame(fid);
+      return rc;
     }
     default:
       succeed;

commit 700f43afbfd02f9e184093ed7c379a16ea4cf409

FIXED: Possible crash in arg(-,+,?).
Bug introduced in 32bbcdac54c8bac721eeced8bd8c5d2d2a933d6d

src/pl-prims.c
@@ -1976,13 +1976,14 @@ PRED_IMPL("arg", 3, arg, PL_FA_NONDETERMINISTIC)
     case FRG_REDO:
     { term_t a;
       fid_t fid;
-      int rc = FALSE;
+      int rc;
 
       if ( !PL_get_name_arity(term, &name, &arity) )
 	sysError("arg/3: PL_get_name_arity() failed");
       argn = (int)CTX_INT + 1;
 
     genarg:
+      rc = FALSE;
       if ( !(fid=PL_open_foreign_frame()) ||
 	   !(a = PL_new_term_ref()) )
 	return FALSE;