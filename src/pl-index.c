a56f150d1c275ecf273dc21b51f6d3008a1c78d1
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2011-09-29 17:22:39 +0200

    Another typo

--- src/pl-index.c
+++ src/pl-index.c
@@ -200,7 +200,7 @@ firstClause(Word argv, LocalFrame fr, Definition def, ClauseChoice chp ARG_LD)
       chp->key = indexOfWord(argv[ci->arg-1] PASS_LD);
       assert(chp->key);
       hi = hashIndex(chp->key, ci->buckets);
-      chp->cref = def->hash_info->entries[hi].head;
+      chp->cref = ci->entries[hi].head;
       return nextClauseArg1(chp, generationFrame(fr));
     }
   }
