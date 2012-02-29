SWI-Prolog/swipl-devel

commit 8d46e7e935dd3280d3441a6579683058173433ca

FIXED: Handle goal_expansion/2 for local meta-predicates
Goal expansion was not performed on <goal>  in the code below. Note that
the current behaviour  is  still   somewhat  troublesome  because  later
(auto-)importing relies on whether or not   a  predicate is defined. The
new  code  considers  a  goal  candidate  for   expansion  if  it  is  a
meta-predicate, whether defined or not. Maybe we should also immediately
import?

:- meta_predicate local(0).

test :- local(<goal>).

local(Goal) :- Goal.

boot/expand.pl
@@ -211,12 +211,10 @@ expand_goal(M:G, M:EG, _M, _MList, Term) :-
 			   '$set_source_module'(_, Old)).
 expand_goal(G0, G, M, MList, Term) :-
 	callable(G0),
-	functor(G0, N, A),
 	(   default_module(M, M2),
-	    current_predicate(M2:N/A)
-	->  true
+	    '$get_predicate_attribute'(M2:G0, meta_predicate, Head)
+	->  true		% is a meta-pred without clauses defined?
 	),
-	'$get_predicate_attribute'(M2:G0, meta_predicate, Head),
 	has_meta_arg(Head),
 	expand_meta(Head, G0, G, M, MList, Term),
 	G0 \== G, !.

src/Tests/core/test_expand.pl
@@ -18,6 +18,8 @@ test_expand :-
 :- dynamic
 	user:term_expansion/2,
 	user:goal_expansion/2.
+:- discontiguous
+	goal_expansion/2.
 
 user:term_expansion(b2c, final_c).
 
@@ -43,6 +45,22 @@ e_not :-
 
 g_c.
 
+% Test meta-predicate declaration before (local) definition of the
+% predicate.
+
+goal_expansion(foo, bar).
+
+:- meta_predicate
+	run(0).
+
+test_foo_bar :-
+	run(foo).
+
+bar.
+
+run(Goal) :- Goal.
+
+
 		 /*******************************
 		 *	       TESTS		*
 		 *******************************/
@@ -57,5 +75,7 @@ test(chained_term_expansion, B == true) :-
 	clause(final_c, B).
 test(meta_arg, [fail]) :-
 	e_not.
+test(goal_expansion_local_pred) :-
+	test_foo_bar.
 
 :- end_tests(expand).

commit 92eb4c2ae8bf0750396ec3dbb7c6cb8844a84dda

FIXED: Commit 8d46e7e935dd3280d3441a6579683058173433ca caused many undefined predicates to be created.

boot/expand.pl
@@ -212,6 +212,7 @@ expand_goal(M:G, M:EG, _M, _MList, Term) :-
 expand_goal(G0, G, M, MList, Term) :-
 	callable(G0),
 	(   default_module(M, M2),
+	    '$c_current_predicate'(_, M2:G0),
 	    '$get_predicate_attribute'(M2:G0, meta_predicate, Head)
 	->  true		% is a meta-pred without clauses defined?
 	),