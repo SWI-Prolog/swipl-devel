451515907091f4c6b8e19497991846fcad9b7959
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2012-03-19 14:01:54 +0100

    Added tests for qualified calls (:/2, @/2).

---
+++ src/Tests/core/test_qcall.pl
@@ -0,0 +1,56 @@
+:- module(test_qcall,
+	  [ test_qcall/0
+	  ]).
+:- use_module(library(plunit)).
+
+test_qcall :-
+	run_tests([ qualified_calls
+		  ]).
+
+:- op(900, xfx, @).
+
+:- module_transparent qm:qm/1.
+
+qm:qm(C) :-
+	context_module(C).
+
+m(qm).
+m2(qm2).
+g(A,qm(A)).
+
+:- begin_tests(qualified_calls).
+
+test(colon, C == qm) :-
+	qm:qm(C).
+test(colonc, C == qm) :-
+	qm:(qm(C),true).
+test(colonv, C == qm) :-
+	m(M),
+	M:qm(C).
+test(coloncv, C == qm) :-
+	m(M),
+	M:(true,qm(C)).
+test(at, C == qm2) :-
+	qm:qm(C)@qm2.
+test(vat, C == qm2) :-
+	m(M),
+	M:qm(C)@qm2.
+test(atv, C == C) :-
+	m2(QC),
+	qm:qm(C)@QC.
+test(atvv, C == C) :-
+	m(M),
+	m2(QC),
+	M:qm(C)@QC.
+test(atvvv, C == C) :-
+	m(M),
+	m2(QC),
+	g(C,G),
+	M:G@QC.
+test(atvvv, C == C) :-
+	m(M),
+	m2(QC),
+	g(C,G),
+	M:(G,true)@QC.
+
+:- end_tests(qualified_calls).
