b49599782ad73ecf8ca57b29754e3feb2185c71b
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2012-03-26 15:31:22 +0200

    Make term/goal expansion link clauses static

--- boot/load.pl
+++ boot/load.pl
@@ -43,7 +43,8 @@
 :- abolish('$expand_goal'/2),
    asserta(('$expand_goal'(In, Out) :- expand_goal(In, Out))),
    abolish('$expand_term'/2),
-   asserta(('$expand_term'(In, Out) :- expand_term(In, Out))).
+   asserta(('$expand_term'(In, Out) :- expand_term(In, Out))),
+   compile_predicates(['$expand_goal'/2, '$expand_term'/2]).
 
 :- consult([ license,			% requires DCG
 	     syspred,
