cfb19eb9b6efac174b862fe53b5c9a2c1352eaca
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2012-07-29 22:26:24 +0200

    Preparing version 6.1.10

--- src/SWI-Prolog.h
+++ src/SWI-Prolog.h
@@ -55,7 +55,7 @@ extern "C" {
 /* PLVERSION: 10000 * <Major> + 100 * <Minor> + <Patch> */
 
 #ifndef PLVERSION
-#define PLVERSION 60109
+#define PLVERSION 60110
 #endif
 
 		 /*******************************
