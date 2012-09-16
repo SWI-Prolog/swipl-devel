00fff370afd89fd13a7fb8be5f921c0133e5b233
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2012-09-16 10:21:08 +0200

    Added SICSTus emulation of library(terms)

--- src/common.mk
+++ src/common.mk
@@ -59,7 +59,7 @@ DCG=	basics.pl
 
 DIALECT=yap.pl hprolog.pl commons.pl ciao.pl sicstus.pl bim.pl ifprolog.pl
 YAP=	README.TXT
-SICSTUS=block.pl timeout.pl system.pl arrays.pl lists.pl \
+SICSTUS=block.pl timeout.pl system.pl arrays.pl lists.pl terms.pl \
 	sockets.pl swipl-lfr.pl
 CIAO=	assertions.pl isomodes.pl regtypes.pl sockets.pl \
 	read.pl write.pl strings.pl format.pl lists.pl \
