e036635382b570b784465501da42743d68adcdb3
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2012-10-23 21:38:28 +0200

    FIXED: Avoid LCO must be in get_prolog_backtrace/3.

--- library/prolog_stack.pl
+++ library/prolog_stack.pl
@@ -110,17 +110,21 @@ to decorate uncaught exceptions:
 %	[frame(Frame)]).
 
 get_prolog_backtrace(MaxDepth, Stack) :-
-	get_prolog_backtrace(MaxDepth, Stack, []),
+	get_prolog_backtrace_lc(MaxDepth, Stack, []).
+
+get_prolog_backtrace(Fr, MaxDepth, Stack) :-
+	integer(Fr), integer(MaxDepth), var(Stack), !,
+	get_prolog_backtrace_lc(MaxDepth, Stack, [frame(Fr)]),
+	nlc.
+get_prolog_backtrace(MaxDepth, Stack, Options) :-
+	get_prolog_backtrace_lc(MaxDepth, Stack, Options),
 	nlc.		% avoid last-call-optimization, such that
 			% the top of the stack is always a nice Prolog
 			% frame
 
 nlc.
 
-get_prolog_backtrace(Fr, MaxDepth, Stack) :-
-	integer(Fr), integer(MaxDepth), var(Stack), !,
-	get_prolog_backtrace(MaxDepth, Stack, [frame(Fr)]).
-get_prolog_backtrace(MaxDepth, Stack, Options) :-
+get_prolog_backtrace_lc(MaxDepth, Stack, Options) :-
 	(   option(frame(Fr), Options)
 	->  PC = call
 	;   prolog_current_frame(Fr0),
@@ -367,7 +371,7 @@ clause_predicate_name(Clause, PredName) :-
 %	Get and print a stacktrace to the user_error stream.
 
 backtrace(MaxDepth) :-
-	get_prolog_backtrace(MaxDepth, Stack, []),
+	get_prolog_backtrace_lc(MaxDepth, Stack, []),
 	print_prolog_backtrace(user_error, Stack).
 
 
