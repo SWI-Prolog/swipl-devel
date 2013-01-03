87220818b13413d30fe94565a686cbdef27548d2
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2013-01-03 17:03:13 +0100

    ADDED: Scurrent_input and Scurrent_output to SWI-Prolog.h

--- src/SWI-Prolog.h
+++ src/SWI-Prolog.h
@@ -785,9 +785,11 @@ PL_EXPORT(IOSTREAM *)	PL_open_resource(module_t m,
 
 PL_EXPORT(IOSTREAM *)*_PL_streams(void);	/* base of streams */
 #ifndef PL_KERNEL
-#define Suser_input  (_PL_streams()[0])
-#define Suser_output (_PL_streams()[1])
-#define Suser_error  (_PL_streams()[2])
+#define Suser_input     (_PL_streams()[0])
+#define Suser_output    (_PL_streams()[1])
+#define Suser_error     (_PL_streams()[2])
+#define Scurrent_input  (_PL_streams()[3])
+#define Scurrent_output (_PL_streams()[4])
 #endif
 
 #define PL_WRT_QUOTED		0x01	/* quote atoms */
