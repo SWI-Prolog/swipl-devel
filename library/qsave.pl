SWI-Prolog/swipl-devel

commit 8f6a3ee28795c0bed893974ad07d0122be67b81c

More cleanup to creating a saved state

library/qsave.pl
@@ -50,7 +50,22 @@ also used by the commandline sequence below.
 :- meta_predicate
 	qsave_program(+, :).
 
-% :- set_prolog_flag(generate_debug_info, false).
+:- predicate_options(qsave_program/2, 2,
+		     [ local(integer),
+		       global(integer),
+		       trail(integer),
+		       goal(callable),
+		       toplevel(callable),
+		       init_file(atom),
+		       class(oneof([runtime,kernel,development])),
+		       autoload(boolean),
+		       map(atom),
+		       op(oneof([save,standard])),
+		       stand_alone(boolean),
+		       emulator(atom)
+		     ]).
+
+:- set_prolog_flag(generate_debug_info, false).
 
 :- dynamic verbose/1.
 :- volatile verbose/1.			% contains a stream-handle
@@ -66,10 +81,6 @@ qsave_program(File) :-
 qsave_program(FileBase, Module:Options) :-
 	check_options(Options),
 	exe_file(FileBase, File),
-	option(autoload(Autoload),  Options, true),
-	option(map(Map),	    Options, []),
-	option(goal(GoalTerm),	    Options, []),
-	option(op(SaveOps),	    Options, save),
 	option(class(SaveClass),    Options, runtime),
 	option(init_file(InitFile), Options, DefInit),
 	default_init_file(SaveClass, DefInit),
@@ -82,11 +93,8 @@ qsave_program(FileBase, Module:Options) :-
 	    define_predicate(user:BannerGoal),
 	    Options2 = Options
 	),
-	(   Autoload == true
-	->  save_autoload
-	;   true
-	),
-	open_map(Map),
+	save_autoload(Options),
+	open_map(Options),
 	create_prolog_flag(saved_program, true, []),
 	create_prolog_flag(saved_program_class, SaveClass, []),
 	(   exists_file(File)
@@ -95,8 +103,7 @@ qsave_program(FileBase, Module:Options) :-
 	),
 	'$rc_open_archive'(File, RC),
 	make_header(RC, SaveClass, Options),
-	save_options(RC, [ class(SaveClass),
-			   init_file(InitFile)
+	save_options(RC, [ init_file(InitFile)
 			 | Options2
 			 ]),
 	save_resources(RC, SaveClass),
@@ -111,7 +118,7 @@ qsave_program(FileBase, Module:Options) :-
 	      save_flags,
 	      save_imports,
 	      save_prolog_flags,
-	      save_operators(SaveOps),
+	      save_operators(Options),
 	      save_format_predicates
 	    ),
 	    set_prolog_flag(access_level, OldLevel)),
@@ -305,8 +312,10 @@ define_predicate(Head) :-
 		 *	      AUTOLOAD		*
 		 *******************************/
 
-save_autoload :-
-	autoload.
+save_autoload(Options) :-
+	option(autoload(true),  Options, true), !,
+	autoload(Options).
+
 
 		 /*******************************
 		 *	       MODULES		*
@@ -519,12 +528,13 @@ restore_prolog_flag(Flag, Value, Type) :-
 		 *	     OPERATORS		*
 		 *******************************/
 
-%%	save_operators(+Save) is det.
+%%	save_operators(+Options) is det.
 %
 %	Save operators for all modules.   Operators for =system= are
 %	not saved because these are read-only anyway.
 
-save_operators(save) :- !,
+save_operators(Options) :- !,
+	option(op(save), Options, save),
 	feedback('~nOPERATORS~n', []),
 	forall(current_module(M), save_module_operators(M)),
 	feedback('~n', []).
@@ -561,11 +571,12 @@ qualify_head(T, user:T).
 		 *	       UTIL		*
 		 *******************************/
 
-open_map([]) :- !,
-	retractall(verbose(_)).
-open_map(File) :-
-	open(File, write, Fd),
+open_map(Options) :- !,
+	option(map(Map), Options), !,
+	open(Map, write, Fd),
 	asserta(verbose(Fd)).
+open_map(_) :-
+	retractall(verbose(_)).
 
 close_map :-
 	retract(verbose(Fd)),

man/runtime.doc
@@ -42,9 +42,8 @@ pairs. The available keys are described in \tabref{qsave-options}.
 local    & \bf -L       & K-bytes       & Size (Limit) of local stack \\
 global   & \bf -G       & K-bytes       & Size (Limit) of global stack \\
 trail    & \bf -T       & K-bytes       & Size (Limit) of trail stack \\
-argument & \bf -A       & K-bytes       & Size (Limit) of argument stack \\
-goal     & \bf -g       & atom          & Initialisation goal \\
-toplevel & \bf -t       & atom          & Prolog top-level goal \\
+goal     & \bf -g       & callable      & Initialisation goal \\
+toplevel & \bf -t       & callable      & Prolog top-level goal \\
 init_file& \bf -f       & atom          & Personal initialisation file \\
 \hline
 class	 &		& atom		& If \const{runtime}, only read
@@ -68,27 +67,20 @@ emulator &		& file		& Emulator attached to the
 \end{tabular}
 \end{center}
 
-    \caption{<Key> = <Value> pairs for qsave_program/2}
+    \caption{Options for qsave_program/2}
     \label{tab:qsave-options}
 \end{table}
 
-Before writing the data to file, qsave_program/2 will run autoload/0 to
-all required autoloading the system can discover.  See autoload/0.
-
 Provided the application does not require any of the Prolog libraries to
 be loaded at runtime, the only file from the SWI-Prolog development
-environment required is the emulator itself.  The emulator may be built
-in two flavours.  The default is the \jargon{development emulator}.  The
-\jargon{runtime emulator} is similar, but lacks the tracer.
+environment required is the emulator itself.
 
 If the option \exam{stand_alone(true)} is present, the emulator is the
 first part of the state. If the emulator is started it will test whether
 a boot-file (state) is attached to the emulator itself and load this
 state. Provided the application has all libraries loaded, the resulting
 executable is completely independent of the runtime environment or
-location where it was build.
-
-See also \secref{cmdlinecomp}.
+location where it was build.  See also \secref{cmdlinecomp}.
 
     \predicate{qsave_program}{1}{+File}
 Equivalent to \exam{qsave_program(File, [])}.

commit ebf6281d782f9d6c60087ef0fcd2eb9760af6219

FIXED: qsave_program/2 failed with autoload(false)
The cleanup in commit 8f6a3ee28795c0bed893974ad07d0122be67b81c caused
qsave_program/2 to fail anytime the autoload option was set to false.
This restores the intended behavior of skipping autoload/0 in that case.

library/qsave.pl
@@ -333,6 +333,7 @@ save_autoload(Options) :-
 	define_toplevel_goal(Options),
 	option(autoload(true),  Options, true), !,
 	autoload(Options).
+save_autoload(_).
 
 
 		 /*******************************