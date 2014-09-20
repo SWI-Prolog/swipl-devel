36adac2f26b389ec50a306885f28d083f6134105
Markus Triska <markus.triska@gmx.at>
2014-09-20 09:53:18 +0200

    better naming in transpose/2 and related predicates

--- library/clp/clpfd.pl
+++ library/clp/clpfd.pl
@@ -6110,17 +6110,17 @@ transpose(Ms, Ts) :-
         must_be(list(list), Ms),
         (   Ms = [] -> Ts = []
         ;   Ms = [F|_],
-            transpose(F, Ms, Ts)
+            transpose_(F, Ms, Ts)
         ).
 
-transpose([], _, []).
-transpose([_|Rs], Ms, [Ts|Tss]) :-
-        lists_firsts_rests(Ms, Ts, Ms1),
-        transpose(Rs, Ms1, Tss).
+transpose_([], _, []).
+transpose_([_|Es], Lists0, [Fs|Fss]) :-
+        lists_firsts_rests(Lists0, Fs, Lists),
+        transpose_(Es, Lists, Fss).
 
 lists_firsts_rests([], [], []).
-lists_firsts_rests([[F|Os]|Rest], [F|Fs], [Os|Oss]) :-
-        lists_firsts_rests(Rest, Fs, Oss).
+lists_firsts_rests([[F|Rs]|Lists], [F|Fs], [Rs|Rss]) :-
+        lists_firsts_rests(Lists, Fs, Rss).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
