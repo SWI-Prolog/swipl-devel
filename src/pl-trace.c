9673e4df788748645b464df167055d2d12918af1
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2014-10-09 13:08:30 +0200

    FIXED: trace: skip on retry of previously skipped goal showed the same port twice.  Kilian Evang.

--- src/pl-trace.c
+++ src/pl-trace.c
@@ -423,17 +423,7 @@ Give a trace on the skipped goal for a redo.
       debugstatus.skiplevel = levelFrame(fr);
       set(fr, FR_SKIPPED);		/* cleared by "case 'c'" */
 
-      switch( rc )
-      { case ACTION_CONTINUE:
-	  if ( debugstatus.skiplevel < levelFrame(frame) )
-	    return ACTION_CONTINUE;
-	  break;
-	case ACTION_RETRY:
-	case ACTION_IGNORE:
-	case ACTION_FAIL:
-	  Sfprintf(Sdout, "Action not yet implemented here\n");
-	  break;
-      }
+      return rc;
     }
   }
 
