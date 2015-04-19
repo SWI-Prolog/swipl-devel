9d2d1ac6ca78479d638e37ae1545919f261760da
Markus Triska <markus.triska@gmx.at>
2015-04-19 12:48:56 +0200

    FIXED: CLP(B): Avoid unintended failure with Boolean second argument        of sat_count/2 in some cases. A query like:           ?- sat(A*B + C*D), clpb:sat_count(~A * ~D, C).        failed unexpectedly, because unifying C prematurely triggered        propagation in a state of BDDs that is not prepared for this.        With this commit, the query is made equivalent to:           ?- sat(A*B + C*D), clpb:sat_count(~A * ~D, Count), Count = C.        thus succeeding with:           A = B, B = 1,           C = 0,           sat(D=:=D).

--- library/clp/clpb.pl
+++ library/clp/clpb.pl
@@ -834,9 +834,9 @@ sat_count(Sat0, N) :-
                foldl(renumber_variable, IVs, 1, VNum),
                bdd_count(BDD2, VNum, Count0),
                var_u(BDD2, VNum, P),
-               N is 2^(P - 1)*Count0,
+               N0 is 2^(P - 1)*Count0,
                % reset all attributes and Aux variables
-               throw(count(N))),
+               throw(count(N0))),
               count(N),
               true).
 
