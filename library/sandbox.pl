01ad1e90ff6c6cc6ef49db6030f32fb85cbf0601
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2015-10-29 20:38:10 +0100

    ADDED: library(sandbox): allow variant_hash/2.

--- library/sandbox.pl
+++ library/sandbox.pl
@@ -487,6 +487,7 @@ safe_primitive(subsumes_term(_,_)).
 safe_primitive(system:term_hash(_,_)).
 safe_primitive(system:term_hash(_,_,_,_)).
 safe_primitive(system:variant_sha1(_,_)).
+safe_primitive(system:variant_hash(_,_)).
 safe_primitive(system:'$term_size'(_,_,_)).
 
 					% dicts
