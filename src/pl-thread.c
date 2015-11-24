117eb29997858cc0eb1e3c49c602200d6fd055b7
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2015-11-24 11:19:58 +0100

    FIXED: Possible race in markAccessedPredicates().  Wouter Beek.

--- src/pl-thread.c
+++ src/pl-thread.c
@@ -5694,7 +5694,8 @@ markAccessedPredicates(PL_local_data_t *ld)
     DirtyDefInfo ddi;
     definition_ref dref = refs->blocks[idx][i];
 
-    if ( (ddi=lookupHTable(GD->procedures.dirty, dref.predicate)) )
+    if ( dref.predicate &&
+	 (ddi=lookupHTable(GD->procedures.dirty, dref.predicate)) )
     { if ( dref.generation < ddi->oldest_generation )
 	ddi->oldest_generation = dref.generation;
     }
