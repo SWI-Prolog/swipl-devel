401de979740bd476753ee9ab6dbb929aede338bf
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2015-12-15 16:37:40 +0100

    Added '$malloc_info'/1 when compiled for debugging to get additional statistics on malloc() on systems using gnu malloc.

--- src/pl-prims.c
+++ src/pl-prims.c
@@ -5154,13 +5154,16 @@ PRED_IMPL("statistics", 2, statistics, 0)
   return pl_statistics_ld(A1, A2, LD PASS_LD);
 }
 
+#ifdef O_DEBUG
+#define O_MEMSTATS 1
+#endif
 
 #ifdef O_MEMSTATS
+#define O_MEMORY_STATISTICS 1
 
 static int
-addNameInteger(term_t list, const char *name, intptr_t val)
-{ GET_LD
-  term_t head = PL_new_term_ref();
+addNameInteger(term_t list, const char *name, intptr_t val ARG_LD)
+{ term_t head = PL_new_term_ref();
 
   if ( !PL_unify_list(list, head, list) )
     return FALSE;
@@ -5190,19 +5193,37 @@ PRED_IMPL("memory_statistics", 1, memory_statistics, 0)
 #ifdef HAVE_MALLINFO
   { struct mallinfo info = mallinfo();
 
-    addNameInteger(tail, "arena",    info.arena);
-    addNameInteger(tail, "ordblks",  info.ordblks);
-    addNameInteger(tail, "hblks",    info.hblks);
-    addNameInteger(tail, "hblkhd",   info.hblkhd);
-    addNameInteger(tail, "uordblks", info.uordblks);
-    addNameInteger(tail, "fordblks", info.fordblks);
-    addNameInteger(tail, "keepcost", info.keepcost);
+    addNameInteger(tail, "arena",    info.arena	   PASS_LD);
+    addNameInteger(tail, "ordblks",  info.ordblks  PASS_LD);
+    addNameInteger(tail, "hblks",    info.hblks	   PASS_LD);
+    addNameInteger(tail, "hblkhd",   info.hblkhd   PASS_LD);
+    addNameInteger(tail, "uordblks", info.uordblks PASS_LD);
+    addNameInteger(tail, "fordblks", info.fordblks PASS_LD);
+    addNameInteger(tail, "keepcost", info.keepcost PASS_LD);
   }
 #endif
 
   return PL_unify_nil(tail);
 }
 
+#if defined(HAVE_OPEN_MEMSTREAM) && defined(HAVE_MALLOC_INFO)
+#define O_DMALLOC_INFO 1
+
+static
+PRED_IMPL("$malloc_info", 1, malloc_info, 0)
+{ char *data = NULL;
+  size_t len = 0;
+  FILE *fp;
+
+  if ( (fp=open_memstream(&data, &len)) )
+  { return ( malloc_info(0, fp) == 0 &&
+	     fclose(fp) == 0 &&
+	     PL_unify_chars(A1, PL_STRING, len, data) );
+  }
+
+  return PL_error("malloc_info", 1, MSG_ERRNO, ERR_SYSCALL, "open_memstream");
+}
+#endif /*defined(HAVE_OPEN_MEMSTREAM) && defined(HAVE_MALLOC_INFO)*/
 #endif /*O_MEMSTATS*/
 
 
@@ -5475,7 +5496,10 @@ BeginPredDefs(prims)
   PRED_DEF("nb_linkarg", 3, nb_linkarg, 0)
   PRED_DEF("$skip_list", 3, skip_list, 0)
   PRED_DEF("throw", 1, throw, PL_FA_ISO)
-#ifdef O_MEMSTATS
+#ifdef O_MEMORY_STATISTICS
   PRED_DEF("memory_statistics", 1, memory_statistics, 0)
+#endif
+#ifdef O_DMALLOC_INFO
+  PRED_DEF("$malloc_info", 1, malloc_info, 0)
 #endif
 EndPredDefs
