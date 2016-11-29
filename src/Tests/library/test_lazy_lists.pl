c72510870191196270b7751d042687bfe5177cd5
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2016-11-29 18:05:34 +0100

    Fixed test case for library(lazy_lists).

--- src/Tests/library/test_lazy_lists.pl
+++ src/Tests/library/test_lazy_lists.pl
@@ -1,7 +1,37 @@
+/*  Part of SWI-Prolog
+
+    Author:        Jan Wielemaker
+    E-mail:        J.Wielemaker@uva.nl
+    WWW:           http://www.swi-prolog.org
+    Copyright (C): 2016, University of Amsterdam
+			 CWI, Amsterdam
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+    As a special exception, if you link this library with other files,
+    compiled with a Free Software compiler, to produce an executable, this
+    library does not by itself cause the resulting executable to be covered
+    by the GNU General Public License. This exception does not however
+    invalidate any other reasons why the executable file might be covered by
+    the GNU General Public License.
+*/
+
 :- module(test_lazy_lists,
 	  [ test_lazy_lists/0
 	  ]).
-:- use_module(lazy_lists).
+:- use_module(library(lazy_lists)).
 :- use_module(library(plunit)).
 :- use_module(library(dcg/basics)).
 
