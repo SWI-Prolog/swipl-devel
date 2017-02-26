6555948feaadcb2f6c191fe60b5e2ab2cd5d29e5
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2017-02-26 10:43:58 +0100

    ADDED: PL_release_stream_noeror(), needed to fix ws_property/2 in the websocket library.

--- src/os/pl-file.c
+++ src/os/pl-file.c
@@ -457,6 +457,13 @@ PL_release_stream(IOSTREAM *s)
 { return streamStatus(s);
 }
 
+int
+PL_release_stream_noerror(IOSTREAM *s)
+{ releaseStream(s);
+
+  return TRUE;
+}
+
 
 		 /*******************************
 		 *	      ERRORS		*
