SWI-Prolog/swipl-devel

commit 038d86b08bfecc0715b4548a6ec3f2d91c20b658

FIXED: CLP(FD): Goal expansion for undefined ground expressions. Jan Burse.
For example, the following would lead to an exception at compilation time:

    undef(Z) :- Z #= 1//0.

It now works as expected. I have taken the opportunity to improve also
other cases where the arguments are fully instantiated.

For example:

    n(X) :- X #= 1+3.

    %?- listing(n/1).
    %@ n(4) :-
    %@      (   integer(4)
    %@      ->  4=:=1+3
    %@      ;   fail
    %@      ->  true
    %@      ;   clpfd:clpfd_equal(4, 4)
    %@      ).
    %@
    %@ true.

Note that 4 is now propagated into the clause head.

library/clp/clpfd.pl
@@ -2759,10 +2759,21 @@ expansion_simpler((False->_;Else0), Else) :-
 expansion_simpler((If->Then0;Else0), (If->Then;Else)) :- !,
         expansion_simpler(Then0, Then),
         expansion_simpler(Else0, Else).
-expansion_simpler((Var is Expr,Goal), Goal) :-
-        ground(Expr), !,
-        Var is Expr.
-expansion_simpler((Var is Expr,Goal), (Var = Expr,Goal)) :- var(Expr), !.
+expansion_simpler((A0,B0), (A,B)) :-
+        expansion_simpler(A0, A),
+        expansion_simpler(B0, B).
+expansion_simpler(Var is Expr0, Goal) :-
+        ground(Expr0), !,
+        phrase(expr_conds(Expr0, Expr), Gs),
+        (   maplist(call, Gs) -> Var is Expr, Goal = true
+        ;   Goal = false
+        ).
+expansion_simpler(Var =:= Expr0, Goal) :-
+        ground(Expr0), !,
+        phrase(expr_conds(Expr0, Expr), Gs),
+        (   maplist(call, Gs) -> Goal = (Var =:= Expr)
+        ;   Goal = false
+        ).
 expansion_simpler(Var is Expr, Var = Expr) :- var(Expr), !.
 expansion_simpler(between(L,U,V), Goal) :- maplist(integer, [L,U,V]), !,
         (   between(L,U,V) -> Goal = true

commit fc38bec83d47c5b49babee5949cd5a9d7a97a2fc

FIXED: CLP(FD): Overeager goal expansion. Julien Cumin.
This mistake was introduced in 038d86b08bfecc0715b4548a6ec3f2d91c20b658.

A new binding *must not* be dragged outside of disjunctions, since the
code may look for example like this:

        i(X) :-
            (   X #= 3
            ;   X #= 4
            ).

In fact, this would previously not even compile, and instead raise:

    put_attr/3: Uninstantiated argument expected, found 3 (1-st argument)

already during goal expansion.

This commit fixes this issue, and still rewrites CLP(FD) expressions
as far as possible already at compilation time.

For example:

    n(X) :- X #= 1+3.

This is now compiled to (note that 1+3 is evaluated to 4):

    ?- listing(n/1).
    %@ n(A) :-
    %@      (   integer(A)
    %@      ->  A=:=4
    %@      ;   var(A)
    %@      ->  A=4
    %@      ;   B=4,
    %@          clpfd:clpfd_equal(A, B)
    %@      ).

Ideally, it should be compiled to:

    n(4).

library/clp/clpfd.pl
@@ -2910,13 +2910,13 @@ expansion_simpler((A0,B0), (A,B)) :-
 expansion_simpler(Var is Expr0, Goal) :-
         ground(Expr0), !,
         phrase(expr_conds(Expr0, Expr), Gs),
-        (   maplist(call, Gs) -> Var is Expr, Goal = true
+        (   maplist(call, Gs) -> Value is Expr, Goal = (Var = Value)
         ;   Goal = false
         ).
 expansion_simpler(Var =:= Expr0, Goal) :-
         ground(Expr0), !,
         phrase(expr_conds(Expr0, Expr), Gs),
-        (   maplist(call, Gs) -> Goal = (Var =:= Expr)
+        (   maplist(call, Gs) -> Value is Expr, Goal = (Var =:= Value)
         ;   Goal = false
         ).
 expansion_simpler(Var is Expr, Var = Expr) :- var(Expr), !.