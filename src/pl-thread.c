ac1d6d8b92cd91804b432dbb0b3ba88e8b13d60c
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2017-11-03 12:53:08 +0100

    FIXED: Engine symbol's are not referenced to allow reclaiming engines in atom-gc.  Thus, we should also not unregister them on completion.

--- src/pl-thread.c
+++ src/pl-thread.c
@@ -2241,7 +2241,7 @@ free_thread_info(PL_thread_info_t *info)
 	unalias_thread(th);
     }
 
-    if ( info->detached )
+    if ( info->detached && !info->is_engine )
       PL_unregister_atom(info->symbol);
   }
 
