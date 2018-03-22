2a7acb00545420e1f6110bf653bb89eb60c5a326
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-03-22 22:01:44 +0100

    Preparing version 7.7.11

--- src/SWI-Prolog.h
+++ src/SWI-Prolog.h
@@ -68,7 +68,7 @@ extern "C" {
 /* PLVERSION_TAG: a string, normally "", but for example "rc1" */
 
 #ifndef PLVERSION
-#define PLVERSION 70710
+#define PLVERSION 70711
 #endif
 #ifndef PLVERSION_TAG
 #define PLVERSION_TAG ""
