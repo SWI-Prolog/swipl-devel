b7a5eba77bd8a6302c3f6b18c6f4f079428a10e3
Jan Wielemaker <J.Wielemaker@vu.nl>
2018-04-09 14:55:15 -0700

    Updated resource primitives

--- boot/rc.pl
+++ boot/rc.pl
@@ -3,7 +3,8 @@
     Author:        Jan Wielemaker
     E-mail:        J.Wielemaker@vu.nl
     WWW:           http://www.swi-prolog.org
-    Copyright (c)  1998-2011, University of Amsterdam
+    Copyright (c)  1998-2018, University of Amsterdam
+                              CWI, Amsterdam
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
@@ -33,51 +34,61 @@
 */
 
 :- module('$rc',
-          [ open_resource/3,            % +Name, ?Class, -Stream
-            open_resource/4,            % +Name, ?Class, +RW, -Stream
-            current_resource/3          % :Name, ?Class, ?File
+          [ open_resource/2,            % +Name, -Stream
+            open_resource/3,            % +Name, +RW, -Stream
+            current_resource/2          % :Name, ?File
           ]).
 
 :- meta_predicate
-    open_resource(:, ?, -),
-    open_resource(:, ?, +, -),
+    open_resource(:, -),
+    open_resource(:, -, +),
     current_resource(:, ?, ?).
 
 :- dynamic
+    user:resource/2,
     user:resource/3.
 :- multifile
+    user:resource/2,
     user:resource/3.
 
-%!  open_resource(:Name, ?Class, -Handle) is det.
-%!  open_resource(:Name, ?Class, +Mode, -Handle) is det.
+%!  open_resource(:Name, -Stream) is det.
+%!  open_resource(:Name, -Stream, +Options) is det.
+%!  open_resource(:Name, ?Class, -Stream) is det.
 %
-%   Open resource with given Name  and   Class,  returning  a stream
-%   handle.
+%   Open resource with given Name, returning a Stream.
 
-open_resource(Name, Class, Handle) :-
-    open_resource(Name, Class, read, Handle).
+open_resource(Name, Handle) :-
+    open_resource(Name, Handle, []).
 
-open_resource(Module:RcName, Class, RW, Handle) :-
+open_resource(Module:RcName, Stream, Options) :-
+    is_list(Options),
+    !,
     (   default_module(Module, RModule),
-        current_resource(RModule:RcName, Class, FileSpec)
+        current_resource(RModule:RcName, FileSpec)
     ->  absolute_file_name(FileSpec, File),
-        open(File, RW, Handle, [type(binary)])
-    ;   '$rc_handle'(RC),
+        open(File, read, Stream, Options)
+    ;   '$rc_handle'(Zipper),
         tag_rc_name(Module, RcName, TaggedName),
-        '$rc_open'(RC, TaggedName, Class, RW, Handle)
+        zip_goto(Zipper, file(TaggedName)),
+        zip_open_current(Zipper, Stream, Options)
     ).
+open_resource(Name, _Class, Stream) :-
+    open_resource(Name, Stream).
 
 tag_rc_name(user, RcName, RcName) :- !.
 tag_rc_name(Module, RcName, TaggedName) :-
     atomic_list_concat([Module, ':', RcName], TaggedName).
 tag_rc_name(_, RcName, RcName).
 
-%!  current_resource(:Name, ?Class, ?File) is nondet.
+%!  current_resource(:Name, ?File) is nondet.
 %
 %   List all currently declared resources.   Should  eventually deal
 %   with resources that are already part of the state.
 
-current_resource(M:Name, Class, File) :-
+current_resource(M:Name, File) :-
     current_module(M),
-    current_predicate(M:resource/3),
-    M:resource(Name, Class, File).
+    (   current_predicate(M:resource/2),
+        M:resource(Name, File)
+    ;   current_predicate(M:resource/3),
+        M:resource(Name, _Class, File)
+    ).
