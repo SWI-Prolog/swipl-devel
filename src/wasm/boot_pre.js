199ba7c28a5d4b859680b3d27298f02af7f7ac26
Raivo Laanemets <raivo@infdot.com>
2018-07-25 22:12:34 +0300

    Use NODERAWFS during wasm build

--- src/wasm/boot_pre.js
+++
@@ -1,6 +0,0 @@
-// Emscripten pre-js to mount the actual
-// filesystem as NODEFS.
-Module.onRuntimeInitialized = () => {
-  FS.mkdir('/swipl-devel');
-  FS.mount(NODEFS, { root: '..' }, '/swipl-devel');
-};
