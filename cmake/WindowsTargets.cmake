5f4b0a9dd08ccbc218b8a9f976d81a74f8d0546e
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-10-15 11:49:25 +0200

    Various Windows issues

--- cmake/WindowsTargets.cmake
+++ cmake/WindowsTargets.cmake
@@ -7,7 +7,7 @@ add_subdirectory(win32/console)
 add_executable(swipl-win WIN32
 	       pl-ntmain.c swipl.rc)
 target_c_stack(swipl-win 4000000)
-target_link_libraries(swipl-win libswipl libplterm)
+target_link_libraries(swipl-win libswipl libplterm ${CMAKE_THREAD_LIBS_INIT})
 target_include_directories(swipl-win BEFORE PRIVATE
 			   ${CMAKE_CURRENT_BINARY_DIR}
 			   ${CMAKE_CURRENT_SOURCE_DIR})
