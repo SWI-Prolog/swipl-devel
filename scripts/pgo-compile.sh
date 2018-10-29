fcdd132f58891d1b778a3a3f102ac156bccfc939
Jan Wielemaker <J.Wielemaker@vu.nl>
2018-10-29 21:09:27 +0100

    scripts/pgo-compile.sh: support --off

--- scripts/pgo-compile.sh
+++ scripts/pgo-compile.sh
@@ -2,16 +2,23 @@ CMAKE_COMMAND=cmake
 PGO_DIR=PGO-data
 PGO_PROGRAM=../bench/run.pl
 
-${CMAKE_COMMAND} -DPROFILE_GUIDED_OPTIMIZATION=GENERATE -G Ninja ..
-ninja -t clean libswipl
-${CMAKE_COMMAND} -E echo "PGO: Compiling instrumented version"
-ninja prolog_products
-${CMAKE_COMMAND} -E echo "PGO: Running program"
-${CMAKE_COMMAND} -E remove_directory ${PGO_DIR}
-src/swipl ${PGO_PROGRAM}
-${CMAKE_COMMAND} -DPROFILE_GUIDED_OPTIMIZATION=USE -G Ninja ..
-ninja -t clean libswipl
-${CMAKE_COMMAND} -E echo "PGO: Compiling optimized version"
-ninja prolog_products
-# ${CMAKE_COMMAND} -DPROFILE_GUIDED_OPTIMIZATION=OFF -G Ninja ..
-# ${CMAKE_COMMAND} -E remove_directory ${PGO_DIR}
+if [ "$1" = "--off" ]; then
+  ${CMAKE_COMMAND} -E echo "PGO: disabling"
+  ${CMAKE_COMMAND} -DPROFILE_GUIDED_OPTIMIZATION=OFF -G Ninja ..
+  ${CMAKE_COMMAND} -E remove_directory ${PGO_DIR}
+  ninja -t clean libswipl
+  ${CMAKE_COMMAND} -E echo "PGO: run ninja to complete rebuild"
+else
+  ${CMAKE_COMMAND} -DPROFILE_GUIDED_OPTIMIZATION=GENERATE -G Ninja ..
+  ninja -t clean libswipl
+  ${CMAKE_COMMAND} -E echo "PGO: Compiling instrumented version"
+  ninja prolog_products
+  ${CMAKE_COMMAND} -E echo "PGO: Running program"
+  ${CMAKE_COMMAND} -E remove_directory ${PGO_DIR}
+  src/swipl ${PGO_PROGRAM}
+  ${CMAKE_COMMAND} -DPROFILE_GUIDED_OPTIMIZATION=USE -G Ninja ..
+  ninja -t clean libswipl
+  ${CMAKE_COMMAND} -E echo "PGO: Compiling optimized core"
+  ninja prolog_products
+  ${CMAKE_COMMAND} -E echo "PGO: run ninja to complete the build"
+fi
