99f90b2708a075566bfb88e8108301d4640a75eb
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-11-16 14:57:43 +0100

    Set CMAKE_HOST_EXECUTABLE_SUFFIX to support finding native tools when cross compiling.

--- cmake/Ports.cmake
+++ cmake/Ports.cmake
@@ -9,3 +9,12 @@ elseif(WIN32)
 elseif(EMSCRIPTEN)
   include(port/Emscripten)
 endif()
+
+# See https://gitlab.kitware.com/cmake/cmake/issues/17553
+# For now we assume we do not cross-compile from Windows
+
+if(CMAKE_CROSSCOMPILING)
+  set(CMAKE_HOST_EXECUTABLE_SUFFIX "")
+else()
+  set(CMAKE_HOST_EXECUTABLE_SUFFIX ${CMAKE_EXECUTABLE_SUFFIX})
+endif()
