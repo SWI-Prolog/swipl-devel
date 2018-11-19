cb65727ad92b035cf0c2467f62195bc6c8fadcc3
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-11-19 11:45:18 +0100

    PORT: Introduce CMake variable ${SWIPL_NATIVE_FRIEND} to specify a native Prolog for doing the build steps that require Prolog.

--- cmake/Ports.cmake
+++ cmake/Ports.cmake
@@ -10,14 +10,48 @@ elseif(EMSCRIPTEN)
   include(port/Emscripten)
 endif()
 
-# See https://gitlab.kitware.com/cmake/cmake/issues/17553
-# For now we assume we do not cross-compile from Windows
+# Setup cross compiling.  As discussed with _erlanger_ in
+# https://github.com/SWI-Prolog/swipl-devel/issues/358#issuecomment-439675854
+# we distinguish three scenarios for building:
+#
+#   1. Native compilation
+#   2. Cross compilation with a good emulator (Wine for Windows, Node.js
+#      for WASM)
+#   3. Cross compilation without a usable emulator.
+#
+# The helper tools mkvmi and  defatoms   (see  src  directory) are build
+# using ${CMAKE_HOST_CC} when cross-compiling.
+#
+# Prolog build steps may be executed using ${SWIPL_NATIVE_FRIEND}, which
+# is  either  the  name  of  a  sibling  build  directory  containing  a
+# compatible native SWI-Prolog or an absolute   path  pointing at such a
+# directory.
 
 if(CMAKE_CROSSCOMPILING)
   set(CMAKE_HOST_EXECUTABLE_SUFFIX "" CACHE STRING
       "File name extension for executables for the host")
   set(CMAKE_HOST_CC cc CACHE STRING
       "C compiler used to compile build tools while cross-compiling")
-else()
+
+  set(SWIPL_NATIVE_FRIEND "" CACHE STRING
+      "CMake binary directory holding a compatible native system")
+
+  if(SWIPL_NATIVE_FRIEND)
+    if(IS_ABSOLUTE ${SWIPL_NATIVE_FRIEND})
+      set(SWIPL_NATIVE_FRIEND_DIR ${SWIPL_NATIVE_FRIEND})
+    else()
+      set(SWIPL_NATIVE_FRIEND_DIR ${CMAKE_SOURCE_DIR}/${SWIPL_NATIVE_FRIEND})
+    endif()
+
+    set(PROG_SWIPL
+	${SWIPL_NATIVE_FRIEND_DIR}/src/swipl${CMAKE_HOST_EXECUTABLE_SUFFIX}
+	CACHE STRING "SWI-Prolog executable to perform build tasks")
+  else()
+    set(PROG_SWIPL swipl
+	CACHE STRING "SWI-Prolog executable to perform build tasks")
+  endif()
+else(CMAKE_CROSSCOMPILING)
   set(CMAKE_HOST_EXECUTABLE_SUFFIX ${CMAKE_EXECUTABLE_SUFFIX})
-endif()
+  set(PROG_SWIPL swipl)
+endif(CMAKE_CROSSCOMPILING)
+
