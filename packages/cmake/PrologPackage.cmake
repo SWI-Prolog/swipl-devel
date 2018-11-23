a113114e5432e5eba1b6ca1cec72f0059577984d
Keri Harris <keri@gentoo.org>
2018-11-23 11:17:25 +0100

    PORT: support older versions of cmake for cross-compiling to Windows

--- packages/cmake/PrologPackage.cmake
+++ packages/cmake/PrologPackage.cmake
@@ -305,7 +305,7 @@ function(test_lib name)
   endforeach()
 
   add_test(NAME "${SWIPL_PKG}:${test_name}"
-	   COMMAND swipl -p "foreign=${pforeign}"
+	   COMMAND ${PROG_SWIPL} -p "foreign=${pforeign}"
 			 -p "library=${plibrary}"
 			 -f none -s ${test_source}
 			 -g "${test_goal}"
