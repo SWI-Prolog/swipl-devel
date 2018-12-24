b7f29923dba268b80a5f33d9944faa0957ac4d2d
Leonardo Taccari <iamleot@gmail.com>
2018-12-24 12:51:09 +0100

    PORT: Define {fopen,ftello,fseeko}64 functions for NetBSD

--- src/minizip/ioapi.h
+++ src/minizip/ioapi.h
@@ -50,7 +50,7 @@
 #define ftello64 ftell
 #define fseeko64 fseek
 #else
-#if defined(__FreeBSD__) || defined(__OpenBSD__)
+#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
 #define fopen64 fopen
 #define ftello64 ftello
 #define fseeko64 fseeko
