4b267258be28c0b959b102c17662d2efc9b26356
Jan Wielemaker <J.Wielemaker@vu.nl>
2019-02-07 21:21:59 +0100

    BUILD: Allow scripts/pgo-compile.sh to run from any build dir, also if this is not an immediate subdir of the source.  Paulo Moura.

--- scripts/pgo-compile.sh
+++ scripts/pgo-compile.sh
@@ -1,15 +1,16 @@
 CMAKE_COMMAND=cmake
 PGO_DIR=PGO-data
-PGO_PROGRAM=../bench/run.pl
+SRC_DIR="$(dirname "$(dirname "$0")")"
+PGO_PROGRAM="${SRC_DIR}/bench/run.pl"
 
 if [ "$1" = "--off" ]; then
   ${CMAKE_COMMAND} -E echo "PGO: disabling"
-  ${CMAKE_COMMAND} -DPROFILE_GUIDED_OPTIMIZATION=OFF -G Ninja ..
+  ${CMAKE_COMMAND} -DPROFILE_GUIDED_OPTIMIZATION=OFF -G Ninja ${SRC_DIR}
   ${CMAKE_COMMAND} -E remove_directory ${PGO_DIR}
   ninja -t clean libswipl
   ${CMAKE_COMMAND} -E echo "PGO: run ninja to complete rebuild"
 else
-  ${CMAKE_COMMAND} -DPROFILE_GUIDED_OPTIMIZATION=GENERATE -G Ninja ..
+  ${CMAKE_COMMAND} -DPROFILE_GUIDED_OPTIMIZATION=GENERATE -G Ninja ${SRC_DIR}
   ninja -t clean libswipl
   ${CMAKE_COMMAND} -E echo "PGO: Compiling instrumented version"
   ninja prolog_products
@@ -24,7 +25,7 @@ else
 
   ${SWIPL} -f none --no-threads ${PGO_PROGRAM}
 
-  ${CMAKE_COMMAND} -DPROFILE_GUIDED_OPTIMIZATION=USE -G Ninja ..
+  ${CMAKE_COMMAND} -DPROFILE_GUIDED_OPTIMIZATION=USE -G Ninja ${SRC_DIR}
   ninja -t clean libswipl
   ${CMAKE_COMMAND} -E echo "PGO: Compiling optimized core"
   ninja prolog_products
