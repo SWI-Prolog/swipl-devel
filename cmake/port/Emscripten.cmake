0b6db54da956d03e29f3f26b029e3f51c0addeb9
Jan Wielemaker <J.Wielemaker@vu.nl>
2019-02-19 23:22:21 -0500

    PORT: Make alignment detection work for Emscripten.

--- cmake/port/Emscripten.cmake
+++ cmake/port/Emscripten.cmake
@@ -1,3 +1,8 @@
 # We could set set linker flags for all excutables if we
 # could override them for the browser binary.
 #set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -s ASSERTIONS=2 -s NODERAWFS=1 -s EXIT_RUNTIME=1")
+
+# AlignOf.cmake doesn't work for Emscripten
+set(ALIGNOF_INT64_T 8 CACHE STRING "Alignment for int64_t")
+set(ALIGNOF_VOIDP   ${SIZEOF_VOIDP} CACHE STRING "Alignment for pointers")
+set(ALIGNOF_DOUBLE  8 CACHE STRING "Alignment for double")
