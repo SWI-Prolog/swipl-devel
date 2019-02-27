7899f717b30f703325a4d7dbd5145385b0ebf00a
Sebastian Sardina <ssardina@gmail.com>
2019-02-27 18:55:38 +1100

    Improved checking of conflicting env variables for CMAKE

--- cmake/CheckEnv.cmake
+++ cmake/CheckEnv.cmake
@@ -1,42 +1,45 @@
 function(conflicting_var name)
   message(WARNING
-	  "-- $${name} is set to $ENV{${name}}\n"
+	  "-- Environment variable ${name} is set to $ENV{${name}}\n"
+	  "This suggests that the variable is referring to a existing installation of SWI-Prolog. "
 	  "This may conflict with building SWI-Prolog as this may cause the "
-	  "build process to use components from an older installed version "
+	  "build process to use components from such (older) installed version "
 	  "of SWI-Prolog.  Make sure to remove this environment from the "
 	  "environment before building the system.")
 endfunction()
 
 if(CMAKE_EXECUTABLE_FORMAT STREQUAL "ELF" AND
-   NOT x"$ENV{LD_PRELOAD}" STREQUAL x AND
-   "$ENV{LD_PRELOAD}" MATCHES swipl)
+   DEFINED ENV{LD_PRELOAD} AND
+   "$ENV{LD_PRELOAD}" MATCHES swipl|swi-prolog)
   conflicting_var(LD_PRELOAD)
 endif()
 
 if(UNIX)
   if(APPLE)
-    if(NOT x"$ENV{DYLD_LIBRARY_PATH}" STREQUAL x AND
-       "$ENV{DYLD_LIBRARY_PATH}" MATCHES swipl)
+    if(DEFINED ENV{DYLD_LIBRARY_PATH} AND
+       "$ENV{DYLD_LIBRARY_PATH}" MATCHES swipl|swi-prolog)
       conflicting_var(DYLD_LIBRARY_PATH)
     endif()
   else()
-    if(NOT x"$ENV{LD_LIBRARY_PATH}" STREQUAL x AND
-       "$ENV{LD_LIBRARY_PATH}" MATCHES swipl)
-      conflicting_var(DYLD_LIBRARY_PATH)
+    if(DEFINED ENV{LD_LIBRARY_PATH} AND
+       "$ENV{LD_LIBRARY_PATH}" MATCHES swipl|swi-prolog)
+      conflicting_var(LD_LIBRARY_PATH)
     endif()
   endif()
 elseif(WIN32)
-  if(NOT x"$ENV{PATH}" STREQUAL x AND
-     "$ENV{PATH}" MATCHES swipl)
+  if(DEFINED ENV{PATH} AND
+     "$ENV{PATH}" MATCHES swipl|swi-prolog)
     conflicting_var(PATH)
   endif()
 endif()
 
-if(NOT x"$ENV{SWI_HOME_DIR}" STREQUAL x AND
-   IS_DIRECTORY "$ENV{SWIPL}")
+if(DEFINED ENV{SWI_HOME_DIR} AND
+   IS_DIRECTORY "$ENV{SWI_HOME_DIR}")
   conflicting_var(SWI_HOME_DIR)
 endif()
-if(NOT "$ENV{SWIPL}" STREQUAL x AND
+if(DEFINED ENV{SWIPL} AND
    IS_DIRECTORY "$ENV{SWIPL}")
   conflicting_var(SWIPL)
 endif()
+
+
