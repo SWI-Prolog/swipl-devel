5b0429cf6dfdb615fdf5c21fc438ba03c6e8e8aa
erlanger <erlanger>
2019-03-11 15:12:50 -0400

    PORT: Android minizip USE_FILE32API

--- cmake/AndroidTargets.cmake
+++ cmake/AndroidTargets.cmake
@@ -1,6 +1,4 @@
 # Set target options for Android
 
-# Minizip needs USE_FILE32API set in 32-bit architectures
-if(CMAKE_SIZEOF_VOID_P EQUAL 4)
-  target_compile_definitions(libswipl PRIVATE USE_FILE32API=1)
-endif()
+# Minizip needs USE_FILE32API on Android
+target_compile_definitions(libswipl PRIVATE USE_FILE32API=1)
