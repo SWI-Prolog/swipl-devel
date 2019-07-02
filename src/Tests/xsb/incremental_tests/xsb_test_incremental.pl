c41968d433008cb30971450b6e7de4c64a2413c3
Jan Wielemaker <j.wielemaker@cwi.nl>
2019-07-02 13:06:49 +0200

    Added more tests and add all XSB tests as commented.

--- src/Tests/xsb/incremental_tests/xsb_test_incremental.pl
+++ src/Tests/xsb/incremental_tests/xsb_test_incremental.pl
@@ -61,7 +61,47 @@ swi_test(M:_P) :-
 
 :- begin_tests(xsb_test_incremental, [sto(rational_trees)]).
 
-incremental_test(incremental).
-incremental_test(incremental1).
-
+incremental_test(incremental).          % general incremental tests
+incremental_test(incremental1).         % same as above, but with changed decls.
+%incremental_test(incremental_trie).	% incremental tests with interned tries
+incremental_test(inc_trie_dyn).         % incremental tests with trie asserts
+%incremental_test(inc_trie_alt).        % inc with interned tries - for storage.P
+incremental_test(incremental_rule).
+/*
+incremental_test(incremental_rule_alt). % inc_rule with initial empty dyn pred
+incremental_test(test_incr_depends).    % inc trans depends (cyclic)
+incremental_test(test_incr_depends_2).  % inc trans depends (non cyclic)
+incremental_test(test_inc_switch).      % test incremental <-> opaque
+incremental_test(test_visitors).        % testing executable incremental decls.
+incremental_test(test_sound_updates).
+incremental_test(test_wfs_update).
+incremental_test(incr_scc).
+incremental_test(incr_test_romero).
+incremental_test(test_abolish_nonincremental).
+incremental_test(test_abolish_incremental_call_single).
+incremental_test(inc_abol).             % inc tests with abolish_all_tables
+incremental_test(inc_atc).              % inc tests with abolish_table_call
+incremental_test(inc_atc_gc).           % inc tests with abolish_table_call + gc
+incremental_test(inc_atc_gc_tricky).    % inc tests with abolish_table_call + gc
+incremental_test(test_directives).
+incremental_test(test_declarations).
+incremental_test(test_table_errors).    % testing executable incremental dirs.
+% Below are disabled in XSB suite
+incremental_test(test_set_pp).
+incremental_test(test_tc).
+incremental_test(test_iso_basic).
+incremental_test(test_iso_mult_visit).
+incremental_test(test_iso_hash).
+incremental_test(test_iso_undef).
+incremental_test(test_iso_attr).
+incremental_test(test_lazy).            % testing executable incremental decls.
+incremental_test(inc_atc_single_gc_deps). % yet more inc tests with abolish_table_call_single + gc
+incremental_test(test_introspection).
+incremental_test(test_invalidate).      % yet more tests of invalidation
+incremental_test(test_goal_interrupt).
+incremental_test(test_trip).
+incremental_test(test_trip_mem).
+incremental_test(test_trip_susp_reset).
+incremental_test(test_maxans).
+*/
 :- end_tests(xsb_test_incremental).
