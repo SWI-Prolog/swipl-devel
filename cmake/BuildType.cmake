2cf497a7ce5b1b879f58462170f50acfbf2bb69b
Jan Wielemaker <jan@swi-prolog.org>
2019-12-11 17:13:26 +0100

    PORT: Add -DO_DEBUG to Debug build type on MacOS

--- cmake/BuildType.cmake
+++ cmake/BuildType.cmake
@@ -44,4 +44,9 @@ if(CMAKE_COMPILER_IS_GNUCC)
   set(CMAKE_C_FLAGS_SANITIZE
       "-O0 -gdwarf-2 -g3 -fsanitize=address -fno-omit-frame-pointer"
       CACHE STRING "CFLAGS for a Sanitize build" FORCE)
+elseif(CMAKE_C_COMPILER_ID STREQUAL AppleClang)
+  set(CMAKE_C_FLAGS_DEBUG "-DO_DEBUG -gdwarf-2 -g3"
+      CACHE STRING "CFLAGS for a Debug build" FORCE)
+else()
+  message("Unkniwn C compiler.  ${CMAKE_C_COMPILER_ID}")
 endif()
