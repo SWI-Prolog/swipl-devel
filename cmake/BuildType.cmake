6635a3514b169f80aa09c3dc9a6338f403d6ccf9
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2020-02-14 15:13:46 +0100

    Fixed typo in message.

--- cmake/BuildType.cmake
+++ cmake/BuildType.cmake
@@ -48,5 +48,5 @@ elseif(CMAKE_C_COMPILER_ID STREQUAL AppleClang)
   set(CMAKE_C_FLAGS_DEBUG "-DO_DEBUG -gdwarf-2 -g3"
       CACHE STRING "CFLAGS for a Debug build" FORCE)
 else()
-  message("Unkniwn C compiler.  ${CMAKE_C_COMPILER_ID}")
+  message("Unknown C compiler.  ${CMAKE_C_COMPILER_ID}")
 endif()
