e513bd568241c54477a706c5c5fa2141774137b9
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2020-05-05 17:15:40 +0200

    Added index handling to cmake

---
+++ cmake/LibIndex.cmake
@@ -0,0 +1,23 @@
+function(library_index)
+
+foreach(dir ${ARGN})
+  string(REGEX REPLACE "/" "_" dirtarget ${dir})
+  set(target library_index_${dirtarget})
+
+  if(NOT TARGET ${target})
+    add_custom_command(
+	OUTPUT  home/${dir}/INDEX.pl
+	COMMAND ${PROG_SWIPL} -f none --no-packs --nopce -q
+		-g "\"make_library_index('${SWIPL_BUILD_HOME}/${dir}')\"" -t halt
+	DEPENDS ${SWIPL_BOOT_FILE} prolog_home
+    )
+
+    add_custom_target(${target} DEPENDS home/${dir}/INDEX.pl)
+
+    add_dependencies(library_index ${target})
+    install(FILES home/${dir}/INDEX.pl
+	    DESTINATION ${SWIPL_INSTALL_PREFIX}/${dir})
+  endif()
+endforeach()
+
+endfunction()
