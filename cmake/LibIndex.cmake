9043f083b2b15bf2571136d1cacdc65b37b30d9e
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2020-05-07 18:38:11 +0200

    Possibly working dependencies

--- cmake/LibIndex.cmake
+++ cmake/LibIndex.cmake
@@ -1,3 +1,5 @@
+include(QLF)
+
 function(library_index)
 
 foreach(dir ${ARGN})
@@ -5,15 +7,11 @@ foreach(dir ${ARGN})
   set(target library_index_${dirtarget})
 
   if(NOT TARGET ${target})
-    add_custom_command(
+    add_swipl_target(
+	${target}
 	OUTPUT  home/${dir}/INDEX.pl
-	COMMAND ${PROG_SWIPL} -f none --no-packs --nopce -q
-		-g "\"make_library_index('${SWIPL_BUILD_HOME}/${dir}')\"" -t halt
-	DEPENDS ${SWIPL_BOOT_FILE} prolog_home
-    )
-
-    add_custom_target(${target} DEPENDS home/${dir}/INDEX.pl)
-
+	COMMAND "make_library_index('${SWIPL_BUILD_HOME}/${dir}')"
+	COMMENT "Build home/${dir}/INDEX.pl")
     add_dependencies(library_index ${target})
     install(FILES ${CMAKE_BINARY_DIR}/home/${dir}/INDEX.pl
 	    DESTINATION ${SWIPL_INSTALL_PREFIX}/${dir})
