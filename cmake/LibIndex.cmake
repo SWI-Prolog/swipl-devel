24eddfad480f7f0441ea93fddc3caec9f12e37da
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2020-05-07 22:20:49 +0200

    Fixed installation of library index files.

--- cmake/LibIndex.cmake
+++ cmake/LibIndex.cmake
@@ -9,13 +9,11 @@ foreach(dir ${ARGN})
   if(NOT TARGET ${target})
     add_swipl_target(
 	${target}
-	OUTPUT  home/${dir}/INDEX.pl
+	OUTPUT  ${SWIPL_BUILD_HOME}/${dir}/INDEX.pl
 	QUIET
 	COMMAND "make_library_index('${SWIPL_BUILD_HOME}/${dir}')"
 	COMMENT "Build home/${dir}/INDEX.pl")
     add_dependencies(library_index ${target})
-    install(FILES ${CMAKE_BINARY_DIR}/home/${dir}/INDEX.pl
-	    DESTINATION ${SWIPL_INSTALL_PREFIX}/${dir})
   endif()
 endforeach()
 
