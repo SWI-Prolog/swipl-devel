077507c6db458e8840a5fcc92c5f93ac8ef3ac26
Jan Wielemaker <J.Wielemaker@vu.nl>
2020-07-18 17:00:40 +0200

    BUILD: Always the library index commands.  Prolog checks the dependencies anyway.

--- cmake/LibIndex.cmake
+++ cmake/LibIndex.cmake
@@ -7,9 +7,14 @@ foreach(dir ${ARGN})
   set(target library_index_${dirtarget})
 
   if(NOT TARGET ${target})
+    add_custom_target(
+	${target}_always ALL
+	DEPENDS ${SWIPL_BUILD_HOME}/${dir}/__INDEX.pl)
+
     add_swipl_target(
 	${target}
 	OUTPUT  ${SWIPL_BUILD_HOME}/${dir}/INDEX.pl
+		${SWIPL_BUILD_HOME}/${dir}/__INDEX.pl
 	QUIET
 	COMMAND "make_library_index('${SWIPL_BUILD_HOME}/${dir}')"
 	COMMENT "Build home/${dir}/INDEX.pl")
