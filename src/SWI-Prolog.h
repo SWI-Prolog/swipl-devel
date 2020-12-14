c86a7dc3f55078662436d5c6adc0bb88dcd52aed
Jan Wielemaker <J.Wielemaker@vu.nl>
2020-12-14 16:38:56 +0100

    Preparing version 8.3.15

--- src/SWI-Prolog.h
+++ src/SWI-Prolog.h
@@ -68,7 +68,7 @@ extern "C" {
 /* PLVERSION_TAG: a string, normally "", but for example "rc1" */
 
 #ifndef PLVERSION
-#define PLVERSION 80314
+#define PLVERSION 80315
 #endif
 #ifndef PLVERSION_TAG
 #define PLVERSION_TAG ""
