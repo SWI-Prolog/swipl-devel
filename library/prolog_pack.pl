1107a005de899d006cbe984e9ac150289309fd8c
Jan Wielemaker <J.Wielemaker@vu.nl>
2020-12-25 09:30:15 +0100

    ENHANCED: cannot_create_dir message if there are no candidates.

--- library/prolog_pack.pl
+++ library/prolog_pack.pl
@@ -2493,9 +2493,10 @@ message(server_reply(exception(E))) -->
     [ 'Server reported the following error:'-[], nl ],
     '$messages':translate_message(E).
 message(cannot_create_dir(Alias)) -->
-    { setof(PackDir,
-            absolute_file_name(Alias, PackDir, [solutions(all)]),
-            PackDirs)
+    { findall(PackDir,
+              absolute_file_name(Alias, PackDir, [solutions(all)]),
+              PackDirs0),
+      sort(PackDirs0, PackDirs)
     },
     [ 'Cannot find a place to create a package directory.'-[],
       'Considered:'-[]
