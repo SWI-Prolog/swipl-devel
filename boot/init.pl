5dca96d4f6a03e26d828e0e072fc6317ddd0ccad
Jan Wielemaker <J.Wielemaker@vu.nl>
2021-01-03 20:59:12 +0100

    MODIFIED: Run compilation of a file in _signal atomic_ mode to avoid partial loads of a source file.  Double Control-C allows (dangerous) abort of the load.

--- boot/init.pl
+++ boot/init.pl
@@ -2423,18 +2423,18 @@ load_files(Module:Files, Options) :-
 '$mt_load_file'(File, FullFile, Module, Options) :-
     current_prolog_flag(threads, true),
     !,
-    setup_call_cleanup(
-        with_mutex('$load_file',
-                   '$mt_start_load'(FullFile, Loading, Options)),
-        '$mt_do_load'(Loading, File, FullFile, Module, Options),
-        '$mt_end_load'(Loading)).
+    '$sig_atomic'(setup_call_cleanup(
+                      with_mutex('$load_file',
+                                 '$mt_start_load'(FullFile, Loading, Options)),
+                      '$mt_do_load'(Loading, File, FullFile, Module, Options),
+                      '$mt_end_load'(Loading))).
 '$mt_load_file'(File, FullFile, Module, Options) :-
     '$option'(if(If), Options, true),
     '$noload'(If, FullFile, Options),
     !,
     '$already_loaded'(File, FullFile, Module, Options).
 '$mt_load_file'(File, FullFile, Module, Options) :-
-    '$qdo_load_file'(File, FullFile, Module, Options).
+    '$sig_atomic'('$qdo_load_file'(File, FullFile, Module, Options)).
 
 '$mt_start_load'(FullFile, queue(Queue), _) :-
     '$loading_file'(FullFile, Queue, LoadThread),
