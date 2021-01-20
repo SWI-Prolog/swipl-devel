a7db5ecc3f199938f95b153240a34dffcabd3a15
Jan Wielemaker <J.Wielemaker@vu.nl>
2021-01-20 09:31:31 +0100

    FIXED: Various small issues that allow using []/N (notably []/3) as a predicate.  After report by Rick Workman.

--- src/pl-comp.c
+++ src/pl-comp.c
@@ -2618,7 +2618,7 @@ A non-void variable. Create a I_USERCALL0 instruction for it.
   { functor = functorTerm(*arg);
     fdef = valueFunctor(functor);
 
-    if ( !isTextAtom(fdef->name) )
+    if ( !isTextAtom(fdef->name) && fdef->name != ATOM_nil )
       return NOT_CALLABLE;
 
     if ( true(fdef, ARITH_F) && !ci->islocal )
