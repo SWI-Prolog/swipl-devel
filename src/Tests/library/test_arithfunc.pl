6a9d886ba58b241fa47057c8e5408733c3a74cef
Jan Wielemaker <J.Wielemaker@vu.nl>
2021-02-01 17:59:44 +0100

    TEST: Fixed user defined arithmetic tests.  Rick Workman.

--- src/Tests/library/test_arithfunc.pl
+++ src/Tests/library/test_arithfunc.pl
@@ -37,8 +37,8 @@
 	  [ test_arithfunc/0
 	  ]).
 :- use_module(library(plunit)).
-:- if(exists_source(library(arithfunc))).
-:- use_module(library(arithfunc)).
+:- if(exists_source(library(arithmetic))).
+:- use_module(library(arithmetic)).
 
 %%	test_arithfunc
 %
@@ -65,7 +65,7 @@ twice(X, R) :-
 mean(X1, X2, R) :-
 	R is (X1 + X2)/2.
 
-euler(2.71828).
+euler(2.718281828459045).
 
 fail(_) :- fail.
 
@@ -85,20 +85,18 @@ test(func, A == 10) :-
 	A is twice(5).
 test(func, A == 10) :-
 	A is mean(0, 20).
-test(euler, EE =:= 6*euler*7*1) :-
+test(euler, EE =:= 6*e*7*1) :-
         EE is 6*euler*7*1.
-test(fail, [blocked(partial_emulation)]) :-
-	catch(_ is fail, E, true),
-	compound(E), E = error(E2, _),
-	compound(E2), E2 = failure_error(_).
-test(except, throw(error(foobar))) :-
+test(fail, fail) :-
+	_A is fail.
+test(except, throws(error(foobar))) :-
 	_ is except.
-test(depth_limit, throw(depth_limit_exceeded)) :-
-	_A is fac(10).
-test(flag, NV == 50) :-
-	flag(f, Old, 100),
-	flag(f, V, mean(V, 0)),
-	flag(f, NV, Old).
+test(fac, A =:= 3628800) :-
+	A is fac(10).
+%test(flag, NV == 50) :-
+%	flag(f, Old, 100),
+%	flag(f, V, mean(V, 0)),
+%	flag(f, NV, Old).
 
 :- if(current_prolog_flag(bounded, false)). % GMP implies rational
 
