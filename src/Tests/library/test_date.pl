SWI-Prolog/swipl-devel

commit e68098ba2b38d7d9597def1b4fc0a8cef1ed43cb

FIXED: format_time/3 using date/9 term computed the weekday and year day of the UTC time rather than the local time.  Mike Elston.

src/os/pl-tai.c
@@ -410,12 +410,14 @@ cal_ftm(ftm *ftm, int required)
     ct.hour       = ftm->tm.tm_hour;
     ct.minute     = ftm->tm.tm_min;
     ct.second     = ftm->tm.tm_sec;
-    ct.offset     = -ftm->utcoff / 60;	/* TBD: make libtai speak seconds */
+    ct.offset     = 0;
 
     caltime_tai(&ct, &tai);
 
     if ( missing & HAS_WYDAY )
-    { caltime_utc(&ct, &tai, &ftm->tm.tm_wday, &ftm->tm.tm_yday);
+    { /* Gets weekday and yday at UTC, so we compensate afterwards! */
+      caltime_utc(&ct, &tai, &ftm->tm.tm_wday, &ftm->tm.tm_yday);
+      tai.x += ftm->utcoff;
       ftm->flags |= HAS_WYDAY;
     }
 
@@ -424,7 +426,6 @@ cal_ftm(ftm *ftm, int required)
     ftm->stamp -= (double)ct.second;
     ftm->stamp += ftm->sec;
     ftm->flags |= HAS_STAMP;
-
   }
 }
 

commit ee64e85fe2ba54d1dd64739bac8bf1ce290b0b40

FIXED: date_time_stamp/2 was broken in timezone handling after e68098ba2b38d7d9597def1b4fc0a8cef1ed43cb.

src/os/pl-tai.c
@@ -3,8 +3,9 @@
     Author:        Jan Wielemaker
     E-mail:        J.Wielemaker@vu.nl
     WWW:           http://www.swi-prolog.org
-    Copyright (c)  2011-2017, University of Amsterdam
+    Copyright (c)  2011-2021, University of Amsterdam
                               VU University Amsterdam
+			      SWI-Prolog Solutions b.v.
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
@@ -417,9 +418,10 @@ cal_ftm(ftm *ftm, int required)
     if ( missing & HAS_WYDAY )
     { /* Gets weekday and yday at UTC, so we compensate afterwards! */
       caltime_utc(&ct, &tai, &ftm->tm.tm_wday, &ftm->tm.tm_yday);
-      tai.x += ftm->utcoff;
       ftm->flags |= HAS_WYDAY;
     }
+
+    tai.x += ftm->utcoff;
 
     leapsecs_sub(&tai);
     ftm->stamp  = (double)((int64_t)tai.x - TAI_UTC_OFFSET);

src/Tests/library/test_date.pl
@@ -3,8 +3,9 @@
     Author:        Jan Wielemaker
     E-mail:        J.Wielemaker@vu.nl
     WWW:           www.swi-prolog.org
-    Copyright (c)  2006-2017, University of Amsterdam
+    Copyright (c)  2006-2021, University of Amsterdam
                               VU University Amsterdam
+			      SWI-Prolog Solutions b.v.
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
@@ -46,7 +47,8 @@
 test_date :-
 	non_unit_tests,
 	run_tests([ parse_time,
-		    format_time
+		    format_time,
+		    date_time_stamp
 		  ]).
 
 non_unit_tests :-
@@ -261,3 +263,10 @@ test(roundtrip_epoch, T =:= T2) :-
 	date_time_stamp(D, T2).
 
 :- end_tests(timestamp_roundtrip).
+
+:- begin_tests(date_time_stamp).
+
+test(tz, TS =:= -3600) :-
+	date_time_stamp(date(1970,1,1,0,0,0,-3600,-,-), TS).
+
+:- end_tests(date_time_stamp).