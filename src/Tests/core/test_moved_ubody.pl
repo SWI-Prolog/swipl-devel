SWI-Prolog/swipl-devel

commit fb96dc77bbd86df31ed7bd2282dd5b20f341f534

FIXED: clause/2 (decompiler for moved unification instructions against the head.

src/pl-comp.c
@@ -2560,7 +2560,9 @@ isvar:
 	{ Word p;
 
 	  if ( (p=argUnifiedTo(*arg)) )
+	  { set(ci->clause, CL_HEAD_TERMS);
 	    return compileArgument(p, where, ci);
+	  }
 	  Output_0(ci, H_VOID);
 	  return TRUE;
 	}
@@ -4901,6 +4903,7 @@ mark_bvar_access(DECL_LD Clause cl, decompileInfo *di)
 	index = 2;
         break;
       case B_VAR:
+      case H_VAR:
       case B_ARGVAR:
 	index = VARNUM(pc[1]);
         break;
@@ -5395,6 +5398,13 @@ decompile(Clause clause, term_t term, term_t bindings)
   } else
     di->variables = 0;
 
+  if ( true(clause, CL_HEAD_TERMS) )
+  { di->bvar_access = alloca(sizeof_bitvector(di->arity));
+    init_bitvector(di->bvar_access, di->arity);
+    if ( !mark_bvar_access(clause, di) )
+      return FALSE;
+  }
+
   if ( true(clause, UNIT_CLAUSE) )	/* fact */
   { if ( decompile_head(clause, term, di) )
     { if ( di->variables )
@@ -5415,13 +5425,6 @@ decompile(Clause clause, term_t term, term_t bindings)
     fail;
   } else
   { term_t a = PL_new_term_ref();
-
-    if ( true(clause, CL_HEAD_TERMS) )
-    { di->bvar_access = alloca(sizeof_bitvector(di->arity));
-      init_bitvector(di->bvar_access, di->arity);
-      if ( !mark_bvar_access(clause, di) )
-	return FALSE;
-    }
 
     TRY(PL_unify_functor(term, clause_functor(clause)));
     _PL_get_arg(1, term, a);

commit a9ead09d05fce1dffd125e1512e91cf96b091bd1

FIXED: clause/2 moved unification for `s(X) :- X = f(Y), p(Y).`  Bug introduced in fb96dc77bbd86df31ed7bd2282dd5b20f341f534.

src/pl-comp.c
@@ -2560,12 +2560,13 @@ isvar:
 	{ Word p;
 
 	  if ( (p=argUnifiedTo(*arg)) )
-	  { set(ci->clause, CL_HEAD_TERMS);
 	    return compileArgument(p, where, ci);
-	  }
 	  Output_0(ci, H_VOID);
 	  return TRUE;
 	}
+	if ( argUnifiedTo(*arg) )
+	  set(ci->clause, CL_HEAD_TERMS);
+
 	Output_0(ci, H_VAR);
       }
       Output_a(ci, VAROFFSET(index));

src/Tests/core/test_moved_ubody.pl
@@ -88,6 +88,10 @@ s6(X) :-
     X = f(_),
     q([X]).
 
+s7(X) :-
+    X = f(A),
+    q(A).
+
 nf_add_case(_Rel,_CLP,_A,_As,_Cs,_B,_Bs,_Ka,_Kb,_Pa).
 q(_).
 q(_,_,_,_).
@@ -127,6 +131,10 @@ test(decomp7, (Head :- Body) =@= (s6(A) :-
                                      A = f(_),
                                      q([A]))) :-
     Head = s6(_),
+    clause(Head, Body).
+test(decomp8, (Head :- Body) =@= (s7(f(A)) :-
+                                     q(A))) :-
+    Head = s7(_),
     clause(Head, Body).
 
 :- end_tests(moved_decompile).