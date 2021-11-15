c187b0c3af0d076502a0cc290830e59690d40870
Jan Wielemaker <J.Wielemaker@vu.nl>
2021-11-15 10:18:07 +0100

    TEST: Migrate dif/2 tests to plunit.

--- src/Tests/attvar/test_dif.pl
+++ src/Tests/attvar/test_dif.pl
@@ -3,8 +3,9 @@
     Author:        Jan Wielemaker
     E-mail:        J.Wielemaker@vu.nl
     WWW:           http://www.swi-prolog.org
-    Copyright (c)  2008-2015, University of Amsterdam
+    Copyright (c)  2008-2021, University of Amsterdam
                               VU University Amsterdam
+			      SWI-Prolog Solutions b.v.
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
@@ -36,48 +37,55 @@
 :- module(test_dif,
 	  [ test_dif/0
 	  ]).
+:- use_module(library(debug)).
+:- use_module(library(dif)).
+:- use_module(library(plunit)).
 
-:- '$clausable'(dif/1).
+test_dif :-
+	run_tests([ dif
+		  ]).
 
-dif(1) :-
+:- begin_tests(dif).
+
+test(1) :-
 	dif(1, A), \+ A = 1.
-dif(2) :-
+test(2) :-
 	dif(1, A), dif(2, A), \+ A = 1.
-dif(3) :-
+test(3) :-
 	dif(1, A), dif(2, A), \+ A = 2.
-dif(4) :-
+test(4) :-
 	dif(A, B), A = 1, \+ B = 1.
-dif(5) :-
+test(5, [sto(rational_trees)]) :-
 	A = a(A, 1),
 	B = a(B, X),
 	dif(A, B), \+ X = 1.
-dif(6) :-
+test(6) :-
 	dif(a(x(1,2), B), a(X, 1)),
 	X = a,
 	\+ attvar(B).
-dif(7) :-
+test(7) :-
 	dif(a(x(1,2), B), a(X, 1)),
 	X = x(1,2),
 	\+ B = 1.
-dif(8) :-
+test(8, [sto(rational_trees)]) :-
 	dif(a(x(1,2), B), a(X, 1)),
 	X = x(1,Y),
 	Y = 3,
 	\+ attvar(B).
-dif(9) :-
+test(9) :-
 	dif(X, Y), \+ X = Y.
-dif(10) :-
+test(10) :-
 	dif(f(X,_Z),f(a,b)),
 	dif(f(X,Y),f(b,b)),
 	X = a, Y = b.
-dif(11) :-
+test(11) :-
 	dif(A,B), memberchk(A, [B, C]),
 	A == C.
-dif(12) :-		% https://github.com/SWI-Prolog/issues/issues/15
+test(12) :-		% https://github.com/SWI-Prolog/issues/issues/15
 	dif(X-Y,1-2), X=Y, Y = 1.
-dif(13) :-		% https://github.com/SWI-Prolog/issues/issues/15
+test(13) :-		% https://github.com/SWI-Prolog/issues/issues/15
 	dif(X-Y,1-2), X=Y, Y = 2.
-dif(14) :-
+test(14) :-
 	P = t3(A,A),
 	A = t5(D,D),
 	D = t8(c,b),
@@ -89,16 +97,4 @@ dif(14) :-
 	G=t8(x,b),
 	assertion(term_attvars(P+Q, [])).
 
-:- dynamic
-	failed/1.
-
-test_dif :-
-	retractall(failed(_)),
-	forall(clause(dif(N), _, _),
-	       (   dif(N)
-	       ->  true
-	       ;   format('~NFailed: ~w~n', [dif(N)]),
-		   assert(failed(N))
-	       )),
-	\+ failed(_).
-
+:- end_tests(dif).
