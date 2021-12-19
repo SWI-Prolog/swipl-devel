be5f41116e0d9d8116574d7bd2b4416525062db2
Jan Wielemaker <jan@swi-prolog.org>
2021-12-19 11:15:27 +0100

    Added MacOS lipo script to create fat dependencies

---
+++ scripts/macos-import-arch.sh
@@ -0,0 +1,36 @@
+#!/bin/zsh
+
+from=$1
+
+usage()
+{
+cat <<_EOF_
+Usage: $0 dir
+_EOF_
+exit 1
+}
+
+if [ ! -d $from ]; then
+    usage
+fi
+
+files=( $(find . -type f \( -perm -0700 -o -name '*.dylib' -o -name '*.so' \) -a \! -name '*.pl' -a \! -name '*.la') )
+
+for f in $files; do
+    if file $f | grep Mach-O > /dev/null; then
+	if [ -f $f -a -f $from/$f ]; then
+	    echo -n "Joining $f and $from/$f ... "
+	    if lipo -create $f $from/$f -output $f.U; then
+		mv $f.U $f
+		echo ok
+	    else
+		echo "FAILED"
+	    fi
+	else
+	    echo "WARNING: Cannot find $from/$f"
+	fi
+    else
+	echo "WARNING: $f is not a native executable"
+    fi
+done
+
