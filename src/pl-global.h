2b79539dbb2f8148953cb603ce64e408c2861982
Danielle Church <dani.church@gmail.com>
2022-02-15 18:29:30 +0100

    FIXED: Preproc/build errors
    Adding a couple GCC builtins to the config tests, using them to force
    short-circuiting of _LD_WITH_FALLBACK even in low-optimization modes
    (e.g. Clang in DEBUG configuration).
    
    Also adding a few missing-but-used defines to config.h.cmake
    (HAVE___BUILTIN_EXPECT, HAVE___THREAD, HAVE_VOLATILE, and HAVE_INLINE)
    and fixing an unbalanced #ifdef pair that got added in f3b79d6.

--- src/pl-global.h
+++ src/pl-global.h
@@ -111,6 +111,7 @@ struct PL_global_data
 
   struct
   { sig_handler handlers[MAXSIGNAL];	/* How Prolog preceives signals */
+#ifdef HAVE_SIGNAL
     int		sig_alert;		/* our alert signal */
 #endif
   } signals;
@@ -457,7 +458,6 @@ struct PL_local_data
 #ifndef __unix__
     int		forced;			/* Forced signal */
 #endif
-#ifdef HAVE_SIGNAL
 #ifdef O_C_STACK_GUARDED
     jmp_buf	context;		/* Guarded for C-stack overflows */
     int		sig_critical;		/* context is valid */
@@ -814,12 +814,6 @@ GLOBAL PL_local_data_t  PL_local_data;
 #ifdef O_MULTIPLE_ENGINES
 GLOBAL PL_local_data_t *PL_current_engine_ptr;
 #endif
-/* See _LD_WITH_FALLBACK().  These should never be compiled,
-   but are on clang in debug mode
- */
-GLOBAL char *__PL_ld;
-GLOBAL struct {char *engine;} *PL__ctx;
-
 
 #define GD (&PL_global_data)
 #define CD (&PL_code_data)
