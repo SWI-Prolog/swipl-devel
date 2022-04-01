440df9e85e3806e709ed1ae3fd7d965f5c90d562
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2022-04-01 20:38:57 +0200

    PORT: Msys2/clang64

--- cmake/CStack.cmake
+++ cmake/CStack.cmake
@@ -1,6 +1,6 @@
 function(target_c_stack target limit)
 if(WIN32)
-  if(CMAKE_COMPILER_IS_GNUCC)
+  if(CMAKE_C_COMPILER_ID MATCHES "Clang|GNU")
     set_target_properties(${target} PROPERTIES
 			  LINK_FLAGS "-Wl,--stack,${limit}")
   else()
