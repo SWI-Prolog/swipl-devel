5874c29c7edcb9e2c25dfe3804cc87fa24375a7e
Jan Wielemaker <J.Wielemaker@vu.nl>
2022-04-21 18:07:24 +0200

    New style FOR_TABLE

--- src/pl-srcfile.c
+++ src/pl-srcfile.c
@@ -1421,15 +1421,14 @@ fix_module(Module m, m_reload *r)
 { GET_LD
 
   LOCKMODULE(m);
-  for_table(m->public, n, v,
-	    { if ( !r->public ||
-		   !lookupHTable(r->public, n) )
-	      { DEBUG(MSG_RECONSULT_MODULE,
-		      Sdprintf("Delete export %s\n",
-			       procedureName(v)));
-		deleteHTable(m->public, n);
-	      }
-	    });
+  FOR_TABLE(m->public, n, v)
+  { if ( !r->public ||
+	 !lookupHTable(r->public, n) )
+    { DEBUG(MSG_RECONSULT_MODULE,
+	    Sdprintf("Delete export %s\n", procedureName(v)));
+      deleteHTable(m->public, n);
+    }
+  };
   UNLOCKMODULE(m);
 }
 
