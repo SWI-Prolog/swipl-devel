c1ca50a94a6ea073c21e2d28c6bd5b749106566e
Jan Wielemaker <J.Wielemaker@vu.nl>
2022-08-03 16:12:54 +0200

    Allow demo server to run using Prolog.

---
+++ src/wasm/server.pl
@@ -0,0 +1,28 @@
+% Provide an HTTP server to make   the  various components available. To
+% use it, build the wasm version  in   e.g.,  `build.wasm` and from this
+% directory, run
+%
+%     swipl ../src/wasm/server.pl
+%
+:- use_module(library(http/http_server)).
+
+user:file_search_path(source, '../src/wasm').
+user:file_search_path(wasm,   'src').
+
+:- http_handler(/, http_reply_file(source('shell.html'), []), []).
+:- http_handler('/wasm/swipl-web.js',
+                http_reply_file(wasm('swipl-web.js'), []), []).
+:- http_handler('/wasm/swipl-web.data',
+                http_reply_file(wasm('swipl-web.data'), []), []).
+:- http_handler('/wasm/swipl-web.wasm',
+                http_reply_file(wasm('swipl-web.wasm'), []), []).
+
+:- initialization(server_loop, main).
+
+server :-
+    http_server([port(8080)]).
+
+server_loop :-
+    server,
+    thread_get_message(quit).
+
