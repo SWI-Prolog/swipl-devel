82c52b0e6a42f41b010526a9e8d39980ddd1a23b
Jan Wielemaker <J.Wielemaker@vu.nl>
2022-09-06 09:46:23 +0200

    PORT: Windows: install a copy of libstdc++*.dll while building and add this to the installer if INSTALL_TESTS is active.

--- cmake/port/Windows.cmake
+++ cmake/port/Windows.cmake
@@ -70,11 +70,18 @@ endfunction()
 
 message("-- Finding required external DLLs")
 find_windows_dlls(WIN32_DLLS ${WIN32_DLL_PATTERNS})
+find_windows_dlls(WIN32_CPP_DLLS "libstdc*.dll")
 
-foreach(dll ${WIN32_DLLS})
+foreach(dll ${WIN32_DLLS} ${WIN32_CPP_DLLS})
   file(COPY ${MINGW_ROOT}/bin/${dll}
        DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/src)
 endforeach()
+
+# Only install libstdc++.dll when installing the C++ tests.
+# It raises the installer size from 13 to 17 Mb.
+if(INSTALL_TESTS)
+  list(APPEND WIN32_DLLS ${WIN32_CPP_DLLS})
+endif()
 
 set(WIN32_DLLS ${WIN32_DLLS} CACHE INTERNAL "WIN32 DLLs to copy")
 endif(NOT DEFINED WIN32_DLLS)
