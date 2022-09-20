164b1f02e2d0d6f6935415e74e7edb4641c56f50
Jan Wielemaker <J.Wielemaker@vu.nl>
2022-09-20 15:43:51 +0200

    Simplify WASM server

--- src/wasm/server.pl
+++ src/wasm/server.pl
@@ -9,54 +9,38 @@
 :- use_module(library(main)).
 :- use_module(library(option)).
 
-user:file_search_path(source, '../src/wasm').
-user:file_search_path(wasm,   'src').
+user:file_search_path(web, '../src/wasm').
+user:file_search_path(web, 'src').
 user:file_search_path(scasp,  Dir) :-
     getenv('SCASP_HOME', Dir).
 
 :- http_handler('/', http_redirect(see_other, '/wasm/'), []).
-:- http_handler('/wasm/',
-                http_reply_file(source('index.html'), []), []).
 :- http_handler('/wasm/shell',
                 http_reply_file(
-                    source('shell.html'),
+                    web('shell.html'),
                     [ headers([ 'Cross-Origin-Opener-Policy'('same-origin'),
                                 'Cross-Origin-Embedder-Policy'('require-corp')
                               ])]),
                 []).
-:- http_handler('/wasm/swipl-web.js',
-                http_reply_file(wasm('swipl-web.js'), []), []).
-:- http_handler('/wasm/swipl-web.worker.js',
-                http_reply_file(wasm('swipl-web.worker.js'), []), []).
-:- http_handler('/wasm/swipl-web.data',
-                http_reply_file(wasm('swipl-web.data'), []), []).
-:- http_handler('/wasm/swipl-web.wasm',
-                http_reply_file(wasm('swipl-web.wasm'), []), []).
-
-% Test code
 :- http_handler('/wasm/test',
                 http_reply_file(
-                    source('test.html'),
+                    web('test.html'),
                     [ headers([ 'Cross-Origin-Opener-Policy'('same-origin'),
                                 'Cross-Origin-Embedder-Policy'('require-corp')
                               ])]),
                 []).
-:- http_handler('/wasm/test.pl',
-                http_reply_file(source('test.pl'), []), []).
-:- http_handler('/wasm/test.qlf',
-                http_reply_file(source('test.qlf'), []), []).
-
-
 :- http_handler('/wasm/cbg',
                 http_reply_file(
-                    source('cbg.html'),
+                    web('cbg.html'),
                     [ headers([ 'Cross-Origin-Opener-Policy'('same-origin'),
                                 'Cross-Origin-Embedder-Policy'('require-corp')
                               ])]),
                 []).
+:- http_handler('/wasm/',
+                http_reply_from_files(web(.), []), [prefix]).
 
 
-:- if(absolute_file_name(scasp(.), _, [file_type(directory)])).
+:- if(absolute_file_name(scasp(.), _, [file_type(directory), file_errors(fail)])).
 :- http_handler('/wasm/scasp/', http_reply_from_files(scasp(.), []), [prefix]).
 :- endif.
 
