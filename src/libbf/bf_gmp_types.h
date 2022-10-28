78ab35d199b9b0a78309f0813cfdb91947f2d018
Jan Wielemaker <J.Wielemaker@vu.nl>
2022-10-28 14:46:09 +0200

    Use Merseanne Twister for random_float/0 function

--- src/libbf/bf_gmp_types.h
+++ src/libbf/bf_gmp_types.h
@@ -2,6 +2,7 @@
 #define BF_GMP_TYPES_H_INCLUDED
 
 #include "libbf.h"
+#include "mersenne-twister.h"
 
 typedef bf_t MP_INT;
 typedef bf_t mpz_t[1];
@@ -9,6 +10,6 @@ typedef bf_t mpq_t[2];			/* Numerator/Denumerator */
 typedef bf_t mpf_t[1];
 typedef limb_t mp_limb_t;
 typedef limb_t mp_bitcnt_t;
-typedef int gmp_randstate_t;		/* dummy */
+typedef MTState gmp_randstate_t[1];
 
 #endif /*BF_GMP_TYPES_H_INCLUDED*/
