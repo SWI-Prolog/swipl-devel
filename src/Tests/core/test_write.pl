SWI-Prolog/swipl-devel

commit cbe691f8af61ccaf466147868a316b96c110fedb

FIXED: term_to_atom/2 must escape \ in quoted atoms.  Keri Harris.

src/pl-write.c
@@ -3,9 +3,10 @@
     Part of SWI-Prolog
 
     Author:        Jan Wielemaker
-    E-mail:        wielemak@science.uva.nl
+    E-mail:        J.Wielemaker@cs.vu.nl
     WWW:           http://www.swi-prolog.org
-    Copyright (C): 1985-2007, University of Amsterdam
+    Copyright (C): 1985-2010, University of Amsterdam
+			      Vu University Amsterdam
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
@@ -341,10 +342,11 @@ putQuoted(int c, int quote, int flags, IOSTREAM *stream)
 	fail;
     }
   } else
-  { if ( c == quote )
-    { TRY(Putc(c, stream) && Putc(c, stream));
-    } else
-    { return Putc(c, stream);
+  { if ( !Putc(c, stream) )
+      fail;
+    if ( c == quote || c == '\\' )	/* write '' or \\ */
+    { if ( !Putc(c, stream) )
+	fail;
     }
   }
 

commit f2fe9a6f32bd996f41745f299779935822344a97

MODIFIED: Writing ``\`` quoted without `character_escapes` should not double the ``\``. This bug was introduced with cbe691f8af61ccaf466147868a316b96c110fedb to "fix" ``term_to_atom('a\\b', A)`` to produce ``A = 'a\\b'`` (which is quoted again by the toplevel to produce ``A = '\'a\\\\b\''``.
The issue is that term_to_atom/2 using PL_write_term() with
``PL_WRT_QUOTED``, but __not__ ``PL_WRT_CHARESCAPES``. There is now a
new flag ``PL_WRT_NO_CHARESCAPES``. If either flag is given, we
explicitly enable or disable character escapes. If none is given, the
behaviour depends on the global (module `user`) value of the
`character_escapes` flag.

This implies that e.g. ``term_to_atom('a\nb', A)`` used to produce a
quoted atom with a plain newline, while in the new version the newline
is escaped as ``\n``.

Many foreign libraries using PL_write_term() or PL_get_chars() using
CVT_WRITE did not use character escapes while they do now. As long as
the output is read by Prolog the result is the same.

man/streams.doc
@@ -643,6 +643,15 @@ constants below.  These flags map to options for write_term/2.
 	\definition{\const{PL_WRT_NUMBERVARS}}
 	\definition{\const{PL_WRT_PORTRAY}}
 	\definition{\const{PL_WRT_CHARESCAPES}}
+        \nodescription
+	\definition{\const{PL_WRT_NO_CHARESCAPES}}
+The \const{PL_WRT_NO_CHARESCAPES} does not map to a write_term/2 option.
+If one of \const{PL_WRT_CHARESCAPES} or \const{PL_WRT_NO_CHARESCAPES} is
+specified, character escapes are (not) applied.  If neither is specified
+the default depends, like for write/1, on the
+\prologflag{character_escapes} flag on the module
+\const{user}.\footnote{Prior to version 9.1.6 the default (no flag) was
+to escape the quotes and the backslash (\chr{\}).}
 	\definition{\const{PL_WRT_BACKQUOTED_STRING}}
 	\definition{\const{PL_WRT_ATTVAR_IGNORE}}
 	\definition{\const{PL_WRT_ATTVAR_DOTS}}

src/SWI-Prolog.h
@@ -998,6 +998,7 @@ PL_EXPORT(IOSTREAM *)*_PL_streams(void);	/* base of streams */
 #define PL_WRT_CHARESCAPES_UNICODE 0x400000 /* Use \uXXXX escapes */
 #define PL_WRT_QUOTE_NON_ASCII	   0x800000 /* Quote atoms containing non-ascii */
 #define PL_WRT_PARTIAL		  0x1000000 /* Partial output */
+#define PL_WRT_NO_CHARESCAPES	  0x2000000 /* Do not Output ISO escapes */
 
 PL_EXPORT(int)	PL_write_term(IOSTREAM *s,
 			     term_t term,

src/pl-write.c
@@ -620,7 +620,7 @@ putQuoted(int c, int quote, int flags, IOSTREAM *stream)
   } else
   { if ( !Putc(c, stream) )
       fail;
-    if ( c == quote || c == '\\' )	/* write '' or \\ */
+    if ( c == quote )	/* write '' */
     { if ( !Putc(c, stream) )
 	fail;
     }
@@ -1753,7 +1753,7 @@ writeTerm2(term_t t, int prec, write_options *options, int flags)
 	    { switch(writeAtom(functor, options))
 	      { case FALSE:
 		  fail;
-	        case TRUE_WITH_SPACE:
+		case TRUE_WITH_SPACE:
 		  TRY(Putc(' ', out));
 	      }
 	    }
@@ -2154,6 +2154,11 @@ PL_write_term(IOSTREAM *s, term_t term, int precedence, int flags)
   options.out	    = s;
   options.module    = MODULE_user;
 
+  if ( !(flags & (PL_WRT_CHARESCAPES|PL_WRT_NO_CHARESCAPES) ) )
+  { if ( true(options.module, M_CHARESCAPE) )
+      options.flags |= PL_WRT_CHARESCAPES;
+  }
+
   if ( (s=PL_acquire_stream(s)) )
   { PutOpenToken(EOF, s);			/* reset this */
     rc = writeTopTerm(term, precedence, &options);
@@ -2460,4 +2465,3 @@ BeginPredDefs(write)
   PRED_DEF("$put_quoted", 4, put_quoted_codes, 0)
   PRED_DEF("write_length", 3, write_length, 0)
 EndPredDefs
-

src/Tests/core/test_text.pl
@@ -3,7 +3,7 @@
     Author:        Jan Wielemaker
     E-mail:        J.Wielemaker@vu.nl
     WWW:           http://www.swi-prolog.org
-    Copyright (c)  2007-2022, University of Amsterdam
+    Copyright (c)  2007-2023, University of Amsterdam
 			      SWI-Prolog Solutions b.v.
     All rights reserved.
 
@@ -119,6 +119,8 @@ test(write, A == 'foo(a)') :-
 	term_to_atom(foo(a), A).
 test(read, T == foo(a)) :-
 	term_to_atom(T, 'foo(a)').
+test(read, T == '\'a\\nb\\\\c\'') :-
+	term_to_atom('a\nb\\c', T).
 
 :- end_tests(term_to_atom).
 

src/Tests/core/test_write.pl
@@ -163,6 +163,18 @@ test(space, S == "= (is)") :-
 					partial(true)
 				      ])
 		       )).
+test(backslash, S == "'te\\\\st'") :-
+    with_output_to(string(S),
+		   write_term('te\\st',
+			      [ character_escapes(true),
+				quoted(true)
+			      ])).
+test(backslash, S == "'te\\st'") :-
+    with_output_to(string(S),
+		   write_term('te\\st',
+			      [ character_escapes(false),
+				quoted(true)
+			      ])).
 
 :- end_tests(write_quoted).
 