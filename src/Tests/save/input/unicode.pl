f6ef239082cecc0aff637af7dc9bf717680cfc0f
Jan Wielemaker <J.Wielemaker@vu.nl>
2023-03-09 21:17:00 +0100

    TEST: Test load/save .qlf files.   Current test deals with Unicode.

---
+++ src/Tests/save/input/unicode.pl
@@ -0,0 +1,22 @@
+:- encoding(utf8).
+
+%! data(?Data)
+%
+%  Provide data for QLF round trip.
+
+data(1).
+data('Hallo').
+data('ታዲያስ').
+data('مرحبا').
+data('Kaixo').
+data('নমস্কার').
+data('Olá').
+data('سلام').
+data('მიესალმები').
+data('नमस्ते').
+data('こんにちは').
+
+data('😁').
+data('🤣').
+
+data('with\u0000null').
