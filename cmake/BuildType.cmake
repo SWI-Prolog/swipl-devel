08404bf0b52743306980a383c1c23197c416d01f
Peter Ludemann <peter.ludemann@gmail.com>
2023-03-26 19:52:24 -0700

    Add C++ flags for debug, sanitize

--- cmake/BuildType.cmake
+++ cmake/BuildType.cmake
@@ -46,6 +46,8 @@ if(CMAKE_COMPILER_IS_GNUCC)
   set(CMAKE_C_FLAGS_SANITIZE
       "-O0 -gdwarf-2 -g3 -fsanitize=address -fno-omit-frame-pointer"
       CACHE STRING "CFLAGS for a Sanitize build" FORCE)
+  set(CMAKE_CXX_FLAGS_DEBUG "-DO_DEBUG -O0 -gdwarf-2 -g3"
+      CACHE STRING "CFLAGS for a Debug build" FORCE)
   set(CMAKE_CXX_FLAGS_SANITIZE
       "-O0 -gdwarf-2 -g3 -fsanitize=address -fno-omit-frame-pointer"
       CACHE STRING "CFLAGS for a Sanitize build" FORCE)
@@ -55,11 +57,15 @@ elseif(CMAKE_C_COMPILER_ID STREQUAL Clang)
   set(CMAKE_C_FLAGS_SANITIZE
       "-gdwarf-2 -g3 -fsanitize=address -O1 -fno-omit-frame-pointer"
       CACHE STRING "CFLAGS for a Sanitize build" FORCE)
+  set(CMAKE_CXX_FLAGS_DEBUG "-DO_DEBUG -gdwarf-2 -g3"
+      CACHE STRING "CFLAGS for a Debug build" FORCE)
   set(CMAKE_CXX_FLAGS_SANITIZE
       "-gdwarf-2 -g3 -fsanitize=address -O1 -fno-omit-frame-pointer"
       CACHE STRING "CFLAGS for a Sanitize build" FORCE)
 elseif(CMAKE_C_COMPILER_ID STREQUAL AppleClang)
   set(CMAKE_C_FLAGS_DEBUG "-DO_DEBUG -gdwarf-2 -g3"
+      CACHE STRING "CFLAGS for a Debug build" FORCE)
+  set(CMAKE_CXX_FLAGS_DEBUG "-DO_DEBUG -gdwarf-2 -g3"
       CACHE STRING "CFLAGS for a Debug build" FORCE)
 elseif(EMSCRIPTEN)
 else()
