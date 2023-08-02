75e15e67d3e0f366e914ab8794789f11dfa1d57e
xpxaxsxi <pasikuuskasi@windowslive.com>
2023-08-02 21:09:11 +0300

    for windows, open_url cut

--- library/www_browser.pl
+++ library/www_browser.pl
@@ -94,7 +94,8 @@ open_url(URL) :-
     run_command(Command, [URL], Mode).
 :- if(current_predicate(win_shell/2)).
 open_url(URL) :-                        % Windows shell
-    win_shell(open, URL).
+    win_shell(open, URL),
+    !.
 :- endif.
 open_url(URL) :-                        % Unix `open document'
     open_command(Open),
