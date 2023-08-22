0ec564164a6af1d045f30a46d6931eebcf523015
Jan Wielemaker <jan@swi-prolog.org>
2023-08-22 11:59:12 +0200

    BUILD: Work around problem generating Unix Makefiles

--- cmake/LibIndex.cmake
+++ cmake/LibIndex.cmake
@@ -11,10 +11,18 @@ foreach(dir ${ARGN})
 	${target}_always ALL
 	DEPENDS ${SWIPL_BUILD_HOME}/${dir}/__INDEX.pl)
 
+    if(CMAKE_GENERATOR STREQUAL "Unix Makefiles")
+      set(out ${SWIPL_BUILD_HOME}/${dir}/__INDEX.pl)
+      set(by)
+    else()
+      set(by ${SWIPL_BUILD_HOME}/${dir}/__INDEX.pl)
+      set(out)
+    endif()
+
     add_swipl_target(
 	${target}
-	OUTPUT  ${SWIPL_BUILD_HOME}/${dir}/INDEX.pl
-	BYPRODUCTS ${SWIPL_BUILD_HOME}/${dir}/__INDEX.pl
+	OUTPUT  ${SWIPL_BUILD_HOME}/${dir}/INDEX.pl ${out}
+	BYPRODUCTS ${by}
 	QUIET
 	COMMAND "make_library_index('${SWIPL_BUILD_HOME}/${dir}')"
 	COMMENT "Build home/${dir}/INDEX.pl")
