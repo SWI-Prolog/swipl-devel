SWI-Prolog/swipl-devel

commit 2ac1553770ac1a9d6753b8b6e72ac2028815a028

ADDED: PL_get_nchars(): CVT_XINTEGER
Allow converting numbers to hexadecimal strings.

src/SWI-Prolog.h
@@ -919,6 +919,7 @@ PL_EXPORT(int)		_PL_get_arg(int index, term_t t, term_t a);
 #define CVT_WRITE_CANONICAL 0x00000100
 #define CVT_WRITEQ	    0x00000200
 #define CVT_ALL		    (CVT_ATOMIC|CVT_LIST)
+#define CVT_XINTEGER	    (0x00000400|CVT_INTEGER)
 #define CVT_MASK	    0x00000fff
 
 #define CVT_EXCEPTION	    0x00001000	/* throw exception on error */

src/os/pl-text.c
@@ -178,17 +178,18 @@ PL_text_length(const PL_chars_t *text)
 #define INT64_DIGITS 20
 
 static char *
-ui64toa(uint64_t val, char *out)
+ui64toa(uint64_t val, char *out, int base)
 { char tmpBuf[INT64_DIGITS + 1];
   char *ptrOrg = tmpBuf + INT64_DIGITS;
   char *ptr = ptrOrg;
   size_t nbDigs;
+  static const char digits[] = "0123456789abcdef";
 
   do
-  { int rem = val % 10;
+  { int rem = val % base;
 
-    *--ptr = rem + '0';
-    val /= 10;
+    *--ptr = digits[rem];
+    val /= base;
   } while ( val );
 
   nbDigs = ptrOrg - ptr;
@@ -201,13 +202,13 @@ ui64toa(uint64_t val, char *out)
 
 
 static char *
-i64toa(int64_t val, char *out)
+i64toa(int64_t val, char *out, int base)
 { if ( val < 0 )
   { *out++ = '-';
     val = -(uint64_t)val;
   }
 
-  return ui64toa((uint64_t)val, out);
+  return ui64toa((uint64_t)val, out, base);
 }
 
 
@@ -228,11 +229,12 @@ PL_get_text(DECL_LD term_t l, PL_chars_t *text, int flags)
   } else if ( ((flags&CVT_RATIONAL) && isRational(w)) ||
 	      ((flags&CVT_INTEGER)  && isInteger(w)) )
   { number n;
+    int base = (flags&CVT_XINTEGER) ? 16 : 10;
 
     PL_get_number(l, &n);
     switch(n.type)
     { case V_INTEGER:
-      { char *ep = i64toa(n.value.i, text->buf);
+      { char *ep = i64toa(n.value.i, text->buf, base);
 
 	text->text.t    = text->buf;
 	text->length    = ep-text->text.t;
@@ -246,7 +248,7 @@ PL_get_text(DECL_LD term_t l, PL_chars_t *text, int flags)
 
 	if ( !growBuffer(b, sz) )
 	  outOfCore();
-	mpz_get_str(b->base, 10, n.value.mpz);
+	mpz_get_str(b->base, base, n.value.mpz);
 	b->top = b->base + strlen(b->base);
 	text->text.t  = baseBuffer(b, char);
 	text->length  = entriesBuffer(b, char);
@@ -261,10 +263,10 @@ PL_get_text(DECL_LD term_t l, PL_chars_t *text, int flags)
 
 	if ( !growBuffer(b, sz) )
 	  outOfCore();
-	mpz_get_str(b->base, 10, mpq_numref(n.value.mpq));
+	mpz_get_str(b->base, base, mpq_numref(n.value.mpq));
 	b->top = b->base + strlen(b->base);
 	*b->top++ = 'r';			/* '/' under some condition? */
-	mpz_get_str(b->top, 10, mpq_denref(n.value.mpq));
+	mpz_get_str(b->top, base, mpq_denref(n.value.mpq));
 	b->top += strlen(b->top);
 	text->text.t  = baseBuffer(b, char);
 	text->length  = entriesBuffer(b, char);

man/foreign.doc
@@ -1153,12 +1153,25 @@ Convert if term is a list of of character codes.
     \termitem{CVT_INTEGER}{}
 Convert if term is an integer.
 
+    \termitem{CVT_RATIONAL}{}
+Convert if term is an rational number.  The number is written as
+<num>r<den>.
+
+    \termitem{CVT_XINTEGER}{}
+Convert if term is an integer to hexadecimal notation. May be combined
+with \const{CVT_RATIONAL} to represent rational numbers using hexadecimal
+notation.  Hexadecimal notation is notably useful for transferring big
+integers to other programming environments if the target system can read
+hexadecimal notation because the result is both more compact and faster
+to write and read.
+
     \termitem{CVT_FLOAT}{}
 Convert if term is a float.  The characters returned are the same as
 write/1 would write for the floating point number.
 
     \termitem{CVT_NUMBER}{}
-Convert if term is an integer or float.
+Convert if term is an integer, rational number or or float.  Equivalent
+to \mbox{\const{CVT_INTEGER}{|}\const{CVT_RATIONAL}{|}\const{CVT_FLOAT}}
 
     \termitem{CVT_ATOMIC}{}
 Convert if term is atomic.

commit c1850ecf59c008614d7d1f7fd335ee98c25e3d14

FIXED: PL_get_nchars() and friends ``CVT_INTEGER`` handling
Broken in 2ac1553770ac1a9d6753b8b6e72ac2028815a028

src/SWI-Prolog.h
@@ -919,7 +919,7 @@ PL_EXPORT(int)		_PL_get_arg(int index, term_t t, term_t a);
 #define CVT_WRITE_CANONICAL 0x00000100
 #define CVT_WRITEQ	    0x00000200
 #define CVT_ALL		    (CVT_ATOMIC|CVT_LIST)
-#define CVT_XINTEGER	    (0x00000400|CVT_INTEGER)
+#define CVT_XINTEGER	   (0x00000400|CVT_INTEGER)
 #define CVT_MASK	    0x00000fff
 
 #define CVT_EXCEPTION	    0x00001000	/* throw exception on error */

src/os/pl-text.c
@@ -229,7 +229,7 @@ PL_get_text(DECL_LD term_t l, PL_chars_t *text, int flags)
   } else if ( ((flags&CVT_RATIONAL) && isRational(w)) ||
 	      ((flags&CVT_INTEGER)  && isInteger(w)) )
   { number n;
-    int base = (flags&CVT_XINTEGER) ? 16 : 10;
+    int base = (flags&CVT_XINTEGER)==CVT_XINTEGER ? 16 : 10;
 
     PL_get_number(l, &n);
     switch(n.type)