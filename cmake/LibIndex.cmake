SWI-Prolog/swipl-devel

commit 71caaeb72188627f0d717d251cab14d3e557a241

BUILD: Simplify forced building of the library indices.

cmake/LibIndex.cmake
@@ -1,5 +1,10 @@
 include(QLF)
 
+# Rebuild all library indexes. First build is   always  ok, but as we do
+# not know all dependencies for each library index and these may change,
+# we wish to re-run these on every build  run. We do this by setting the
+# OUTPUT to a dummy file that is not really created.
+
 function(library_index)
 
 foreach(dir ${ARGN})
@@ -7,23 +12,9 @@ foreach(dir ${ARGN})
   set(target library_index_${dirtarget})
 
   if(NOT TARGET ${target})
-    add_custom_target(
-	${target}_always ALL
-	DEPENDS ${SWIPL_BUILD_HOME}/${dir}/__INDEX.pl)
-
-    if(CMAKE_GENERATOR STREQUAL "Unix Makefiles")
-      set(out ${SWIPL_BUILD_HOME}/${dir}/__INDEX.pl)
-      set(by)
-    else()
-      set(by ${SWIPL_BUILD_HOME}/${dir}/__INDEX.pl)
-      set(out)
-    endif()
-
     add_swipl_target(
 	${target}
-	OUTPUT  ${SWIPL_BUILD_HOME}/${dir}/INDEX.pl ${out}
-	BYPRODUCTS ${by}
-	QUIET
+	OUTPUT ${SWIPL_BUILD_HOME}/${dir}/__INDEX.pl
 	COMMAND "make_library_index('${SWIPL_BUILD_HOME}/${dir}')"
 	COMMENT "Build home/${dir}/INDEX.pl")
     add_dependencies(library_index ${target})

commit e7bb59d037814949f25957665b57e9be9758b324

BUILD: Make installation work again
Broken with 71caaeb72188627f0d717d251cab14d3e557a241

cmake/LibIndex.cmake
@@ -14,9 +14,12 @@ foreach(dir ${ARGN})
   if(NOT TARGET ${target})
     add_swipl_target(
 	${target}
+	NOINSTALL
 	OUTPUT ${SWIPL_BUILD_HOME}/${dir}/__INDEX.pl
 	COMMAND "make_library_index('${SWIPL_BUILD_HOME}/${dir}')"
 	COMMENT "Build home/${dir}/INDEX.pl")
+    install(FILES ${SWIPL_BUILD_HOME}/${dir}/INDEX.pl
+	    DESTINATION ${SWIPL_INSTALL_PREFIX}/${dir})
     add_dependencies(library_index ${target})
   endif()
 endforeach()

cmake/QLF.cmake
@@ -19,7 +19,7 @@
 function(add_swipl_target name)
   set(options -f none --no-packs -t halt "--home=${SWIPL_BUILD_HOME}")
   cmake_parse_arguments(
-      my "QUIET;QLF" "COMMENT;COMMAND" "OUTPUT;BYPRODUCTS;SCRIPT;DEPENDS;OPTIONS;LIBS" ${ARGN})
+      my "QUIET;QLF;NOINSTALL" "COMMENT;COMMAND" "OUTPUT;BYPRODUCTS;SCRIPT;DEPENDS;OPTIONS;LIBS" ${ARGN})
 
   if(my_QUIET)
     set(options ${options} -q)
@@ -53,11 +53,13 @@ function(add_swipl_target name)
       ${name} ALL
       DEPENDS ${my_OUTPUT})
 
-  list(GET my_OUTPUT 0 primary)
-  string(REPLACE "${SWIPL_BUILD_HOME}" "" rel "${primary}")
-  get_filename_component(rel ${rel} DIRECTORY)
-  install(FILES ${primary}
-	  DESTINATION ${SWIPL_INSTALL_PREFIX}/${rel})
+  if(NOT my_NOINSTALL)
+    list(GET my_OUTPUT 0 primary)
+    string(REPLACE "${SWIPL_BUILD_HOME}" "" rel "${primary}")
+    get_filename_component(rel ${rel} DIRECTORY)
+    install(FILES ${primary}
+	    DESTINATION ${SWIPL_INSTALL_PREFIX}/${rel})
+  endif()
 endfunction()
 
 # add_qcompile_target(