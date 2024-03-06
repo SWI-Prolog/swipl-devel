0bd41c30de947d549ff2933a6770b197330884af
Jan Wielemaker <jan@swi-prolog.org>
2024-03-06 09:55:43 +0100

    CLARIFIED: PL_free_blob() can be called multiple times.
    This patch changes subsequent calls to a no-op that returns ``FALSE`` and
    document this is valid.   After comment by Peter Ludemann.

--- src/pl-fli.c
+++ src/pl-fli.c
@@ -3957,7 +3957,7 @@ PL_free_blob(atom_t a)
 { Atom x = atomValue(a);
   const PL_blob_t *type = x->type;
 
-  if ( true(type, PL_BLOB_NOCOPY) && type->release )
+  if ( true(type, PL_BLOB_NOCOPY) && type->release && x->name )
   { if ( (*type->release)(a) )
     { x->length = 0;
       x->name = NULL;
