3347d6e219b125909186fc602327ad1d36e87c4f
Jan Wielemaker <jan@swi-prolog.org>
2024-03-13 17:13:19 +0100

    ENHANCED: help/1: filter user predicates on the module class.
    This use to filter on whether they are documented in the manual, making
    it impossible to show documentation for loaded predicates that conflict
    with something defined in the manual.

--- library/help.pl
+++ library/help.pl
@@ -297,7 +297,7 @@ current_predicate_help(M:Name/Arity) :-
     current_predicate(M:Name/Arity),
     pi_head(Name/Arity,Head),
     \+ predicate_property(M:Head, imported_from(_)),
-    \+ man_object_property(Name/Arity, _), % must not be indexed already
+    module_property(M, class(user)),
     (   mode(M:_, _)             % Some predicates are documented
     ->  true
     ;   \+ module_property(M, class(system)),
