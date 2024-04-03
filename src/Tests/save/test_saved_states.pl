ed0871ef4d80a08552b08739ba0f5ca5949b6db0
Jan Wielemaker <jan@swi-prolog.org>
2024-04-03 15:33:51 +0200

    Layout for test file

--- src/Tests/save/test_saved_states.pl
+++ src/Tests/save/test_saved_states.pl
@@ -33,15 +33,15 @@
 */
 
 :- module(test_saved_states,
-	  [ test_saved_states/0
-	  ]).
+          [ test_saved_states/0
+          ]).
 
 has_foreign_lib(Lib) :-
     absolute_file_name(foreign(Lib), _,
-		       [ file_type(executable),
-			 file_errors(fail),
-			 access(read)
-		       ]).
+                       [ file_type(executable),
+                         file_errors(fail),
+                         access(read)
+                       ]).
 
 :- if(has_foreign_lib(process)).
 
@@ -66,150 +66,156 @@ This module tests the saved state generation capabilities.
 */
 
 test_saved_states :-
-	\+ enough_files, !,
-	format(user_error,
-	       'Skipped saved state files because the system does\n\c
-	not offer us enough open files~n', []).
+    \+ enough_files,
+    !,
+    format(user_error,
+           'Skipped saved state files because the system does\n\c
+        not offer us enough open files~n', []).
 test_saved_states :-
-	run_tests([ saved_state
-		  ]).
+    run_tests([ saved_state
+              ]).
 
 :- dynamic
-	test_dir/1.
+    test_dir/1.
 
 :- prolog_load_context(directory, Dir),
    retractall(test_dir(_)),
    asserta(test_dir(Dir)).
 
-%%	enough_files
+%!  enough_files
 %
-%	This test uses quite a  few   file  descriptors, apparently more
-%	than some confined build and test  environments offer. Hence, we
-%	test for this and try to enlarge the number.
+%   This test uses quite a  few   file  descriptors, apparently more
+%   than some confined build and test  environments offer. Hence, we
+%   test for this and try to enlarge the number.
 
 :- if(current_predicate(rlimit/3)).
 enough_files :-
-	catch(rlimit(nofile, Limit, Limit), E,
-	      print_message(warning, E)),
-	(   subsumes_term(error(domain_error(resource, nofile), _), E)
-	->  debug(save(max_files), 'Unknown max open files', [])
-	;   Limit > 16,
-	    debug(save(max_files), 'Reported ~D max open files', [Limit])
-	), !.
+    catch(rlimit(nofile, Limit, Limit), E,
+          print_message(warning, E)),
+    (   subsumes_term(error(domain_error(resource, nofile), _), E)
+    ->  debug(save(max_files), 'Unknown max open files', [])
+    ;   Limit > 16,
+        debug(save(max_files), 'Reported ~D max open files', [Limit])
+    ),
+    !.
 enough_files :-
-	MaxOpen = 16,
-	catch(rlimit(nofile, _, MaxOpen), E,
-	      ( print_message(warning, E),
-		fail
-	      )),
-	debug(save(max_files), 'Raised max open files to ~d', [MaxOpen]).
+    MaxOpen = 16,
+    catch(rlimit(nofile, _, MaxOpen), E,
+          ( print_message(warning, E),
+            fail
+          )),
+    debug(save(max_files), 'Raised max open files to ~d', [MaxOpen]).
 :- else.
 enough_files :-
-	debug(save(max_files), 'No info on max open files; assuming ok', []).
+    debug(save(max_files), 'No info on max open files; assuming ok', []).
 :- endif.
 
-%%	state_output(+Id, -FileName)
+%!  state_output(+Id, -FileName)
 %
-%	Name of the file we use for temporary output of the state.
+%   Name of the file we use for temporary output of the state.
 
 state_output(Id, State) :-
-	working_directory(Dir, Dir),
-	current_prolog_flag(pid, Pid),
-	format(atom(File), 'test_state_~w_~w.exe', [Id, Pid]),
-	directory_file_path(Dir, File, State).
+    working_directory(Dir, Dir),
+    current_prolog_flag(pid, Pid),
+    format(atom(File), 'test_state_~w_~w.exe', [Id, Pid]),
+    directory_file_path(Dir, File, State).
 
 me(Exe) :-
-	current_prolog_flag(executable, WinExeOS),
-	prolog_to_os_filename(WinExe, WinExeOS),
-	file_base_name(WinExe, WinFile),
-	downcase_atom(WinFile, 'swipl-win.exe'), !,
-	file_directory_name(WinExe, WinDir),
-	atomic_list_concat([WinDir, 'swipl.exe'], /, PlExe),
-	prolog_to_os_filename(PlExe, Exe).
+    current_prolog_flag(executable, WinExeOS),
+    prolog_to_os_filename(WinExe, WinExeOS),
+    file_base_name(WinExe, WinFile),
+    downcase_atom(WinFile, 'swipl-win.exe'),
+    !,
+    file_directory_name(WinExe, WinDir),
+    atomic_list_concat([WinDir, 'swipl.exe'], /, PlExe),
+    prolog_to_os_filename(PlExe, Exe).
 me(MeSH) :-
-	absolute_file_name('swipl.sh', MeSH,
-			   [ access(execute),
-			     file_errors(fail)
-			   ]), !.
+    absolute_file_name('swipl.sh', MeSH,
+                       [ access(execute),
+                         file_errors(fail)
+                       ]),
+    !.
 me(Exe) :-
-	current_prolog_flag(executable, Exe).
+    current_prolog_flag(executable, Exe).
 
 :- dynamic
-	win_path_set/0.
+    win_path_set/0.
 
 set_windows_path :-
-	\+ win_path_set,
-	current_prolog_flag(windows, true), !,
-	current_prolog_flag(executable, WinExeOS),
-	prolog_to_os_filename(WinExe, WinExeOS),
-	file_directory_name(WinExe, PlWinDir),
-	prolog_to_os_filename(PlWinDir, WinDir),
-	getenv('PATH', Path0),
-	atomic_list_concat([WinDir, Path0], ';', Path),
-	setenv('PATH', Path),
-	asserta(win_path_set).
+    \+ win_path_set,
+    current_prolog_flag(windows, true),
+    !,
+    current_prolog_flag(executable, WinExeOS),
+    prolog_to_os_filename(WinExe, WinExeOS),
+    file_directory_name(WinExe, PlWinDir),
+    prolog_to_os_filename(PlWinDir, WinDir),
+    getenv('PATH', Path0),
+    atomic_list_concat([WinDir, Path0], ';', Path),
+    setenv('PATH', Path),
+    asserta(win_path_set).
 set_windows_path.
 
 create_state(File, Output, Args) :-
-	me(Me),
-	append(Args, ['-o', Output, '-c', File, '-f', none], AllArgs),
-	test_dir(TestDir),
-	debug(save, 'Creating state in ~q using ~q ~q', [TestDir, Me, AllArgs]),
-	process_create(Me, AllArgs,
-		       [ cwd(TestDir),
-			 stderr(pipe(Err))
-		       ]),
-	read_stream_to_codes(Err, ErrOutput),
-	close(Err),
-	debug(save, 'Saved state', []),
-	assertion(no_error(ErrOutput)).
+    me(Me),
+    append(Args, ['-o', Output, '-c', File, '-f', none], AllArgs),
+    test_dir(TestDir),
+    debug(save, 'Creating state in ~q using ~q ~q', [TestDir, Me, AllArgs]),
+    process_create(Me, AllArgs,
+                   [ cwd(TestDir),
+                     stderr(pipe(Err))
+                   ]),
+    read_stream_to_codes(Err, ErrOutput),
+    close(Err),
+    debug(save, 'Saved state', []),
+    assertion(no_error(ErrOutput)).
 
 run_state(Exe, Args, Result) :-
-	debug(save, 'Running state ~q ~q', [Exe, Args]),
-	set_windows_path,
-	process_create(Exe, Args,
-		       [ stdout(pipe(Out)),
-			 stderr(pipe(Err))
-		       ]),
-	call_cleanup(
-	    ( read_terms(Out, Result),
-	      read_stream_to_codes(Err, ErrOutput)
-	    ),
-	    ( close(Out),
-	      close(Err)
-	    )),
-	assertion(no_error(ErrOutput)).
+    debug(save, 'Running state ~q ~q', [Exe, Args]),
+    set_windows_path,
+    process_create(Exe, Args,
+                   [ stdout(pipe(Out)),
+                     stderr(pipe(Err))
+                   ]),
+    call_cleanup(
+        ( read_terms(Out, Result),
+          read_stream_to_codes(Err, ErrOutput)
+        ),
+        ( close(Out),
+          close(Err)
+        )),
+    assertion(no_error(ErrOutput)).
 
 remove_state(_State) :-
-	debugging(save(keep)), !.
+    debugging(save(keep)),
+    !.
 remove_state(State) :-
-	catch(delete_file(State), _, true).
+    catch(delete_file(State), _, true).
 
-%%	read_terms(+In:stream, -Data:list)
+%!  read_terms(+In:stream, -Data:list)
 %
-%	True when Data are the Prolog terms on In.
+%   True when Data are the Prolog terms on In.
 
 read_terms(In, List) :-
-	read_term(In, T0, []),
-	read_terms(T0, In, List).
+    read_term(In, T0, []),
+    read_terms(T0, In, List).
 
 read_terms(end_of_file, _, []) :- !.
 read_terms(H, In, [H|T]) :-
-	read_term(In, H2, []),
-	read_terms(H2, In, T).
+    read_term(In, H2, []),
+    read_terms(H2, In, T).
 
 
 no_error(Codes) :-
-	\+ phrase((..., "ERROR:"), Codes),
-	\+ phrase((..., "error:"), Codes),
-	\+ phrase((..., "Warning:"), Codes).
+    \+ phrase((..., "ERROR:"), Codes),
+    \+ phrase((..., "error:"), Codes),
+    \+ phrase((..., "Warning:"), Codes).
 
 ... --> [] | [_], ... .
 
 
-		 /*******************************
-		 *	       TESTS		*
-		 *******************************/
+                 /*******************************
+                 *             TESTS            *
+                 *******************************/
 
 wine :-
     current_prolog_flag(wine_version, _).
@@ -217,32 +223,32 @@ wine :-
 :- begin_tests(saved_state, [condition(not(wine))]).
 
 test(true, Result == [true]) :-
-	state_output(1, Exe),
-	call_cleanup(
-	    ( create_state('input/plain.pl', Exe, ['-g', echo_true]),
-	      run_state(Exe, [], Result)
-	    ),
-	    remove_state(Exe)).
+    state_output(1, Exe),
+    call_cleanup(
+        ( create_state('input/plain.pl', Exe, ['-g', echo_true]),
+          run_state(Exe, [], Result)
+        ),
+        remove_state(Exe)).
 test(argv, Result == [[aap,noot,mies]]) :-
-	state_output(2, Exe),
-	call_cleanup(
-	    ( create_state('input/plain.pl', Exe, ['-g', echo_argv]),
-	      run_state(Exe, [aap, noot, mies], Result)
-	    ),
-	    remove_state(Exe)).
+    state_output(2, Exe),
+    call_cleanup(
+        ( create_state('input/plain.pl', Exe, ['-g', echo_argv]),
+          run_state(Exe, [aap, noot, mies], Result)
+        ),
+        remove_state(Exe)).
 test(true, Result == [true]) :-
-	state_output(3, Exe),
-	call_cleanup(
-	    ( create_state('input/data.pl', Exe, ['-g', test]),
-	      run_state(Exe, [], Result)
-	    ),
-	    remove_state(Exe)).
+    state_output(3, Exe),
+    call_cleanup(
+        ( create_state('input/data.pl', Exe, ['-g', test]),
+          run_state(Exe, [], Result)
+        ),
+        remove_state(Exe)).
 
 :- end_tests(saved_state).
 
-:- else.				% No library(process) found
+:- else.                                % No library(process) found
 
 test_saved_states :-
-	format(user_error, 'Skipped saved state tests; requires clib~n', []).
+    format(user_error, 'Skipped saved state tests; requires clib~n', []).
 
 :- endif.
