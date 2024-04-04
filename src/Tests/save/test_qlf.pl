ff4ffeb3c3abbf1f820e36f3c1d07277f6077d16
Jan Wielemaker <jan@swi-prolog.org>
2024-04-04 10:17:38 +0200

    Generalized QLF testing.

--- src/Tests/save/test_qlf.pl
+++ src/Tests/save/test_qlf.pl
@@ -3,9 +3,10 @@
     Author:        Jan Wielemaker
     E-mail:        J.Wielemaker@vu.nl
     WWW:           http://www.swi-prolog.org
-    Copyright (c)  2023, University of Amsterdam
-                         VU University Amsterdam
-		CWI, Amsterdam
+    Copyright (c)  2023-2024, University of Amsterdam
+                              VU University Amsterdam
+		              CWI, Amsterdam
+                              SWI-Prolog Solutions b.v.
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
@@ -39,14 +40,18 @@
           ]).
 :- use_module(library(plunit)).
 :- use_module(library(filesex), [directory_file_path/3]).
-:- use_module(library(debug), [assertion/1]).
+:- use_module(library(debug), [assertion/1, debug/3]).
+:- use_module(library(apply), [maplist/3, maplist/2]).
+:- use_module(library(prolog_code), [pi_head/2]).
 
 test_qlf :-
     run_tests([ qlf
               ]).
 
-find_me.
+:- meta_predicate
+    qlf_trip(+, +, +, -, -).
 
+find_me.
 file_path(File, Path) :-
     source_file(find_me, Here),
     file_directory_name(Here, Dir),
@@ -54,24 +59,59 @@ file_path(File, Path) :-
 
 :- begin_tests(qlf).
 
-% Note that ['$qlf'(Qlf)] is an undocumented way  to save the file in an
-% explicit location.
-
 test(unicode,
      [ Found =@= Expected,
+       setup(test_files(unicode, Prolog, Qlf)),
        cleanup(catch(delete_file(Qlf), _, true))
      ]) :-
-    file_path('input/unicode', In),
-    file_base_name(In, Base),
-    file_name_extension(Base, qlf, QlfFile),
-    current_prolog_flag(tmp_dir, Tmp),
-    directory_file_path(Tmp, QlfFile, Qlf),
-    catch(delete_file(Qlf), _, true),
-    load_files(In, ['$qlf'(Qlf)]),
-    findall(Data, data(Data), Expected),
-    unload_file(In),
-    assertion(\+ current_predicate(data/1)),
-    consult(Qlf),
-    findall(Data, data(Data), Found).
+    qlf_trip(Prolog,
+             Qlf,
+             [data(_)],
+             Expected, Found),
+    debug(qlf(result), '~q~n~q', [Expected, Found]).
 
 :- end_tests(qlf).
+
+test_files(Spec, Prolog, Qlf) :-
+    atomic_list_concat([input,Spec], /, RelFile),
+    file_path(RelFile, Prolog),
+    file_base_name(Prolog, Base),
+    file_name_extension(Base, qlf, QlfFile),
+    current_prolog_flag(tmp_dir, Tmp),
+    directory_file_path(Tmp, QlfFile, Qlf).
+
+%!  qlf_trip(+Prolog, +Qlf, :Goals, -Expected, -Found) is det.
+%
+%   Load and qcompile the file  Input   from  the directory `input`, run
+%   call(Goal, Expected) to get the expected data, unload the file, load
+%   the generated `.qlf` file and call call(Goal, Found.
+%
+%   Note that ['$qlf'(Qlf)] is an undocumented way   to save the file in
+%   an explicit location.
+
+qlf_trip(Prolog, Qlf, Goals, Expected, Found) :-
+    catch(delete_file(Qlf), _, true),
+    load_files(Prolog, ['$qlf'(Qlf)]),
+    run(Goals, Expected),
+    unload_file(Prolog),
+    assert_unloaded(Goals),
+    consult(Qlf),
+    run(Goals, Found).
+
+run(Goals, Result) :-
+    maplist(run1, Goals, Result).
+
+run1(Goal, Result) :-
+    term_variables(Goal, Vars),
+    (   Vars = [Templ]
+    ->  true
+    ;   Templ =.. [v|Vars]
+    ),
+    findall(Templ, Goal, Result).
+
+assert_unloaded(Goals) :-
+    maplist(assert_unloaded1, Goals).
+
+assert_unloaded1(Head) :-
+    pi_head(PI, Head),
+    assertion(\+ current_predicate(PI)).
