e0f5df13452bcae7c07dafcc85795fec0e4f05d6
Jan Wielemaker <jan@swi-prolog.org>
2024-08-14 19:37:38 +0200

    Preparing version 9.3.9

--- src/SWI-Prolog.h
+++ src/SWI-Prolog.h
@@ -60,7 +60,7 @@ extern "C" {
 /* PLVERSION_TAG: a string, normally "", but for example "rc1" */
 
 #ifndef PLVERSION
-#define PLVERSION 90308
+#define PLVERSION 90309
 #endif
 #ifndef PLVERSION_TAG
 #define PLVERSION_TAG ""
