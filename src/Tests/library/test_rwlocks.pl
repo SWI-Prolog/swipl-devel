78cc859c365e23c707dc98db07b4985cf1824f5d
Younes <git@younes.io>
2024-09-07 09:04:48 +0200

    update

--- src/Tests/library/test_rwlocks.pl
+++ src/Tests/library/test_rwlocks.pl
@@ -35,87 +35,105 @@
 	  [ test_rwlocks/0
 	  ]).
 :- use_module(library(plunit)).
-
+:- use_module(library(debug)).
+:- use_module(library(rwlocks)).
+:- use_module(library(thread)).
 
 test_rwlocks :-
 	run_tests([ rwlocks
 		  ]).
 
+:- begin_tests(rwlocks).
 
-:- begin_tests(rwlocks).                                                                                                                                          
-:- use_module(library(rwlocks)).
-                                                                                                                                                                      
-test(priority_handling) :-
+test(priority_handling, [thread]) :-
     retractall(started(_)),
-    with_rwlock(test_lock, 
-                (assertz(started(write)), 
-                sleep(1), 
-                writeln('Write operation completed')), 
-                write),
-    with_rwlock(test_lock, 
-                (assertz(started(read)), 
-                sleep(2), 
-                writeln('Read operation completed')), 
-                read),
+    message_queue_create(Queue),
+    thread_create(with_rwlock(test_lock,
+                              (assertz(started(write)),
+                               thread_send_message(Queue, write_done)),
+                              write),
+                 WriteThread, []),
+    thread_create(with_rwlock(test_lock,
+                              (assertz(started(read)),
+                               thread_send_message(Queue, read_done)),
+                              read),
+                 ReadThread, []),
+    thread_get_message(Queue, write_done),
+    thread_get_message(Queue, read_done),
+    maplist(thread_join, [WriteThread, ReadThread]),
     findall(X, started(X), Started),
-    writeln(Started),
-    assertion(Started == [write, read]).                                                                                                                    
-                                                                                                                                                                    
+    debug(test_rwlocks, 'Started order: ~w', [Started]),
+    assertion(Started == [write, read]),
+    message_queue_destroy(Queue).
+
 test(timeout_handling, [throws(time_limit_exceeded(rwlock))]) :-
-    thread_create(with_rwlock(test_lock, sleep(2), write), WriteThread, []),
-    sleep(0.1),  % Give some time for the write lock to be acquired
+    thread_create(with_rwlock(test_lock, thread_get_message(_), write), WriteThread, []),
+    thread_get_message(WriteThread, ready),
     catch(with_rwlock(test_lock, true, write, [timeout(0.5)]),
-        time_limit_exceeded(rwlock),
-        ( thread_signal(WriteThread, throw(abort_timeout_test)),
+          time_limit_exceeded(rwlock),
+          ( thread_signal(WriteThread, throw(abort_timeout_test)),
             retract_rwlock_data(test_lock),
             rethrow(time_limit_exceeded(rwlock))
-        )),
+          )),
     thread_join(WriteThread, _).
 
 % Helper predicate to clean up any leftover rwlock data
 retract_rwlock_data(LockId) :-
-    retractall(access(LockId, _, _)),
-    retractall(waiting(LockId, _, _, _)).
+    retractall(rwlocks:access(LockId, _, _)),
+    retractall(rwlocks:waiting(LockId, _, _, _)).
 
 % Helper predicate to re-throw the exception
 rethrow(Exception) :-
-    throw(Exception).                                                                                                          
-                                                                                                                                                                    
-test(concurrent_reads) :-
+    throw(Exception).
+
+test(concurrent_reads, [thread]) :-
     flag(concurrent_read_counter, _, 0),
-    thread_create(with_rwlock(test_lock, 
-                            (flag(concurrent_read_counter, X, X+1), 
-                            sleep(0.5)), 
-                            read),
-                T1, []),
-    thread_create(with_rwlock(test_lock, 
-                            (flag(concurrent_read_counter, Y, Y+1), 
-                            sleep(0.5)), 
-                            read),
-                T2, []),
-    thread_join(T1),
-    thread_join(T2),
+    message_queue_create(Queue),
+    thread_create(with_rwlock(test_lock,
+                              (flag(concurrent_read_counter, X, X+1),
+                               thread_send_message(Queue, done)),
+                              read),
+                 T1, []),
+    thread_create(with_rwlock(test_lock,
+                              (flag(concurrent_read_counter, Y, Y+1),
+                               thread_send_message(Queue, done)),
+                              read),
+                 T2, []),
+    thread_get_message(Queue, done),
+    thread_get_message(Queue, done),
+    maplist(thread_join, [T1, T2]),
     flag(concurrent_read_counter, FinalCount, FinalCount),
-    assertion(FinalCount == 2).                                                                                                                                  
-                                                                                                                                                                    
-test(write_blocks_reads) :-
+    debug(test_rwlocks, 'Final concurrent read count: ~w', [FinalCount]),
+    assertion(FinalCount == 2),
+    message_queue_destroy(Queue).
+
+test(write_blocks_reads, [thread]) :-
     flag(read_counter, _, 0),
-    thread_create(with_rwlock(test_lock, sleep(1), write), WriteThread, []),
-    sleep(0.1),  % Give some time for the write lock to be acquired
-    (   catch(with_rwlock(test_lock, 
-                        (flag(read_counter, _, 1), fail),  % Always fail after incrementing
-                        read, 
-                        [timeout(0.5)]),
-            time_limit_exceeded(rwlock),
-            true)
-    ->  true
-    ;   fail  % Fail if the read operation succeeded
-    ),
-    thread_join(WriteThread),
+    message_queue_create(Queue),
+    thread_create(with_rwlock(test_lock,
+                              thread_send_message(Queue, write_done),
+                              write),
+                 WriteThread, []),
+    thread_create((   catch(with_rwlock(test_lock,
+                                        (flag(read_counter, _, 1), fail),
+                                        read,
+                                        [timeout(0.5)]),
+                            time_limit_exceeded(rwlock),
+                            true)
+                  ->  thread_send_message(Queue, read_blocked)
+                  ;   thread_send_message(Queue, read_succeeded)
+                  ),
+                 ReadThread, []),
+    thread_get_message(Queue, write_done),
+    thread_get_message(Queue, ReadResult),
+    maplist(thread_join, [WriteThread, ReadThread]),
     flag(read_counter, ReadCount, ReadCount),
-    assertion(ReadCount == 0).                                                                                                      
-                                                                                                                                                                    
-test(invalid_mode, [throws(error(type_error(rwlock_mode, invalid), _))]) :-                                                                                       
-    with_rwlock(test_lock, true, invalid).                                                                                                                        
-                                                                                                                                                                      
-:- end_tests(rwlocks).  
+    debug(test_rwlocks, 'Read result: ~w, Read count: ~w', [ReadResult, ReadCount]),
+    assertion(ReadResult == read_blocked),
+    assertion(ReadCount == 0),
+    message_queue_destroy(Queue).
+
+test(invalid_mode, [throws(error(type_error(rwlock_mode, invalid), _))]) :-
+    with_rwlock(test_lock, true, invalid).
+
+:- end_tests(rwlocks).
