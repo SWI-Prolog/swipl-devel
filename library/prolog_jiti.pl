393f982c34807b68cf8e6c8d5d30cef0959710d1
Jan Wielemaker <jan@swi-prolog.org>
2025-01-20 17:45:00 +0100

    ADDED: jiti_suggest_modes/0,1
    Inspect the created JIT indexes, looking for arguments for which
    we could create an index, but the predicate is never called with
    the argument instantiated.

--- library/prolog_jiti.pl
+++ library/prolog_jiti.pl
@@ -36,15 +36,22 @@
 
 :- module(prolog_jiti,
           [ jiti_list/0,
-            jiti_list/1                         % +Spec
+            jiti_list/1,                % +Spec
+            jiti_suggest_modes/1,       % :Spec
+            jiti_suggest_modes/0
           ]).
-:- autoload(library(apply), [maplist/2, foldl/4]).
+:- autoload(library(apply), [maplist/2, foldl/4, convlist/3]).
 :- autoload(library(dcg/basics), [number/3]).
 :- autoload(library(ansi_term), [ansi_format/3]).
+:- autoload(library(prolog_code), [pi_head/2, most_general_goal/2]).
+:- autoload(library(listing), [portray_clause/1]).
+:- autoload(library(lists), [append/2]).
+:- autoload(library(ordsets), [ord_subtract/3]).
 
 
 :- meta_predicate
-    jiti_list(:).
+    jiti_list(:),
+    jiti_suggest_modes(:).
 
 /** <module> Just In Time Indexing (JITI) utilities
 
@@ -90,23 +97,10 @@ by the system and can help diagnosing space and performance issues.
 jiti_list :-
     jiti_list(_:_).
 
-jiti_list(Module:Name/Arity) :-
-    atom(Name),
-    integer(Arity),
+jiti_list(Spec) :-
+    spec_head(Spec, Head),
     !,
-    functor(Head, Name, Arity),
-    jiti_list(Module:Head).
-jiti_list(Module:Name/Arity) :-
-    atom(Name),
-    var(Arity),
-    !,
-    freeze(Head, functor(Head, Name, _)),
-    jiti_list(Module:Head).
-jiti_list(Module:Name) :-
-    atom(Name),
-    !,
-    freeze(Head, functor(Head, Name, _)),
-    jiti_list(Module:Head).
+    jiti_list(Head).
 jiti_list(Head) :-
     tty_width(TTYW),
     findall(Head-Indexed,
@@ -193,8 +187,126 @@ dcg_pi(Name/Arity, DCG) =>
     Arity2 is Arity-2,
     DCG = Name//Arity2.
 
+
+                /*******************************
+                *            MODES             *
+                *******************************/
+
+%!  jiti_suggest_modes is det.
+%!  jiti_suggest_modes(:Spec) is det.
+%
+%   Propose modes for the predicates referenced   by  Spec. This utility
+%   may be executed _after_ a  clean  load   of  your  program and after
+%   running the program. It searches  for   static  predicates that have
+%   been called and (thus) have been  examined for candidate indexes. If
+%   candidate indexes have not been materialized   this implies that the
+%   predicate was never called with a nonvar value for the corresponding
+%   argument. Adding a mode/1 declaration  may   be  used  to inform the
+%   system thereof. The system will never examine arguments for indexing
+%   that have been declared as mode `-`.
+%
+%   __Note:__ This predicate merely detects that some predicate is never
+%   called with instantiated specific arguments __during this run__. The
+%   user should verify whether the suggested   `-` arguments are correct
+%   and typically complete the mode by changing   `?`  into `+` (or `-`)
+%   where applicable. Currently, in SWI-Prolog, mode/1 declarations have
+%   no effect on the semantics of the   code. In particular, a predicate
+%   that declares some argument as `-` may  be called with this argument
+%   instantiated. This may change in the future.
+%
+%   @arg Spec uses the same conventions as jiti_list/1.
+
+jiti_suggest_modes :-
+    jiti_suggest_modes(_:_).
+
+jiti_suggest_modes(Partial) :-
+    spec_head(Partial, Head),
+    !,
+    jiti_suggest_modes(Head).
+jiti_suggest_modes(Head) :-
+    Head = M:_,
+    freeze(M, module_property(M, class(user))),
+    findall(Head-Indexed,
+            (   predicate_property(Head, indexed(Indexed)),
+                \+ predicate_property(Head, imported_from(_))
+            ), Pairs),
+    convlist(suggest_mode, Pairs, Modes),
+    (   Modes == []
+    ->  print_message(informational, jiti(no_modes(Head)))
+    ;   maplist(portray_clause, Modes)
+    ).
+
+suggest_mode((M:Head)-Indexes, (:- mode(M:GenHead))) :-
+    convlist(not_realised_index_arg, Indexes, FArgs),
+    convlist(realised_index_arg, Indexes, ArgsL),
+    append(ArgsL, Realised),
+    sort(FArgs, Sargs),
+    sort(Realised, RArgs),
+    ord_subtract(Sargs, RArgs, Args),
+    Args \== [],
+    !,
+    most_general_goal(Head, GenHead),
+    make_mode(Args, GenHead).
+
+not_realised_index_arg(Index, Arg) :-
+    _{ arguments:[Arg], position:[], realised:false } :< Index.
+
+realised_index_arg(Index, Args) :-
+    _{ arguments:Args, position:[], realised:true } :< Index.
+
+make_mode([], GenHead) =>
+    functor(GenHead, _, Arity),
+    set_any(1, Arity, GenHead).
+make_mode([H|T], GenHead) =>
+    arg(H, GenHead, -),
+    make_mode(T, GenHead).
+
+set_any(I, Arity, GenHead), arg(I, GenHead, Var) =>
+    (   var(Var)
+    ->  Var = '?'
+    ;   true
+    ),
+    I2 is I+1,
+    set_any(I2, Arity, GenHead).
+set_any(_, _, _) =>
+    true.
+
+
+                /*******************************
+                *      SPECIFY PREDICATES      *
+                *******************************/
+
+spec_head(Module:Name/Arity, Head), atom(Name), integer(Arity) =>
+    Head = Module:Head0,
+    functor(Head0, Name, Arity).
+spec_head(Module:Name/Arity, Head), atom(Name), var(Arity) =>
+    Head = Module:Head0,
+    freeze(Head0, functor(Head0, Name, _)).
+spec_head(Module:Name, Head), atom(Name) =>
+    Head = Module:Head0,
+    freeze(Head0, functor(Head0, Name, _)).
+spec_head(_, _) =>
+    fail.
+
+                /*******************************
+                *            OUTPUT            *
+                *******************************/
+
 tty_width(W) :-
     catch(tty_size(_, TtyW), _, fail),
     !,
     W is max(65, TtyW).
 tty_width(80).
+
+                /*******************************
+                *           MESSAGES           *
+                *******************************/
+
+:- multifile prolog:message//1.
+
+prolog:message(jiti(no_modes(M:Head))) -->
+    { var(Head) },
+    [ 'No mode suggestions for predicates in module ~p'-[M] ].
+prolog:message(jiti(no_modes(Head))) -->
+    { numbervars(Head, 0, _, [singletons(true)]) },
+    [ 'No mode suggestions for ~p'-[Head] ].
