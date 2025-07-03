3b9b286beb3085463aeefbd50f303d26d06249e5
Jan Wielemaker <jan@swi-prolog.org>
2025-07-03 21:49:01 +0200

    PORT: Find MinGW DLL dependencies more dynamically
    This provides two CMake functions to find the direct and indirect MinGW
    DLLs that must be included into the distribution

--- cmake/port/Windows.cmake
+++ cmake/port/Windows.cmake
@@ -34,9 +34,130 @@ set(SRC_OS_SPECIFIC pl-nt.c pl-ntconsole.c pl-dde.c os/windows/uxnt.c)
 set(LIBSWIPL_LIBRARIES ${LIBSWIPL_LIBRARIES} winmm.lib ws2_32.lib psapi.lib)
 
 if(MINGW_ROOT)
+set(MINGW_BIN "${MINGW_ROOT}/bin")
 # For MinGW we need to copy the   dlls  to the target destinations. When
 # using MSVC we use vcpkg. The  included toolchain installs the required
 # DLLs for us.
+
+function(add_mingw_indirect_deps result_list)
+  set(queue "")
+  set(visited "")
+  set(found "")
+  
+  set(initial_dlls ${ARGN})
+
+  foreach(dll IN LISTS initial_dlls)
+    if(IS_ABSOLUTE "${dll}")
+      list(APPEND queue "${dll}")
+    else()
+      file(GLOB dll_path "${MINGW_BIN}/${dll}")
+      if(NOT dll_path)
+	message(FATAL_ERROR "Initial DLL '${dll}' not found in ${MINGW_BIN}")
+      endif()
+      list(APPEND queue "${dll_path}")
+    endif()
+  endforeach()
+  set(found ${queue})
+
+  while(queue)
+    list(GET queue 0 current)
+    list(REMOVE_AT queue 0)
+
+    list(FIND visited "${current}" already_idx)
+    if(NOT already_idx EQUAL -1)
+      continue()
+    endif()
+    list(APPEND visited "${current}")
+    
+    execute_process(
+      COMMAND x86_64-w64-mingw32-objdump -p "${current}"
+      OUTPUT_VARIABLE objdump_out
+      ERROR_QUIET
+      RESULT_VARIABLE objdump_res
+    )
+
+    if(NOT objdump_res EQUAL 0)
+      message(WARNING "Failed to objdump ${current}")
+      continue()
+    endif()
+
+    string(REGEX MATCHALL "DLL Name:[^\n\r]*" dll_lines "${objdump_out}")
+    foreach(line IN LISTS dll_lines)
+      string(REGEX REPLACE ".*DLL Name:[ \t]*" "" dep_name "${line}")
+
+      file(GLOB dep_path "${MINGW_BIN}/${dep_name}")
+      if(dep_path)
+        list(FIND visited "${dep_path}" visited_idx)
+        if(visited_idx EQUAL -1)
+          list(APPEND queue "${dep_path}")
+        endif()
+        list(APPEND found "${dep_path}")
+      endif()
+    endforeach()
+  endwhile()
+
+  list(REMOVE_DUPLICATES found)
+  set(${result_list} "${found}" PARENT_SCOPE)
+endfunction()
+
+# Find the DLLs required immediately by a target 
+
+function(get_target_mingw_dlls result_list target)
+  get_target_property(libs ${target} LINK_LIBRARIES)
+
+  if(NOT libs)
+    message(WARNING "Target '${target}' has no link libraries")
+    set(${result_list} "" PARENT_SCOPE)
+    return()
+  endif()
+
+  set(dlls "")
+  
+  if(CMAKE_BUILD_TYPE)
+    string(TOUPPER "${CMAKE_BUILD_TYPE}" config)
+  else()
+    set(config RELWITHDEBINFO)
+  endif()
+
+  foreach(lib IN LISTS libs)
+    if(TARGET ${lib})
+      get_target_property(is_imported ${lib} IMPORTED)
+      if(is_imported)
+        get_target_property(loc ${lib} IMPORTED_LOCATION_${config})
+	if(NOT loc)
+	  get_target_property(loc "${lib}" IMPORTED_LOCATION)
+	endif()
+	if(NOT loc)
+	  get_target_property(loc "${lib}" LOCATION_${config})
+	endif()
+	if(NOT loc)
+	  get_target_property(loc "${lib}" LOCATION)
+	endif()
+
+        if(loc)
+          file(TO_CMAKE_PATH "${loc}" lib_path)
+	else()
+	  get_target_property(llibs "${lib}" INTERFACE_LINK_LIBRARIES)
+	  foreach(llib ${llibs})
+	    get_filename_component(lname "${llib}" NAME_WE)
+	    file(GLOB dll "${MINGW_BIN}/${lname}*.dll")
+	    list(APPEND dlls ${dll})
+	  endforeach()
+        endif()
+      else()
+	file(TO_CMAKE_PATH "${lib}" lib_path)
+      endif()
+
+      if(lib_path MATCHES "${MINGW_BIN}/.*\\.dll$")
+        list(APPEND dlls "${lib_path}")
+      endif()
+    endif()
+  endforeach()
+
+  list(REMOVE_DUPLICATES dlls)
+  set(${result_list} "${dlls}" PARENT_SCOPE)
+endfunction()
+
 
 if(NOT DEFINED WIN32_DLLS)
 
