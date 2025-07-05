55c4d0b5e37ef77367fbd8ec9a2717d7ce26b715
Jan Wielemaker <jan@swi-prolog.org>
2025-07-05 18:43:16 +0200

    Small cleanup

--- src/pl-main.c
+++ src/pl-main.c
@@ -146,13 +146,10 @@ main(int argc, char **argv)
   if ( !PL_initialise(argc, argv) )
     PL_halt(1);
 
-  for(;;)
-  { int status = PL_toplevel() ? 0 : 1;
+  int status = PL_toplevel() ? 0 : 1;
+  PL_halt(status);
 
-    PL_halt(status);
-  }
-
-  return 0;
+  return status;
 }
 
 
