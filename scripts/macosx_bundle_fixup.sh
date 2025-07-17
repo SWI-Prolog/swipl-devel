741bb269cf8d9a4f501827f09e843f8e21044c01
Jan Wielemaker <jan@swi-prolog.org>
2025-07-17 17:46:21 +0200

    PORT: Fixed MacOS installer to include transitive dependencies and fix the @rpath.

--- scripts/macosx_bundle_fixup.sh
+++ scripts/macosx_bundle_fixup.sh
@@ -52,39 +52,45 @@ fi
 
 printf "Adding Macport dylibs to modules\n"
 
-changeset="$(echo $moduledir/*) $app/Contents/MacOS/swipl"
-while [ ! -z "$changeset" ]; do
-  newchanges=
-  for f in $changeset; do
-    case "$(file $f)" in
-      *Mach-O*)
-        opt_dep=$(otool -L $f |
-		  grep '\(/opt/local\|/deps/\)' |
-		  grep -v libjvm |
-		  awk '{print $1}')
-        if [ ! -z "$opt_dep" ]; then
-          change=""
-          for dep in $opt_dep; do
-            file="$frameworkdir/$(basename $dep)"
-            if [ ! -f $file ]; then
-              printf "   Adding $dep ... "
-              cp $dep $frameworkdir
-              chmod 755 $file
-              newchanges="$newchanges $file"
-              printf 'ok\n'
-            fi
-            change="$change -change $dep @rpath/$(basename $dep)"
-          done
-          if [ ! -z "$change" ]; then
-            install_name_tool $change $f
-          fi
-        fi
-        ;;
-      *)
-    esac
-  done
-  changeset="$newchanges"
-done
+fixup_files()
+{
+    changeset="$*"
+    while [ ! -z "$changeset" ]; do
+	newchanges=
+	for f in $changeset; do
+	    case "$(file $f)" in
+		*Mach-O*)
+		    opt_dep=$(otool -L $f |
+				  grep '\(/opt/local\|/deps/\)' |
+				  grep -v libjvm |
+				  awk '{print $1}')
+		    if [ ! -z "$opt_dep" ]; then
+			change=""
+			for dep in $opt_dep; do
+			    file="$frameworkdir/$(basename $dep)"
+			    if [ ! -f $file ]; then
+				printf "   Adding $dep ... "
+				cp $dep $frameworkdir
+				chmod 755 $file
+				newchanges="$newchanges $file"
+				printf 'ok\n'
+			    fi
+			    change="$change -change $dep @rpath/$(basename $dep)"
+			done
+			if [ ! -z "$change" ]; then
+			    install_name_tool $change $f
+			fi
+		    fi
+		    ;;
+		*)
+	    esac
+	done
+	changeset="$newchanges"
+    done
+}
+
+fixup_files $(echo $moduledir/*) $app/Contents/MacOS/swipl
+fixup_files $(echo $frameworkdir/*)
 
 # Code signing
 
