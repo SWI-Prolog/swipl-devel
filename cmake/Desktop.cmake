d7f6c47660e4532c3dbba0f5edce2f17fa795ec0
Jan Wielemaker <jan@swi-prolog.org>
2025-07-21 19:22:26 +0200

    Updated desktop integration files

--- cmake/Desktop.cmake
+++ cmake/Desktop.cmake
@@ -7,6 +7,14 @@ if(has_xpce OR has_swipl_win)
   set(DESKTOP_SOURCE ${CMAKE_SOURCE_DIR}/desktop)
   set(SWIPL_INSTALL_DESKTOP ${SWIPL_INSTALL_PREFIX}/desktop)
 
+  if(EPILOG)
+    set(SWIPL_APP_ID org.swi_prolog.swipl-win)
+    set(SWIPL_CLI_ID org.swi_prolog.swipl-win)
+  else()
+    set(SWIPL_APP_ID SWI-Prolog)
+    set(SWIPL_CLI_ID SWI-Prolog)
+  endif()
+
   install_src(icon
     FILES
     ${DESKTOP_SOURCE}/swipl.png
@@ -16,7 +24,7 @@ if(has_xpce OR has_swipl_win)
 
   if(UNIX AND NOT APPLE)
     message("Configuring freedesktop.org desktop files")
-    if(has_swipl_win)
+    if(has_swipl_win OR EPILOG)
       configure_file(${DESKTOP_SOURCE}/swipl-win.desktop.in
 	${SWIPL_BUILD_HOME}/desktop/swipl-win.desktop)
       install(FILES ${SWIPL_BUILD_HOME}/desktop/swipl-win.desktop
