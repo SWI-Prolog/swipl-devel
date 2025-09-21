3b512208b5eb5d93e9566149b4218de10017640e
Jan Wielemaker <jan@swi-prolog.org>
2025-09-21 11:22:49 +0200

    CLEANUP: Remove old swipl-win.exe code.

--- src/pl-fli.c
+++ src/pl-fli.c
@@ -5346,10 +5346,8 @@ PL_dispatch_hook(PL_dispatch_hook_t hook)
 #endif
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-Note that this is  used  to   integrate  X11  event-dispatching into the
-SWI-Prolog  toplevel.  Integration  of  event-handling   in  Windows  is
-achieved through the plterm DLL (see  win32/console). For this reason we
-do never want this code in Windows.
+Note that this is  used  to   integrate  SDL  event-dispatching into the
+SWI-Prolog  toplevel.
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 static bool
