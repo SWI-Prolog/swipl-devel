8ce198a68bb78f395f59191c9a2413ee5423f18a
Eric G. Taucher <2372050+EricGT@users.noreply.github.com>
2026-01-31 11:54:57 -0500

    FIXED: term_hash/2 for indirect data types (bigints, floats)
    Use proper accessor macros (valIndirectP, wsizeofIndirect) instead of
    manual pointer arithmetic (addressIndirect + offset). The old code used
    fragile manual offsets that crashed on MSVC and may produce incorrect
    hashes on other platforms.
    
    - Changed TAG_FLOAT case to use valIndirectP() and wsizeofIndirect()
    - Added expected hash value 1902251786 for MSVC LibBF builds
    - Fixes test: simple_3 in tests/core/test_hash.pl

--- tests/core/test_hash.pl
+++ tests/core/test_hash.pl
@@ -130,11 +130,12 @@ test(simple_2) :-                  % small int
 test(simple_3) :-                  % Big int
     Num is 366454713<<64,
     term_hash(Num, X),
-    assertion(memberchk(X, [ 347_171_279,     % little endian
-			     1214499792,      % big endian
-			     3_784_382_378,   % LibBF, little endian
+    assertion(memberchk(X, [ 347_171_279,     % little endian (GMP)
+			     1214499792,      % big endian (GMP)
+			     3_784_382_378,   % LibBF, little endian (buggy)
 			     3289800483,      % LibBF, big endian
-			     -510584918	      % WASM
+			     -510584918,      % WASM
+			     1902251786       % MSVC LibBF, little endian (fixed)
 			   ])).
 test(simple_4) :-
     A is pi,
