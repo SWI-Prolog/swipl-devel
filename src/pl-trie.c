b40bcb11df3fac5e79a799b9424e5037d425715e
Jan Wielemaker <jan@swi-prolog.org>
2026-01-31 17:54:20 +0100

    CLEANUP: Avoid reading uninitialized local variable.
    Not entirely sure why the popSegStack() can fail.   Surely it does on the
    XSB tests from `tests/xsb/sub_tests/xsb_test_sub.pl`.  In debug mode we
    set `dstate` such that accessing it crashes.  This seems not to be the
    case.

--- src/pl-trie.c
+++ src/pl-trie.c
@@ -2247,9 +2247,16 @@ add_choice(DECL_LD trie_gen_state *state,
 
 	  if ( IS_TRIE_KEY_POP(children.key->key) && dstate->compound )
 	  { desc_tstate dts;
-	    popSegStack(&dstate->stack, &dts, desc_tstate);
-	    dstate->term = dts.term;
-	    dstate->size = dts.size;
+
+	    if ( popSegStack(&dstate->stack, &dts, desc_tstate) )
+	    { dstate->term = dts.term;
+	      dstate->size = dts.size;
+	    } else	      /* Force a crash if we access these */
+	    { DEBUG(0,
+		    { dstate->term = NULL;
+		      dstate->size = (size_t)-1;
+		    });
+	    }
 	    DEBUG(MSG_TRIE_GEN,
 		  Sdprintf("Popped %p, left %zd\n",
 			   dstate->term, dstate->size));
