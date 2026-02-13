0025759ddf6656826329c9edb27d5e31b590fe6d
Eric G. Taucher <2372050+EricGT@users.noreply.github.com>
2026-02-13 02:56:49 -0500

    CLEANUP: Avoid unreferenced variable warning for MSVC

--- src/pl-load.c
+++ src/pl-load.c
@@ -192,9 +192,8 @@ blocked_unload_shared_objects(void)
   if ( vg == -1 )
   {
 #ifdef __SANITIZE_ADDRESS__
-    char *s;
-
 #ifndef __WINDOWS__
+    char *s;
     if ( (s=getenv("ASAN_OPTIONS")) && strstr(s,"detect_leaks=1") )
 #endif
     { vg = true;
