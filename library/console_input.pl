91bbb735bd936a897401af97cba41ca40686ab3e
Jan Wielemaker <jan@swi-prolog.org>
2026-03-04 12:05:25 +0100

    ADDED: prolog:complete_input/4: enhanced CLI TAB completion
      - Complete variables in the query using Emacs _dabrev_ completion
      - Complete in $Var for saved toplevel variables.

--- library/console_input.pl
+++ library/console_input.pl
@@ -40,6 +40,15 @@
 :- autoload(library(dcg/basics), [remainder/3]).
 :- autoload(library(apply), [maplist/3, convlist/3]).
 
+/** <module> Support entering toplevel queries
+
+This module implements prolog:complete_input/4, which is used notably by
+the command line editor  library(edit)  to   perform  context  aware TAB
+completion. It is a seperate library such that other input scenarios can
+reuse this code.
+*/
+
+
 :- multifile
     prolog:complete_input/4.
 
@@ -47,7 +56,32 @@
 %!                        -Delete:atom, -Completions:list) is semidet.
 %
 %   Compute    auto    completions    for      the     input    line
-%   BeforeCursor+AfterCursor.
+%   BeforeCursor+AfterCursor.  Implemented completions
+%
+%     - Files<br>
+%       - Text starting ``'<prefix>`` (single quotes) is completed as a
+%         file. If there is a single matching file a terminating `'` is
+%         added.
+%       - If the start of the line is ``[<prefix>`` and text can be
+%         a non-quoted atom, look for Prolog files with this name.
+%         Directories can be inserted as `/` (unquoted), as long as
+%         each _segment_ is a valid atom.  If a single ``.pl`` file
+%         is found it is closed with ``]``, _without_ the extension.
+%       - Text starting ``library(<prefix>`` is completed as a
+%         library file using the same rules as ``[<prefix>``, but
+%         looking in all library directories instead of the current
+%         directory.
+%     - Atoms<br>
+%       ``<prefix>`` is used to complete atoms that can be written
+%       without quotes (excluding symbol atoms such as ``-->``) and
+%       are _permanent_, i.e., have references from clauses records,
+%       flags, foreign code, etc.
+%     - Variables<br>
+%       ``<Prefix>`` is completed as an atom if it is a valid variable
+%       name.  It is completed with other atoms from the query being
+%       edited using the same search as Emacs _dabbrev_ (_Dynamic
+%       Abbreviation_). ``$<Prefix>`` expands to saved toplevel
+%       variables.
 %
 %   @arg    Delete is an atom or string representing the text that is
 %           replaced by the completion
@@ -59,12 +93,12 @@
 %             Used for a completion with comment.  This will be
 %             used for predicates.
 
-prolog:complete_input(Before, _After, Delete, Completions) :-
+prolog:complete_input(Before, After, Delete, Completions) :-
     string_codes(Before, Chars),
     reverse(Chars, BeforeRev),
-    complete(BeforeRev, Delete, Completions).
+    complete(BeforeRev, After, Delete, Completions).
 
-complete(BeforeRev, Prefix, Files) :-   % complete files
+complete(BeforeRev, _After, Prefix, Files) :-   % complete files
     phrase(file_prefix(Prefix, Type), BeforeRev),
     !,
     (   Type = library(Close)
@@ -73,16 +107,58 @@ complete(BeforeRev, Prefix, Files) :-   % complete files
         expand_file_name(Pattern, Files0),
         finish_file_name(Files0, Type, Files)
     ).
-complete(BeforeRev, Prefix, Atoms) :-   % complete atoms
-    phrase(atom_prefix(Prefix), BeforeRev, _),
+complete(BeforeRev, After, Prefix, Completions) :-   % complete atoms
+    phrase(identifier_prefix_r(Prefix, Type), BeforeRev, BeforeRev1),
     !,
+    identifier_completions(Type, Prefix, BeforeRev1, After, Completions).
+
+identifier_completions(atom, Prefix, _, _, Atoms) :-
     '$atom_completions'(Prefix, Atoms).
+identifier_completions(var, Prefix, BeforeRev, After, Vars) :-
+    findall(Var, var_starts(Prefix, BeforeRev, After, Var), Vars0),
+    sort(Vars0, Vars).
 
-%!  atom_prefix(-Prefix) is det.
+var_starts(Prefix, [0'$|_], _After, Var) :-        % $Var
+    !,
+    recorded('$topvar', Var = _),
+    sub_atom(Var, 0, _, _, Prefix).
+var_starts(Prefix, BeforeRev, _After, Var) :-        % Backard search
+    phrase((..., nonid_char, identifier_prefix_r(Var, var)),
+           BeforeRev, _),
+    sub_string(Var, 0, _, _, Prefix).
+var_starts(Prefix, _BeforeRev, AfterString, Var) :-  % Forward search
+    string_codes(AfterString, After),
+    phrase((..., nonid_char, identifier_prefix(Var, var)),
+           After, _),
+    sub_string(Var, 0, _, _, Prefix).
 
-atom_prefix(Prefix) -->
+... --> [] ; [_], ... .
+
+nonid_char -->
+    [C],
+    { \+ code_type(C, prolog_identifier_continue) }.
+
+%!  identifier_prefix(-Prefix, -Type)   is semidet.
+%!  identifier_prefix_r(-Prefix, -Type) is semidet.
+
+identifier_prefix(Prefix, Type) -->
+    atom_chars(String),
+    { String = [H|_],
+      (   code_type(H, prolog_var_start)
+      ->  Type = var
+      ;   Type = atom
+      ),
+      string_codes(Prefix, String) % do not create an atom
+    }.
+
+identifier_prefix_r(Prefix, Type) -->
     atom_chars(RevString),
     { reverse(RevString, String),
+      String = [H|_],
+      (   code_type(H, prolog_var_start)
+      ->  Type = var
+      ;   Type = atom
+      ),
       string_codes(Prefix, String) % do not create an atom
     }.
 
@@ -91,7 +167,7 @@ atom_chars([]) --> [].
 
 atom_char(C) --> [C], { atom_char(C) }.
 
-atom_char(C) :- code_type(C, csym).
+atom_char(C) :- code_type(C, prolog_identifier_continue).
 
 %!  file_prefix(-Prefix, -Type)// is semidet.
 %
