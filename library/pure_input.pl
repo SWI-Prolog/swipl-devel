6f9c1a09778d8018edff3bd075b94400079a9c48
Jan Wielemaker <jan@swi-prolog.org>
2026-07-14 15:28:02 +0200

    ADDED: #667 phrase_from_file/3: as(Type) argument, phrase_from_stream/3, stream_to_lazy_codes/2 and stream_to_lazy_chars/2
    Allow library(pure_input) to work with _chars_ lists as well as _codes_ lists.

--- library/pure_input.pl
+++ library/pure_input.pl
@@ -1,10 +1,11 @@
 /*  Part of SWI-Prolog
 
     Author:        Jan Wielemaker
-    E-mail:        J.Wielemaker@vu.nl
-    WWW:           http://www.swi-prolog.org
-    Copyright (C): 2008-2016, University of Amsterdam
+    E-mail:        jan@swi-prolog.org
+    WWW:           https://www.swi-prolog.org
+    Copyright (C): 2008-2026, University of Amsterdam
                               VU University Amsterdam
+                              SWI-Prolog Solutions b.v.
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
@@ -37,15 +38,18 @@
           [ phrase_from_file/2,         % :Grammar, +File
             phrase_from_file/3,         % :Grammar, +File, +Options
             phrase_from_stream/2,       % :Grammar, +Stream
-            stream_to_lazy_list/2,      % :Stream -List
+            phrase_from_stream/3,       % :Grammar, +Stream, +Options
+            stream_to_lazy_codes/2,     % :Stream -Codes
+            stream_to_lazy_chars/2,     % :Stream -Chars
+            stream_to_lazy_list/2,      % :Stream -Codes
 
             syntax_error//1,            % +ErrorTerm
                                         % Low level interface
             lazy_list_location//1,      % -Location
             lazy_list_character_count//1 % -CharacterCount
           ]).
-:- autoload(library(error),[type_error/2,permission_error/3]).
-
+:- autoload(library(error), [type_error/2, permission_error/3, must_be/2]).
+:- autoload(library(option), [option/3]).
 
 :- set_prolog_flag(generate_debug_info, false).
 
@@ -70,23 +74,33 @@ attribute handling to realise pure input on any (buffered) stream.
         terms, tokens, etc.
 */
 
+:- predicate_options(phrase_from_stream/3, 3,
+                     [ as(oneof([codes,chars]))
+                     ]).
 :- predicate_options(phrase_from_file/3, 3,
-                     [ pass_to(system:open/4, 4)
+                     [ pass_to(system:open/4, 4),
+                       pass_to(phrase_from_stream/3, 3)
                      ]).
 
 %!  phrase_from_file(:Grammar, +File) is nondet.
+%!  phrase_from_file(:Grammar, +File, +Options) is nondet.
 %
-%   Process the content of File  using   the  DCG  rule Grammar. The
-%   space usage of this mechanism depends on   the length of the not
-%   committed part of Grammar. Committed parts of the temporary list
-%   are reclaimed by the  garbage  collector,   while  the  list  is
-%   extended on demand due to  unification   of  the attributed tail
-%   variable. Below is an example that counts  the number of times a
-%   string appears in  a  file.   The  library  dcg/basics  provides
-%   string//1 matching an arbitrary string   and  remainder//1 which
-%   matches the remainder of the input without parsing.
+%   Process the content of File using the   DCG  rule Grammar. The space
+%   usage of this mechanism depends on the   length of the not committed
+%   part of Grammar. Committed parts of the temporary list are reclaimed
+%   by the garbage collector, while the list   is extended on demand due
+%   to unification of the attributed tail variable.
 %
-%   ==
+%   Options are passed to open/4   and  phrase_from_stream/3. The latter
+%   notable allows for as(chars) to call a DCG compiled for dealing with
+%   character lists rather than codes.
+%
+%   Below is an example that counts the number of times a string appears
+%   in a file. The library  dcg/basics   provides  string//1 matching an
+%   arbitrary string and remainder//1 which matches the remainder of the
+%   input without parsing.
+%
+%   ```
 %   :- use_module(library(dcg/basics)).
 %
 %   file_contains(File, Pattern) :-
@@ -99,14 +113,14 @@ attribute handling to realise pure input on any (buffered) stream.
 %
 %   match_count(File, Pattern, Count) :-
 %           aggregate_all(count, file_contains(File, Pattern), Count).
-%   ==
+%   ```
 %
 %   This can be called as (note that   the  pattern must be a string
 %   (code list)):
 %
-%   ==
+%   ```
 %   ?- match_count('pure_input.pl', `file`, Count).
-%   ==
+%   ```
 
 :- meta_predicate
     phrase_from_file(//, +),
@@ -116,24 +130,29 @@ attribute handling to realise pure input on any (buffered) stream.
 phrase_from_file(Grammar, File) :-
     phrase_from_file(Grammar, File, []).
 
-%!  phrase_from_file(:Grammar, +File, +Options) is nondet.
-%
-%   As phrase_from_file/2, providing additional Options. Options are
-%   passed to open/4.
-
 phrase_from_file(Grammar, File, Options) :-
     setup_call_cleanup(
         open(File, read, In, Options),
-        phrase_from_stream(Grammar, In),
+        phrase_from_stream(Grammar, In, Options),
         close(In)).
 
-%!  phrase_from_stream(:Grammar, +Stream)
+%!  phrase_from_stream(:Grammar, +Stream).
+%!  phrase_from_stream(:Grammar, +Stream, +Options).
 %
 %   Run Grammer against the character codes   on Stream. Stream must
-%   be buffered.
+%   be buffered.  Options:
+%
+%     - as(Type)
+%       Where Type is one of `codes` (default) or `chars`.  See also
+%       stream_to_lazy_codes/2 and stream_to_lazy_chars/2.
 
 phrase_from_stream(Grammar, In) :-
-    stream_to_lazy_list(In, List),
+    phrase_from_stream(Grammar, In, []).
+
+phrase_from_stream(Grammar, In, Options) :-
+    option(as(Type), Options, codes),
+    must_be(oneof([codes,chars]), Type),
+    stream_to_lazy_list(In, List, Type),
     phrase(Grammar, List).
 
 %!  syntax_error(+Error)//
@@ -170,7 +189,7 @@ lazy_list_location(Here, Location) :-
     '$skip_list'(Skipped, Here, Tail),
     (   attvar(Tail)
     ->  get_attr(Tail, pure_input, State),
-        State = lazy_input(Stream, PrevPos, Pos, _),
+        State = lazy_input(Stream, PrevPos, Pos, _Type, _),
         Details = [Line, LinePos, CharNo],
         (   stream_property(Stream, file_name(File))
         ->  PosParts = [file, File|Details]
@@ -223,43 +242,57 @@ lazy_list_character_count(Here, CharNo) :-
     ).
 
 
+%!  stream_to_lazy_codes(+Stream, -List) is det.
+%!  stream_to_lazy_chars(+Stream, -List) is det.
 %!  stream_to_lazy_list(+Stream, -List) is det.
 %
-%   Create a lazy list representing the   character codes in Stream.
-%   List is a  partial  list  ending   in  an  attributed  variable.
-%   Unifying this variable reads the next   block of data. The block
-%   is stored with the attribute value such that there is no need to
+%   Create a lazy list  representing  the   character  codes  (chars) in
+%   Stream. List is a partial  list   ending  in an attributed variable.
+%   Unifying this variable reads the next block   of  data. The block is
+%   stored with the attribute value  such  that   there  is  no  need to
 %   re-read it.
 %
+%   @deprecated stream_to_lazy_list/2 is a backward compatibility alias
+%   for stream_to_lazy_codes/2.
 %   @compat Unlike the previous version of this predicate this
 %           version does not require a repositionable stream.  It
 %           does require a buffer size of at least the maximum
 %           number of bytes of a multi-byte sequence (6).
 
+stream_to_lazy_codes(Stream, List) :-
+    stream_to_lazy_list(Stream, List, codes).
+stream_to_lazy_chars(Stream, List) :-
+    stream_to_lazy_list(Stream, List, chars).
 stream_to_lazy_list(Stream, List) :-
+    stream_to_lazy_list(Stream, List, codes).
+
+stream_to_lazy_list(Stream, List, Type) :-
     (   stream_property(Stream, buffer(false))
     ->  permission_error(create, lazy_list, Stream)
     ;   true
     ),
-    stream_to_lazy_list(Stream, -, List).
+    stream_to_lazy_list(Stream, -, Type, List).
 
-stream_to_lazy_list(Stream, PrevPos, List) :-
+stream_to_lazy_list(Stream, PrevPos, Type, List) :-
     stream_property(Stream, position(Pos)),
-    put_attr(List, pure_input, lazy_input(Stream, PrevPos, Pos, _)).
+    put_attr(List, pure_input, lazy_input(Stream, PrevPos, Pos, Type, _)).
 
 attr_unify_hook(State, Value) :-
     '$notrace'(attr_unify_hook_ndebug(State, Value)).
 
 attr_unify_hook_ndebug(State, Value) :-
-    State = lazy_input(Stream, _PrevPos, Pos, Read),
+    State = lazy_input(Stream, _PrevPos, Pos, Type, Read),
     (   var(Read)
     ->  fill_buffer(Stream),
-        read_pending_codes(Stream, NewList, Tail),
+        (   Type == codes
+        ->  read_pending_codes(Stream, NewList, Tail)
+        ;   read_pending_chars(Stream, NewList, Tail)
+        ),
         (   Tail == []
-        ->  nb_setarg(4, State, []),
+        ->  nb_setarg(5, State, []),
             Value = []
-        ;   stream_to_lazy_list(Stream, Pos, Tail),
-            nb_linkarg(4, State, NewList),
+        ;   stream_to_lazy_list(Stream, Pos, Type, Tail),
+            nb_linkarg(5, State, NewList),
             Value = NewList
         )
     ;   Value = Read
